fc238d84f59203439dad25b74a9dec1031c76e92
Yunfei He <i.heyunfei@gmail.com>
2023-09-20 21:59:03 +0800

    chore: add comments for `NamedImport`, `LocalExport` and `ReExport`

--- crates/rolldown_common/src/named_export.rs
+++ crates/rolldown_common/src/named_export.rs
@@ -3,6 +3,10 @@ use oxc::span::Atom;
 use super::import_record::ImportRecordId;
 use crate::symbol_ref::SymbolRef;
 
+/// This is a representation for statements like
+/// - Case A: `export function foo() {}`
+/// - Case B: `const foo = 1; export { foo }`
+/// - Case C: `const foo = 1; export { foo as foo2 }`
 #[derive(Debug)]
 pub struct LocalExport {
   pub referenced: SymbolRef,
@@ -14,9 +18,17 @@ impl From<LocalExport> for LocalOrReExport {
   }
 }
 
+/// This is a representation for statements like
+/// - Case A: `export { foo } from 'foo'`
+/// - Case B: `export * as fooNs from 'foo'`
+/// - Case C: `export { foo as foo2 } from 'foo'`
 #[derive(Debug)]
 pub struct ReExport {
+  /// For case A, the `imported` is `foo`.
+  /// For case B, the `imported` is meaningless.
+  /// For case C, the `imported` is `foo`.
   pub imported: Atom,
+  /// This will only be `true` for case B.
   pub is_imported_star: bool,
   pub record_id: ImportRecordId,
 }

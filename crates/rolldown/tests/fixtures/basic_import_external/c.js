adee7e7846e21c7396116fc4c6556ff37cfbbb2b
underfin <likui6666666@gmail.com>
2023-09-21 21:21:23 +0800

    chore: add test

--- crates/rolldown/tests/fixtures/basic_import_external/c.js
+++
@@ -1,6 +0,0 @@
-import * as ext from 'external'
-import { a, b } from 'external'
-console.log(ext, a, b)
-
-export { a as a1,b as b1 } from 'external'
-export * as ext1 from 'external'
---
+++ crates/rolldown/tests/fixtures/export_external/foo.js
@@ -0,0 +1,3 @@
+export { a as a1, b as b1 } from 'external'
+export * as ext1 from 'external'
+

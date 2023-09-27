c5bd419dc04b261ae448b0215019ed1dd27bce74
underfin <likui6666666@gmail.com>
2023-09-27 20:28:06 +0800

    chore: fix ci root issue

--- crates/bench/src/lib.rs
+++ crates/bench/src/lib.rs
@@ -24,6 +24,10 @@ pub async fn run_fixture(fixture_path: PathBuf) {
 }
 
 pub fn normalized_fixture_path(path: &str) -> PathBuf {
-  let project_root = std::env::current_dir().unwrap();
+  let project_root = if std::env::var("CI").is_ok() {
+    std::env::current_dir().unwrap().join("crates/bench")
+  } else {
+    std::env::current_dir().unwrap()
+  };
   project_root.join(path)
 }

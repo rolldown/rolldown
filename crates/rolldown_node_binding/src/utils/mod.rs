46a407b5d73e8ca1c2142ceadebda60c1395eb90
underfin <likui6666666@gmail.com>
2023-10-12 22:20:20 +0800

    chore: enable trace with env TRACE=1

--- crates/rolldown_node_binding/src/utils/mod.rs
+++ crates/rolldown_node_binding/src/utils/mod.rs
@@ -1,7 +1,7 @@
 pub mod napi_error_ext;
-use std::sync::atomic::AtomicBool;
-
 use napi::Env;
+use std::env;
+use std::sync::atomic::AtomicBool;
 mod into_js_unknown_vec;
 pub use into_js_unknown_vec::*;
 mod js_callback;
@@ -10,11 +10,12 @@ use rolldown_tracing::enable_tracing_on_demand;
 
 static IS_ENABLE_TRACING: AtomicBool = AtomicBool::new(false);
 
-pub fn init_custom_trace_subscriber(mut env: Env) {
-  if !IS_ENABLE_TRACING.swap(true, std::sync::atomic::Ordering::SeqCst) {
+pub fn init_custom_trace_subscriber(mut napi_env: Env) {
+  let value = env::var("TRACE").unwrap_or_default();
+  if value == "1" && !IS_ENABLE_TRACING.swap(true, std::sync::atomic::Ordering::SeqCst) {
     let guard = enable_tracing_on_demand();
     if let Some(guard) = guard {
-      env
+      napi_env
         .add_env_cleanup_hook(guard, |flush_guard| {
           flush_guard.flush();
           drop(flush_guard);

15f671f453a06af2eef373febfb5a89254e6c358
likui <2218301630@qq.com>
2023-11-02 21:16:45 +0800

    chore: enable more test

--- crates/rolldown/tests/esbuild/default/.import_then_catch/entry.js
+++
@@ -1,3 +0,0 @@
-import(name).then(pass, fail)
-import(name).then(pass).catch(fail)
-import(name).catch(fail)
--- crates/rolldown/tests/esbuild/default/var_relocating_bundle/let.js
+++ crates/rolldown/tests/esbuild/default/var_relocating_bundle/let.js
@@ -1,7 +1,7 @@
 if (true) {
 	let a;
 	for (let b; 0;);
-	for (let { c, x: [d] } = {}; 0;);
+	// for (let { c, x: [d] } = {}; 0;);
 	for (let e of []);
 	for (let { f, x: [g] } of []);
 	for (let h in {});

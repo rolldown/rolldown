2335f31e6cf010ce4664a039af94b80ad316f7ab
houyunlu <1311450681@qq.com>
2023-11-06 20:52:08 +0800

    chore:recover

--- crates/rolldown/examples/basic.rs
+++ crates/rolldown/examples/basic.rs
@@ -1,21 +1,24 @@
+use std::{path::PathBuf, sync::Arc};
+
 use rolldown::{Bundler, InputItem, InputOptions};
-use rolldown_fs::FileSystemVfs;
+use rolldown_fs::FileSystemOs;
+use sugar_path::SugarPathBuf;
 
 #[tokio::main]
 async fn main() {
-  // let root = PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap());
-  // let cwd = root.join("./examples").into_normalize();
-  let fs = FileSystemVfs::new(&[("/index.js", "const a = 3000000")]);
+  let root = PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap());
+  let cwd = root.join("./examples").into_normalize();
   let mut bundler = Bundler::new(
     InputOptions {
       input: Some(vec![InputItem {
         name: Some("basic".to_string()),
         import: "./index.js".to_string(),
       }]),
-      cwd: Some("/".into()),
+      cwd: Some(cwd),
     },
-    fs,
+    FileSystemOs,
   );
 
-  let _outputs = bundler.write(Default::default()).await.unwrap();
+  let outputs = bundler.write(Default::default()).await.unwrap();
+  println!("{outputs:#?}");
 }

51f1be1c0c9c02c3416f18bbe493f644ccd265fe
KEYs <1311450681@qq.com>
2023-11-09 22:38:50 +0800

    fix: async_scope_block_on (#210)

--- crates/rolldown_utils/src/lib.rs
+++ crates/rolldown_utils/src/lib.rs
@@ -3,3 +3,30 @@ mod magic_string_ext;
 pub mod reserved_word;
 
 pub use crate::magic_string_ext::MagicStringExt;
+use std::future::Future;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn block_on_spawn_all<Iter, Out>(iter: Iter) -> Vec<Out>
+where
+  Iter: Iterator,
+  Out: Send + 'static,
+  Iter::Item: Future<Output = Out> + Send,
+{
+  use async_scoped::TokioScope;
+  let (_ret, collections) =
+    async_scoped::Scope::scope_and_block(|scope: &mut TokioScope<'_, _>| {
+      iter.into_iter().for_each(|fut| scope.spawn(fut));
+    });
+  collections.into_iter().map(Result::unwrap).collect()
+}
+
+#[cfg(target_arch = "wasm32")]
+pub fn block_on_spawn_all<Iter, Out>(iter: Iter) -> Vec<Out>
+where
+  Iter: Iterator,
+  Out: Send + 'static,
+  Iter::Item: Future<Output = Out> + Send,
+{
+  use futures::{executor::block_on, future};
+  block_on(future::join_all(iter))
+}

5e2411b3ed5bfd0597c8d70c20e7ed5c8a47e04f
underfin <2218301630@qq.com>
2023-11-09 14:03:37 +0800

    chore: fix lint

--- crates/rolldown/src/bundler/mod.rs
+++ crates/rolldown/src/bundler/mod.rs
@@ -1,14 +1,14 @@
 pub mod bundle;
+#[allow(clippy::module_inception)]
+pub mod bundler;
+mod chunk;
 mod chunk_graph;
 mod graph;
 mod module;
+mod module_loader;
 pub mod options;
+pub mod plugin_driver;
 mod renderer;
 mod runtime;
 pub mod utils;
 mod visitors;
-pub mod plugin_driver;
-#[allow(clippy::module_inception)]
-pub mod bundler;
-mod chunk;
-mod module_loader;

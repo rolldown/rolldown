54cfc21a7a5811f61d5be7c5706dfe1435836001
Yunfei He <i.heyunfei@gmail.com>
2023-11-09 12:23:37 +0800

    test(deconflict): add case of complex params  patterns

---
+++ crates/rolldown/tests/fixtures/deconflict/complex_params_patterns/main.js
@@ -0,0 +1,23 @@
+import assert from 'assert'
+import {
+    a as a2,
+    b as b2,
+    c as c2,
+    d as d2,
+    e as e2,
+} from './names.js'
+
+export const [a, b, c, d, e] = ['a', 'b', 'c', 'd', 'e']
+
+function foo(a$1, { b$1 }, { c$1 = 'c$1' }, { d$1 } = { d$1: 'd$1' }, { e$1 = '' } = { e$1: 'e$1' }) {
+    return {
+        names: [a2, b2, c2, d2, e2],
+        main: [a, b, c, d, e],
+        params: [a$1, b$1, c$1, d$1, e$1],
+    }
+}
+
+const ret = foo('a$1', { b$1: 'b$1' }, {}, undefined, undefined)
+assert.deepStrictEqual(ret.main, ['a', 'b', 'c', 'd', 'e'])
+assert.deepStrictEqual(ret.names, ['a2', 'b2', 'c2', 'd2', 'e2'])
+assert.deepStrictEqual(ret.params, ['a$1', 'b$1', 'c$1', 'd$1', 'e$1'])
---
+++ crates/rolldown/tests/fixtures/deconflict/complex_params_patterns/names.js
@@ -0,0 +1,1 @@
+export const [a, b, c, d, e] = ['a2', 'b2', 'c2', 'd2', 'e2']

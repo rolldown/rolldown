5849639efa0c90c9caf9c4e174aaec79a2cc52eb
houyunlu <1311450681@qq.com>
2023-11-12 01:21:29 +0800

    fix: fmt

--- web/playground/vite.config.js
+++ web/playground/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
-import topLevelAwait from "vite-plugin-top-level-await";
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
-	plugins: [
-		topLevelAwait({
-			// The export name of top-level await promise for each chunk module
-			promiseExportName: "__tla",
-			// The function to generate import names of top-level await promise in each chunk module
-			promiseImportName: (i) => `__tla_${i}`,
-		}),
-	],
-});
+  plugins: [
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
+})

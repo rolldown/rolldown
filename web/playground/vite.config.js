ac60fdf03ec566e2b219e7c06dcfce85a66588ea
KEYs <1311450681@qq.com>
2023-11-12 01:29:24 +0800

    fix: top level await fix compile error (#226)
    * fix: top level await fix compile error
    
    * fix: fmt

--- web/playground/vite.config.js
+++ web/playground/vite.config.js
@@ -1,4 +1,14 @@
 import { defineConfig } from 'vite'
-import * as path from 'path'
 
-export default defineConfig({})
+import topLevelAwait from 'vite-plugin-top-level-await'
+
+export default defineConfig({
+  plugins: [
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
+})

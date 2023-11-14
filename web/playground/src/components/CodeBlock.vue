10ae130664ae5e51aaadcb609207667247cd01f6
houyunlu <1311450681@qq.com>
2023-11-14 21:49:48 +0800

    feat: output code block should be readonly

--- web/playground/src/components/CodeBlock.vue
+++ web/playground/src/components/CodeBlock.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ShallowRef, defineComponent, ref, shallowRef } from 'vue'
+import { ShallowRef, shallowRef } from 'vue'
 import { Codemirror } from 'vue-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { oneDark } from '@codemirror/theme-one-dark'
@@ -12,14 +12,19 @@ type Payload = {
   container: HTMLElement
 }
 
-defineProps({
+let props = defineProps({
   code: String,
+  readonly: {
+    type: Boolean,
+    required: false
+  }
 })
-
-const emit = defineEmits(['code'])
 
 const extensions = [javascript(), oneDark]
 
+if (props.readonly) {
+  extensions.push(EditorView.editable.of(false))
+}
 // Codemirror EditorView instance ref
 const view = <ShallowRef<EditorView>>shallowRef()
 const handleReady = (payload: Payload) => {
@@ -43,24 +48,8 @@ const handleReady = (payload: Payload) => {
 //     lines
 //   }
 // }
-
-const handleCodeChange = (e: string) => {
-  emit('code', e)
-}
-const log = console.log
 </script>
 <template>
-  <codemirror
-    :model-value="code"
-    placeholder="Code goes here..."
-    :style="{ height: '400px' }"
-    :autofocus="false"
-    :indent-with-tab="true"
-    :tab-size="2"
-    :extensions="extensions"
-    @ready="handleReady"
-    @change="handleCodeChange"
-    @focus="log('focus', $event)"
-    @blur="console.log('blur', $event)"
-  />
+  <codemirror :model-value="code" placeholder="Code goes here..." :style="{ height: '400px' }" :autofocus="false"
+    :indent-with-tab="true" :tab-size="2" :extensions="extensions" @ready="handleReady" @change="$emit('code', $event)" />
 </template>

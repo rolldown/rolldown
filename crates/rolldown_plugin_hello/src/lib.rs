246693f26d2f5a081186dae3c7b70237a5924038
Yunfei He <i.heyunfei@gmail.com>
2023-11-15 08:22:56 -0600

    feat: add native plugin example (#262)

---
+++ crates/rolldown_plugin_hello/src/lib.rs
@@ -0,0 +1,19 @@
+use std::borrow::Cow;
+
+use rolldown::{HookNoopReturn, Plugin, PluginContext};
+
+#[derive(Debug)]
+pub struct HelloPlugin;
+
+#[async_trait::async_trait]
+impl Plugin for HelloPlugin {
+  fn name(&self) -> Cow<'static, str> {
+    "hello".into()
+  }
+
+  #[allow(clippy::print_stdout)]
+  async fn build_start(&self, _ctx: &mut PluginContext) -> HookNoopReturn {
+    println!("hello");
+    Ok(())
+  }
+}

68c0dee17813b390da3496390eec47ae507b7926
underfin <2218301630@qq.com>
2023-11-15 15:31:25 +0800

    feat: export some types

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,3 +1,14 @@
-export type { InputOptions } from './options/input-options'
-export type { OutputOptions } from './options/output-options'
+import type { InputOptions } from './options/input-options'
+import type { OutputOptions } from './options/output-options'
 export { rolldown } from './rolldown'
+
+interface RollupOptions extends InputOptions {
+	// This is included for compatibility with config files but ignored by rollup.rollup
+	output?: OutputOptions | OutputOptions[];
+}
+
+// export types from rolldown
+export type { RollupOptions, InputOptions, OutputOptions }
+
+// export types from rollup
+export type { RollupOutput, Plugin } from 'rollup'

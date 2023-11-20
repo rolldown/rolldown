971b0ecf2ede264b4a458d49be43f5d63d63137d
Yunfei <i.heyunfei@gmail.com>
2023-11-20 16:56:59 +0800

    chore: mark unstable things as experimental

--- packages/node/src/rolldown.ts
+++ packages/node/src/rolldown.ts
@@ -6,7 +6,11 @@ export const rolldown = (input: InputOptions): Promise<RolldownBuild> => {
   return RolldownBuild.fromInputOptions(input)
 }
 
-// It is only for scan module graph.
-export const scan = (input: InputOptions): Promise<void> => {
+/**
+ * @description
+ * This is a experimental API. It's behavior may change in the future.
+ * Calling this API will only execute the scan stage of rolldown.
+ */
+export const experimental_scan = (input: InputOptions): Promise<void> => {
   return RolldownBuild.fromInputOptionsForScanStage(input)
 }

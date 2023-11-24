6c8f6f76de35bea62ae640de9bffedf31bc47276
underfin <2218301630@qq.com>
2023-11-24 15:19:52 +0800

    fix: deconflict export named declartion

---
+++ crates/rolldown/tests/fixtures/deconflict/wrapped_esm_export_named_function/main.js
@@ -0,0 +1,10 @@
+import assert from 'assert'
+import bar from './bar'
+
+const a = 2; // make foo `a` conflict
+
+const { foo } = bar
+
+assert.strictEqual(typeof foo, 'function')
+
+require('./foo')

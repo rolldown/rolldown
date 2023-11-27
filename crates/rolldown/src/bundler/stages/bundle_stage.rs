13a05b9649f10be31df5b09fb7f1961e7f90ef0c
underfin <2218301630@qq.com>
2023-11-27 14:20:58 +0800

    fix: clean up render exports from external module

--- crates/rolldown/src/bundler/stages/bundle_stage.rs
+++ crates/rolldown/src/bundler/stages/bundle_stage.rs
@@ -158,9 +158,7 @@ impl<'a> BundleStage<'a> {
               }
             }
           }
-          Module::External(_) => {
-            // TODO: process external module
-          }
+          Module::External(_) => {}
         }
       }
 

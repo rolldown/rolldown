ba07d9e36264c6aad9a446452bb62ffca0a87c23
underfin <2218301630@qq.com>
2024-01-09 10:02:08 +0800

    chore: rebase to main

--- crates/rolldown/src/bundler/stages/bundle_stage.rs
+++ crates/rolldown/src/bundler/stages/bundle_stage.rs
@@ -19,17 +19,16 @@ use crate::{
 };
 use index_vec::{index_vec, IndexVec};
 use rolldown_common::{EntryPointKind, ExportsKind, ImportKind, ModuleId, NamedImport, SymbolRef};
-use rolldown_fs::FileSystem;
 use rustc_hash::{FxHashMap, FxHashSet};
 
-pub struct BundleStage<'a, T: FileSystem + Default> {
+pub struct BundleStage<'a> {
   link_output: &'a mut LinkStageOutput,
   output_options: &'a OutputOptions,
   input_options: &'a InputOptions,
   plugin_driver: &'a SharedPluginDriver,
 }
 
-impl<'a, T: FileSystem + Default + 'static> BundleStage<'a, T> {
+impl<'a> BundleStage<'a> {
   pub fn new(
     link_output: &'a mut LinkStageOutput,
     input_options: &'a InputOptions,

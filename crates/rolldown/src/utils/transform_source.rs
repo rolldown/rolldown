794e87b1d0a98cebc5165dd6518cca2686326034
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-03-09 01:24:31 +0800

    fix: correct id in transform hook

--- crates/rolldown/src/utils/transform_source.rs
+++ crates/rolldown/src/utils/transform_source.rs
@@ -1,3 +1,4 @@
+use rolldown_common::ResolvedPath;
 use rolldown_plugin::HookTransformArgs;
 use rolldown_sourcemap::SourceMap;
 
@@ -5,11 +6,12 @@ use crate::{error::BatchedErrors, plugin_driver::PluginDriver};
 
 pub async fn transform_source(
   plugin_driver: &PluginDriver,
+  resolved_path: &ResolvedPath,
   source: String,
   sourcemap_chain: &mut Vec<SourceMap>,
 ) -> Result<String, BatchedErrors> {
   let (code, map_chain) =
-    plugin_driver.transform(&HookTransformArgs { id: source.as_ref(), code: &source }).await?;
+    plugin_driver.transform(&HookTransformArgs { id: &resolved_path.path, code: &source }).await?;
 
   sourcemap_chain.extend(map_chain);
 
--- packages/node/test/cases/plugin/transform/config.ts
+++ packages/node/test/cases/plugin/transform/config.ts
@@ -7,13 +7,13 @@ const config: RollupOptions = {
   plugins: [
     {
       name: 'test-plugin',
-      transform: function (id, code) {
+      transform: function (code, id) {
         transformFn()
         if (id.endsWith('foo.js')) {
           expect(code).toStrictEqual('')
           return {
-            code: `console.log('foo')`,
-            map: { mappings: "" }
+            code: `console.log('transformed')`,
+            map: { mappings: '' },
           }
         }
       },
@@ -24,6 +24,8 @@ const config: RollupOptions = {
 export default {
   config,
   afterTest: (output: RollupOutput) => {
+    expect.assertions(3)
     expect(transformFn).toHaveBeenCalledTimes(2)
+    expect(output.output[0].code).contains('transformed')
   },
 }

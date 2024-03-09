f81f87dc4a67f18b8de26f3084d20e003aec3d68
Kirk Lin <linkirk@163.com>
2024-03-09 20:08:45 +0800

    chore: fix duplicate entries (#502)
    * chore: fix typo
    
    * chore: fix typo

--- packages/node/src/rolldown.ts
+++ packages/node/src/rolldown.ts
@@ -2,7 +2,7 @@ import { InputOptions } from './options/input-options'
 import { RolldownBuild } from './rolldown-build'
 import { createBundler } from './utils'
 
-// Compat to `rollup.rollup`, it is include scan module graph and linker.
+// Compat to `rollup.rollup`, it is included scan module graph and linker.
 export const rolldown = async (input: InputOptions): Promise<RolldownBuild> => {
   const bundler = await createBundler(input)
   return new RolldownBuild(bundler)
@@ -10,7 +10,7 @@ export const rolldown = async (input: InputOptions): Promise<RolldownBuild> => {
 
 /**
  * @description
- * This is a experimental API. It's behavior may change in the future.
+ * This is an experimental API. It's behavior may change in the future.
  * Calling this API will only execute the scan stage of rolldown.
  */
 export const experimental_scan = async (input: InputOptions): Promise<void> => {

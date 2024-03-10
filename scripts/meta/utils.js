cc1d754d527b7161c0b1d9647153317a531b06c4
Yunfei <i.heyunfei@gmail.com>
2024-03-10 21:12:06 +0800

    chore: rewrite rust benchmark setup using node

---
+++ scripts/meta/utils.js
@@ -0,0 +1,7 @@
+import * as nodePath from 'node:path'
+import * as nodeAssert from 'node:assert'
+import { repoRoot } from './constants.js'
+
+export function assertRunningScriptFromRepoRoot() {
+  nodeAssert.equal(nodePath.normalize(process.cwd()), nodePath.normalize(repoRoot), 'The script must be run from the root of the repo');
+}
---
+++ scripts/setup-rust-benchmark-input.js
@@ -0,0 +1,27 @@
+import 'zx/globals'
+import { assertRunningScriptFromRepoRoot } from './meta/utils.js'
+import fsExtra from 'fs-extra'
+import assert from 'assert'
+
+assertRunningScriptFromRepoRoot()
+
+await $`git clone --branch r108 --depth 1 https://github.com/mrdoob/three.js.git ./temp/three`
+
+// setup three-js
+
+fsExtra.writeFileSync('./temp/three/entry.js', `import * as three from './src/Three.js'; export { three }`)
+
+
+// setup three-js 10x
+
+fsExtra.ensureDirSync('./temp/three10x')
+
+for (let i = 1; i <= 10; i++) {
+  fsExtra.ensureDirSync(`./temp/three10x/copy${i}`)
+  fsExtra.copySync('./temp/three/src', `./temp/three10x/copy${i}/`)
+}
+
+fsExtra.writeFileSync('./temp/three10x/entry.js', '')
+for (let i = 1; i <= 10; i++) {
+  fsExtra.appendFileSync('./temp/three10x/entry.js', `import * as three${i} from './copy${i}/Three.js'; export { three${i} }\n`)
+}

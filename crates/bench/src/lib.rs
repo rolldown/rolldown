ab733b78de910f1ccfe8a9c06b54584fdccd5dea
Yunfei <i.heyunfei@gmail.com>
2024-03-10 22:17:32 +0800

    ci: fix codespeed benchmark again

--- crates/bench/src/lib.rs
+++ crates/bench/src/lib.rs
@@ -3,8 +3,20 @@ use std::path::PathBuf;
 use rolldown::{Bundler, InputItem, InputOptions, OutputOptions};
 
 pub fn repo_root() -> PathBuf {
-  let project_root = PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap());
-  project_root.parent().unwrap().parent().unwrap().to_path_buf()
+  let cargo_manifest_dir = std::env::var("CARGO_MANIFEST_DIR").map(PathBuf::from);
+  let project_root = if let Ok(cargo_manifest_dir) = cargo_manifest_dir {
+    cargo_manifest_dir.parent().unwrap().parent().unwrap().to_path_buf()
+  } else {
+    std::env::current_dir().expect("failed to get current dir")
+  };
+
+  assert_eq!(
+    project_root.file_name().unwrap(),
+    "rolldown",
+    "Benchmark must be run from the root of the repo, got wrong `project_root` {}",
+    project_root.display()
+  );
+  project_root
 }
 
 pub async fn run_fixture(fixture_path: PathBuf) {

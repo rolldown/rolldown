10e19ebb2a85d841b6dff3ddfdfb150dcbeca3b9
erkelost <1256029807@qq.com>
2024-03-11 13:58:13 +0800

    chore: optimize transformSourcemap code robustness

--- packages/node/src/utils/transform-sourcemap.ts
+++ packages/node/src/utils/transform-sourcemap.ts
@@ -1,31 +1,31 @@
 import type { SourceMap } from '@rolldown/node-binding'
-
 import type { SourceMapInput } from '../rollup-types'
+import type { ExistingRawSourceMap } from '../rollup'
 
-export function transformSourcemap(
-  value?: SourceMapInput,
-): SourceMap | undefined {
-  if (value === undefined || value === null) {
-    return
-  }
+export function transformSourcemap(value?: SourceMapInput): SourceMap | undefined {
+  if (!value) return undefined
+
   if (typeof value === 'string') {
-    return JSON.parse(value) as SourceMap
-  }
-  if (typeof value === 'object') {
-    return {
-      // TODO file, version, x_google_ignoreList
-      mappings: value.mappings,
-      names: 'names' in value ? value.names : [],
-      sourceRoot: 'sourceRoot' in value ? value.sourceRoot : '',
-      sources: 'sources' in value ? value.sources : [],
-      sourcesContent:
-        'sourcesContent' in value
-          ? value.sourcesContent
-            ? (value.sourcesContent.filter(
-                (v) => typeof v === 'string',
-              ) as string[])
-            : []
-          : [],
+    try {
+      return JSON.parse(value) as SourceMap
+    } catch (error) {
+      console.error('Error parsing source map:', error)
+      return undefined
     }
   }
+
+  if (typeof value === 'object') {
+    const { mappings, names = [], sourceRoot = '', sources = [], sourcesContent = [] } = value as ExistingRawSourceMap
+
+    return {
+      // TODO file, version, x_google_ignoreList
+      mappings,
+      names,
+      sourceRoot,
+      sources,
+      sourcesContent: sourcesContent.filter((v): v is string => typeof v === 'string'),
+    }
+  }
+
+  return undefined
 }

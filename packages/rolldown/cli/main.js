d7a30819c34ff8ea9db11e1d565beb554473eac8
kazuya kawaguchi <kawakazu80@gmail.com>
2024-03-20 17:36:27 +0900

    fix: build implementation and test fixtures

--- packages/rolldown/cli/main.js
+++ packages/rolldown/cli/main.js
@@ -1,10 +1,14 @@
 import { defineCommand, runMain, showUsage } from 'citty'
-import { colors } from 'consola/utils'
+import consola from 'consola'
+import process from 'node:process'
+import path from 'node:path'
 import pkgJson from '../package.json' assert { type: 'json' }
+import { normalizeConfigPath, loadConfig } from './config.js'
+import build from './build.js'
 
 /**
  * NOTE:
- *  currenctly, It's hard to customize usage with citty `renderUsage`.
+ *  currently, It's hard to customize usage with citty `renderUsage`.
  *  It may be better to use another CLI library or construct our own.
  */
 
@@ -17,6 +21,7 @@ const main = defineCommand({
   args: {
     config: {
       type: 'string',
+      alias: 'c',
       description:
         'Use this config file (if argument is used but value is unspecified, defaults to rolldown.config.js)',
     },
@@ -26,6 +31,15 @@ const main = defineCommand({
       await showUsage(ctx.cmd)
       return
     }
+
+    const currentDir = path.resolve(process.cwd(), '.')
+    const configPath = normalizeConfigPath(
+      path.resolve(currentDir, ctx.args.config),
+    )
+    const { default: config } = loadConfig(configPath)
+    consola.debug('loaded config', config)
+
+    await build(config)
   },
 })
 
---
+++ packages/rolldown/test/fixtures/basic/src/index.js
@@ -0,0 +1,7 @@
+import { add, sub } from './calc'
+import { say } from 'modules/say'
+
+console.log(add(1, 2)) // 3
+console.log(sub(1, 2)) // -1
+
+say('hello world!')

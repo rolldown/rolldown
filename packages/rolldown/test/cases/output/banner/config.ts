eb28aec1409f1863757e5ed6f605ef490296439d
PengBoUESTC <pengbotju@163.com>
2024-03-22 19:26:43 +0800

    chore: add test

--- packages/rolldown/test/cases/output/banner/config.ts
+++ packages/rolldown/test/cases/output/banner/config.ts
@@ -1,12 +1,22 @@
-import type { RollupOptions } from '../../../../src'
+import type { RollupOptions, RolldownOutput, RolldownOutputChunk } from '../../../../src'
+import { expect } from 'vitest'
+
+const banner = '// banner test\n'
 
 const config: RollupOptions = {
   external: [/external/, 'external-a'],
   output: {
-    banner: '// banner test\n',
+    banner,
   },
 }
 
 export default {
   config,
+  afterTest: (output: RolldownOutput) => {
+    expect(
+      output.output
+        .filter(({ type }) => type === 'chunk')
+        .every((chunk) => (chunk as RolldownOutputChunk).code.startsWith(banner)),
+    ).toBe(true)
+  },
 }

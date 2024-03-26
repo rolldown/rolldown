706f44dbfe146514df01756f85b5683ba1ed066d
Yunfei He <i.heyunfei@gmail.com>
2024-03-27 06:17:30 +0800

    chore(rust): improve comments of `JsAsyncCallback` (#671)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info about the "what" this PR is solving -->
    
    ### Test Plan
    
    <!-- e.g. is there anything you'd like reviewers to focus on? -->
    
    ---

--- crates/rolldown_binding/src/types/js_async_callback.rs
+++ crates/rolldown_binding/src/types/js_async_callback.rs
@@ -4,6 +4,9 @@ use napi::{
 };
 
 /// This represents a JavaScript function, whose return value is `Promise<T> | T`
+///
+/// For example, the equivalent type of `(a: string, b: number) => Promise<number | undefined> | number | undefined` in Rust is
+/// `JsAsyncCallback<(String, i32), Option<i32>>`
 pub type JsAsyncCallback<Args, Ret> = ThreadsafeFunction<Args, JsAsyncCallbackReturn<Ret>, false>;
 
 // Explicitly using `UnknownReturnValue` in `Either3<Promise<T>, T, UnknownReturnValue>` to get control in rust while

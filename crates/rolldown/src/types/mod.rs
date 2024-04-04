8e1c8f8b5677759f07694bda4816d83848de94a4
underfin <likui.underfin@gmail.com>
2024-04-04 16:58:53 +0800

    refactor: render normal module (#746)

--- crates/rolldown/src/types/mod.rs
+++ crates/rolldown/src/types/mod.rs
@@ -6,7 +6,7 @@ pub mod ast_symbols;
 pub mod bundler_fs;
 pub mod linking_metadata;
 pub mod match_import_kind;
-pub mod module_render_context;
+pub mod module_render_output;
 pub mod module_table;
 pub mod namespace_alias;
 pub mod normal_module_builder;

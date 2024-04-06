f7b1d94c2eb301e846a3758ef2196d127ff5fe9f
Yunfei <i.heyunfei@gmail.com>
2024-04-06 17:47:09 +0800

    chore(rust/tracing): use env variable to fully control tracing output

--- crates/rolldown_tracing/src/lib.rs
+++ crates/rolldown_tracing/src/lib.rs
@@ -2,11 +2,11 @@
 /// - By default, only allow tracing events from crates of this repo.
 /// - Using `LOG_OUTPUT=chrome` to collect tracing events into a json file.
 ///   - This only works on using `@rolldown/node`. If you are running rolldown in rust, this doesn't works.
-/// - Using `LOG=TRACE` to enable tracing or other values for more specific tracing.
+/// - Using `RUST_LOG=TRACE` to enable tracing or other values for more specific tracing.
 ///   - See  https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html#example-syntax for more syntax details.
+///   - https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html#directives
 use std::sync::atomic::AtomicBool;
 
-use tracing::{level_filters::LevelFilter, Level};
 use tracing_chrome::FlushGuard;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::{fmt, prelude::*};
@@ -14,32 +14,25 @@ use tracing_subscriber::{fmt, prelude::*};
 static IS_INITIALIZED: AtomicBool = AtomicBool::new(false);
 
 pub fn try_init_tracing() {
-  if std::env::var("LOG").is_err() {
+  if std::env::var("RUST_LOG").is_err() {
     // tracing will slow down the bundling process, so we only enable it when `LOG` is set.
     return;
   }
-  if !IS_INITIALIZED.swap(true, std::sync::atomic::Ordering::SeqCst) {
-    tracing_subscriber::registry()
-      .with(
-        tracing_subscriber::filter::Targets::new()
-          .with_targets(vec![("rolldown", Level::TRACE), ("rolldown_binding", Level::TRACE)]),
-      )
-      .with(
-        EnvFilter::builder()
-          .with_env_var("LOG")
-          .with_default_directive(LevelFilter::TRACE.into())
-          .from_env_lossy(),
-      )
-      .with(fmt::layer().pretty().without_time())
-      .init();
-    tracing::trace!("Tracing is initialized.");
+  if IS_INITIALIZED.swap(true, std::sync::atomic::Ordering::SeqCst) {
+    return;
   }
+
+  tracing_subscriber::registry()
+    .with(EnvFilter::from_default_env())
+    .with(fmt::layer().pretty().without_time())
+    .init();
+  tracing::trace!("Tracing is initialized.");
 }
 
 pub fn try_init_tracing_with_chrome_layer() -> Option<FlushGuard> {
   use tracing_chrome::ChromeLayerBuilder;
   use tracing_subscriber::prelude::*;
-  if std::env::var("LOG").is_err() {
+  if std::env::var("RUST_LOG").is_err() {
     // tracing will slow down the bundling process, so we only enable it when `LOG` is set.
     return None;
   }
@@ -47,19 +40,7 @@ pub fn try_init_tracing_with_chrome_layer() -> Option<FlushGuard> {
     None
   } else {
     let (chrome_layer, guard) = ChromeLayerBuilder::new().build();
-    tracing_subscriber::registry()
-      .with(
-        tracing_subscriber::filter::Targets::new()
-          .with_targets(vec![("rolldown", Level::TRACE), ("rolldown_binding", Level::TRACE)]),
-      )
-      .with(
-        EnvFilter::builder()
-          .with_env_var("LOG")
-          .with_default_directive(LevelFilter::TRACE.into())
-          .from_env_lossy(),
-      )
-      .with(chrome_layer)
-      .init();
+    tracing_subscriber::registry().with(EnvFilter::from_default_env()).with(chrome_layer).init();
     Some(guard)
   }
 }

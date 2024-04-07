8ee943516712aff919288e9cd1a40a66c3b2b270
PengBoUESTC <pengbotju@163.com>
2024-04-07 20:20:43 +0800

    chore: run roll

--- packages/rolldown/tests/fixtures/output/fileNames/_config.ts
+++ packages/rolldown/tests/fixtures/output/fileNames/_config.ts
@@ -5,7 +5,7 @@ import { expect } from 'vitest'
 export default defineTest({
   config: {
     output: {
-      entryFileNames: '[name]test.js',
+      entryFileNames: '[name]-test.js',
       chunkFileNames: '[name]-chunk.js',
     },
   },
@@ -13,7 +13,7 @@ export default defineTest({
     expect(
       output.output.find((chunk) => (chunk as RolldownOutputChunk).isEntry)
         ?.fileName,
-    ).toBe('maintest.js')
+    ).toBe('main-test.js')
     expect(
       output.output.find((chunk) => !(chunk as RolldownOutputChunk).isEntry)
         ?.fileName,

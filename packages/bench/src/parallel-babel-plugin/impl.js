eb669e9bf487af9821a271ddf5d62f628c4b571f
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-04-10 02:15:31 +0900

    chore: try sucrase

--- packages/bench/src/parallel-babel-plugin/impl.js
+++ packages/bench/src/parallel-babel-plugin/impl.js
@@ -1,28 +1,22 @@
 import { defineThreadSafePluginImplementation } from 'rolldown/thread-safe-plugin'
-import babel from '@babel/core'
+import { transform } from 'sucrase'
 import nodePath from 'node:path'
 
 /** @returns {import('rolldown').Plugin} */
 export const babelPlugin = () => {
   return {
     name: 'parallel-babel-plugin',
-    async transform(code, id) {
+    transform(code, id) {
       const ext = nodePath.extname(id)
       if (ext === '.ts' || ext === '.tsx') {
-        const ret = /** @type {babel.BabelFileResult} */ (
-          await babel.transformAsync(code, {
-            filename: id,
-            presets: [
-              ['@babel/preset-env', { bugfixes: true }],
-              '@babel/preset-typescript',
-            ],
-            targets: 'chrome >= 80',
-            sourceMaps: true,
-            configFile: false,
-            browserslistConfigFile: false,
-          })
-        )
-        return { code: /** @type {string} */ (ret.code) }
+        /** @type {import('sucrase').Transform[]} */
+        const transforms = ['typescript']
+        if (ext === '.tsx') {
+          transforms.push('jsx')
+        }
+
+        const ret = transform(code, { filePath: id, transforms })
+        return { code: ret.code }
       }
     },
   }

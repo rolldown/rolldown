03b48ff179ba1a689c3c06859422454fe927bfb5
Yunfei <i.heyunfei@gmail.com>
2024-04-26 01:47:10 +0800

    chore(rust/testing_config): avoid unnecessary re-runs for `build.rs`

--- crates/rolldown_testing/build.rs
+++ crates/rolldown_testing/build.rs
@@ -6,6 +6,9 @@ use std::path::PathBuf;
 use rolldown_testing_config::TestConfig;
 
 fn main() {
+  // If the definition of `TestConfig` changes, this build script will automatically re-run due we rely on `rolldown_testing_config` in `Cargo.toml`.
+  // So we only add `build.rs` as the dependency to prevent unnecessary re-runs for every `cargo build`
+  println!("cargo:rerun-if-changed=build.rs");
   let schema = schema_for!(TestConfig);
   let scheme_path =
     PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").expect("Should have CARGO_MANIFEST_DIR"))

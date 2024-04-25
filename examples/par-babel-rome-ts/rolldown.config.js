7437119be8174208de026672e85ede1be0abf3fd
Yunfei He <i.heyunfei@gmail.com>
2024-04-26 01:53:29 +0800

    chore(exmaples): add `par-babel-rome-ts` (#978)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info about the "what" this PR is solving -->

---
+++ examples/par-babel-rome-ts/rolldown.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'rolldown'
+import { builtinModules } from 'node:module'
+import nodePath from 'node:path'
+import nodeUrl from 'node:url'
+import { default as parallelBabelPluginSync } from './parallel-babel-plugin/index.js'
+
+const dirname = nodePath.dirname(nodeUrl.fileURLToPath(import.meta.url))
+
+export const REPO_ROOT = nodePath.resolve(dirname, '../..')
+
+export default defineConfig({
+  logLevel: 'silent',
+  input: {
+    rome: nodePath.join(REPO_ROOT, './tmp/bench/rome/src/entry.ts'),
+  },
+  external: builtinModules,
+  // Need this due rome is not written with `isolatedModules: true`
+  shimMissingExports: true,
+  plugins: [parallelBabelPluginSync()],
+  resolve: {
+    extensions: ['.ts'],
+    tsconfigFilename: nodePath.join(
+      REPO_ROOT,
+      './tmp/bench/rome/src/tsconfig.json',
+    ),
+  },
+})

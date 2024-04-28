76f912b6db1b65a46708b1f3fa3e8c139a558527
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-04-28 17:24:40 +0900

    wip: code with compile errors

--- crates/rolldown_binding/src/options/plugin/binding_plugin_options.rs
+++ crates/rolldown_binding/src/options/plugin/binding_plugin_options.rs
@@ -28,7 +28,7 @@ pub struct BindingPluginOptions<const WEAK: bool = false> {
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "(ctx: BindingPluginContext) => MaybePromise<VoidNullable>")]
-  pub build_start: Option<MaybeAsyncJsCallback<BindingPluginContext, (), false>>,
+  pub build_start: Option<MaybeAsyncJsCallback<BindingPluginContext, (), WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(
@@ -38,52 +38,52 @@ pub struct BindingPluginOptions<const WEAK: bool = false> {
     MaybeAsyncJsCallback<
       (String, Option<String>, BindingHookResolveIdExtraOptions),
       Option<BindingHookResolveIdOutput>,
-      false,
+      WEAK,
     >,
   >,
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "(id: string) => MaybePromise<VoidNullable<BindingHookLoadOutput>>")]
-  pub load: Option<MaybeAsyncJsCallback<String, Option<BindingHookLoadOutput>, false>>,
+  pub load: Option<MaybeAsyncJsCallback<String, Option<BindingHookLoadOutput>, WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(
     ts_type = "(id: string, code: string) => MaybePromise<VoidNullable<BindingHookLoadOutput>>"
   )]
-  pub transform: Option<MaybeAsyncJsCallback<(String, String), Option<BindingHookLoadOutput>, false>>,
+  pub transform: Option<MaybeAsyncJsCallback<(String, String), Option<BindingHookLoadOutput>, WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(
     ts_type = "(ctx: BindingPluginContext, module: BindingModuleInfo) => MaybePromise<VoidNullable>"
   )]
-  pub module_parsed: Option<MaybeAsyncJsCallback<(BindingPluginContext, BindingModuleInfo), (), false>>,
+  pub module_parsed: Option<MaybeAsyncJsCallback<(BindingPluginContext, BindingModuleInfo), (), WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "(error: Nullable<string>) => MaybePromise<VoidNullable>")]
-  pub build_end: Option<MaybeAsyncJsCallback<Option<String>, (), false>>,
+  pub build_end: Option<MaybeAsyncJsCallback<Option<String>, (), WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(
     ts_type = "(code: string, chunk: RenderedChunk) => MaybePromise<VoidNullable<BindingHookRenderChunkOutput>>"
   )]
   pub render_chunk:
-    Option<MaybeAsyncJsCallback<(String, RenderedChunk), Option<BindingHookRenderChunkOutput>, false>>,
+    Option<MaybeAsyncJsCallback<(String, RenderedChunk), Option<BindingHookRenderChunkOutput>, WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "() => void")]
-  pub render_start: Option<MaybeAsyncJsCallback<(), (), false>>,
+  pub render_start: Option<MaybeAsyncJsCallback<(), (), WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "(error: string) => void")]
-  pub render_error: Option<MaybeAsyncJsCallback<String, (), false>>,
+  pub render_error: Option<MaybeAsyncJsCallback<String, (), WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "(bundle: BindingOutputs, isWrite: boolean) => MaybePromise<VoidNullable>")]
-  pub generate_bundle: Option<MaybeAsyncJsCallback<(BindingOutputs, bool), (), false>>,
+  pub generate_bundle: Option<MaybeAsyncJsCallback<(BindingOutputs, bool), (), WEAK>>,
 
   #[serde(skip_deserializing)]
   #[napi(ts_type = "(bundle: BindingOutputs) => MaybePromise<VoidNullable>")]
-  pub write_bundle: Option<MaybeAsyncJsCallback<BindingOutputs, (), false>>,
+  pub write_bundle: Option<MaybeAsyncJsCallback<BindingOutputs, (), WEAK>>,
 }
 
 impl<const WEAK: bool> Debug for BindingPluginOptions<WEAK> {
@@ -92,65 +92,6 @@ impl<const WEAK: bool> Debug for BindingPluginOptions<WEAK> {
   }
 }
 
-impl FromNapiValue for BindingPluginOptions<true> {
-  #[allow(deprecated)] // cb.unref is deprecated, fix napi_derive to support const generics
-  unsafe fn from_napi_value(
-    env: napi::sys::napi_env,
-    napi_val: napi::sys::napi_value,
-  ) -> napi::Result<Self> {
-    let mut false_options = BindingPluginOptions::<false>::from_napi_value(env, napi_val)?;
-    let env = &napi::Env::from_raw(env);
-
-    if let Some(cb) = &mut false_options.build_start {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.resolve_id {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.load {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.transform {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.module_parsed {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.build_end {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.render_chunk {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.render_start {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.render_error {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.generate_bundle {
-      cb.unref(env)?;
-    }
-    if let Some(cb) = &mut false_options.write_bundle {
-      cb.unref(env)?;
-    }
-
-    Ok(Self {
-      name: false_options.name,
-      build_start: false_options.build_start,
-      resolve_id: false_options.resolve_id,
-      load: false_options.load,
-      transform: false_options.transform,
-      module_parsed: false_options.module_parsed,
-      build_end: false_options.build_end,
-      render_chunk: false_options.render_chunk,
-      render_start: false_options.render_start,
-      render_error: false_options.render_error,
-      generate_bundle: false_options.generate_bundle,
-      write_bundle: false_options.write_bundle,
-    })
-  }
-}
 
 #[napi_derive::napi(object, object_to_js = false)]
 #[derive(Debug, Deserialize, Default)]

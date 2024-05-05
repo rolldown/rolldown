61609d8d8beeaab5ff0929815c8cbf05c6cec99f
寞然 <chenhaojie.chj@alibaba-inc.com>
2024-05-05 19:36:53 +0800

    fix(rolldown/cli): detect argument is unspecified.

--- packages/rolldown/src/cli/index.ts
+++ packages/rolldown/src/cli/index.ts
@@ -1,21 +1,10 @@
-import process from 'node:process'
-import parseArgs from 'mri'
 import { defineCommand, runMain, showUsage } from 'citty'
-import { ensureConfig, logger } from './utils.js'
 import { bundle } from './commands/bundle.js'
 import {
   version,
   description,
 } from '../../package.json' assert { type: 'json' }
 import { DEFAULT_CONFIG_FILENAME } from './constants.js'
-
-interface ParsedArgs {
-  config?: string | true
-  c?: string | true
-  // `citty` intercept the help option, so we don't need to deal with it
-  // help?: boolean
-  // h?: boolean
-}
 
 const main = defineCommand({
   meta: {
@@ -25,7 +14,6 @@ const main = defineCommand({
   },
   args: {
     config: {
-      type: 'string',
       alias: 'c',
       description:
         'Use this config file (if argument is used but value is unspecified, defaults to `rolldown.config.js`)',
@@ -37,12 +25,10 @@ const main = defineCommand({
     },
   },
   async run(_ctx) {
-    // FIXME: `citty` doesn't support detecting if an argument is unspecified
-    const parsedArgs = parseArgs<ParsedArgs>(process.argv.slice(2))
-    let argConfig = parsedArgs.c || parsedArgs.config
-    if (argConfig) {
+    let argConfig = _ctx.args.config
+    if (typeof argConfig === 'string' || argConfig === true) {
       // If config is specified, we will ignore other arguments and bundle with the specified config
-      if (argConfig == true) {
+      if (argConfig === true) {
         argConfig = DEFAULT_CONFIG_FILENAME
       }
       await bundle(argConfig)

81dc6ca5ab9c3f5c9d4ef720919b446eff67f2b9
Yunfei <i.heyunfei@gmail.com>
2024-05-12 19:47:19 +0800

    chore(node/example): fix scripts of `par-plugin`

--- examples/par-plugin/cases/babel-rome-ts/par-babel.rolldown.config.js
+++ examples/par-plugin/cases/babel-rome-ts/par-babel.rolldown.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'rolldown'
 import { builtinModules } from 'node:module'
 import nodePath from 'node:path'
-import { default as parallelBabelPluginSync } from '../../parallel-babel-plugin/index.js'
+import { default as parallelPlugin } from '../../parallel-babel-plugin/index.js'
 
 export const REPO_ROOT = nodePath.resolve(import.meta.dirname, '../../../..')
 
@@ -13,7 +13,7 @@ export default defineConfig({
   external: builtinModules,
   // Need this due rome is not written with `isolatedModules: true`
   shimMissingExports: true,
-  plugins: [parallelBabelPluginSync()],
+  plugins: [parallelPlugin()],
   resolve: {
     extensions: ['.ts'],
     tsconfigFilename: nodePath.join(

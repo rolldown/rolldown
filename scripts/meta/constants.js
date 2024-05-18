f671b46fab938c11315ff16e68f4db443c1a471a
Yunfei He <i.heyunfei@gmail.com>
2024-05-18 20:52:05 +0800

    chore(scripts): fix `constants.js` (#1162)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info about the "what" this PR is solving -->

--- scripts/meta/constants.js
+++ scripts/meta/constants.js
@@ -3,7 +3,7 @@ import { workspaceRoot } from '@rolldown/testing'
 
 export const REPO_ROOT = workspaceRoot()
 
-if (process.argv[1] === __filename) {
+if (process.argv[1] === import.meta.filename) {
   // If this file is executed directly, print the exports
   console.log(selfExports)
 }

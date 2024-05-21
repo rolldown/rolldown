7116c9b4529ba3079b19457236cff3a708727e81
Yunfei <i.heyunfei@gmail.com>
2024-05-21 12:14:19 +0800

    Using glob-match

--- crates/rolldown_common/src/types/package_json.rs
+++ crates/rolldown_common/src/types/package_json.rs
@@ -1,29 +1,20 @@
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use globset::GlobSet;
-
-use crate::side_effects::SideEffects;
+use crate::side_effects::{glob_match_with_normalized_pattern, SideEffects};
 
 #[derive(Debug, Clone)]
 pub struct PackageJson {
   pub raw: Arc<serde_json::Value>,
   /// Realpath to `package.json`. Contains the `package.json` filename.
   pub realpath: PathBuf,
-  pub side_effects: Option<(SideEffects, GlobSet)>,
+  pub side_effects: Option<SideEffects>,
 }
 
 impl PackageJson {
   pub fn new(raw: impl Into<Arc<serde_json::Value>>, realpath: PathBuf) -> Self {
     let raw = raw.into();
-    Self {
-      side_effects: SideEffects::from_description(&raw).map(|item| {
-        let matcher = item.global_matcher();
-        (item, matcher)
-      }),
-      raw,
-      realpath,
-    }
+    Self { side_effects: SideEffects::from_description(&raw), raw, realpath }
   }
 
   pub fn r#type(&self) -> Option<&str> {
@@ -31,11 +22,14 @@ impl PackageJson {
   }
 
   pub fn check_side_effects_for(&self, module_path: &str) -> Option<bool> {
-    let (side_effects, matcher) = self.side_effects.as_ref()?;
+    let side_effects = self.side_effects.as_ref()?;
     // Is it necessary to convert module_path to relative path?
     match side_effects {
       SideEffects::Bool(s) => Some(*s),
-      _ => Some(matcher.is_match(module_path)),
+      SideEffects::String(p) => Some(glob_match_with_normalized_pattern(p.as_str(), module_path)),
+      SideEffects::Array(pats) => {
+        Some(pats.iter().any(|p| glob_match_with_normalized_pattern(p.as_str(), module_path)))
+      }
     }
   }
 }
--- crates/rolldown_common/src/types/side_effects.rs
+++ crates/rolldown_common/src/types/side_effects.rs
@@ -1,5 +1,3 @@
-use globset::{Glob, GlobSet, GlobSetBuilder};
-
 #[derive(Clone, Debug)]
 pub enum SideEffects {
   Bool(bool),
@@ -23,52 +21,16 @@ impl SideEffects {
       _ => None,
     })
   }
-
-  pub(crate) fn global_matcher(&self) -> GlobSet {
-    let mut set_builder = GlobSetBuilder::new();
-    match self {
-      SideEffects::Bool(_) => {}
-      SideEffects::String(pat) => {
-        set_builder.add(Glob::new(&normalized_pattern(pat)).unwrap());
-      }
-      SideEffects::Array(pats) => {
-        for pat in pats {
-          set_builder.add(Glob::new(&normalized_pattern(pat)).unwrap());
-        }
-      }
-    };
-    set_builder.build().unwrap()
-  }
 }
 
-/// Possible patterns in `sideEffects` field:
-/// - `./src/some-side-effect-file.js`
-/// - `*.css`
-/// - `**/*.css`
-/// - `./esnext/index.js`
-fn normalized_pattern(pattern: &str) -> String {
-  // For relative path, remove leading `./`, and add `**/` prefix.
-  // `./src/foo.js` -> `**/src/foo.js`.
+pub(crate) fn glob_match_with_normalized_pattern(pattern: &str, path: &str) -> bool {
   let trimmed_str = pattern.trim_start_matches("./");
-  if trimmed_str.len() == pattern.len() {
+  let normalized_glob = if trimmed_str.len() != pattern.len() {
+    String::from("**/") + trimmed_str
+  } else if trimmed_str.contains('/') {
     trimmed_str.to_string()
   } else {
-    format!("**/{trimmed_str}")
-  }
-}
-
-#[test]
-fn test_side_effects() {
-  use serde_json::json;
-  let side_effects = SideEffects::from_description(&json!({
-    "sideEffects": [
-      "./ke?p/*/file.js",
-      "./remove/this/file.j",
-      "./re?ve/this/file.js"
-    ]
-  }));
-
-  let side_effects = side_effects.unwrap();
-
-  assert!(side_effects.global_matcher().is_match("node_modules/demo-pkg/keep/this/file.js"),);
+    String::from("**/") + trimmed_str
+  };
+  glob_match::glob_match(&normalized_glob, path.trim_start_matches("./"))
 }

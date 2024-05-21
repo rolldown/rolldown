b050dcfccf381644c340ed892f904dce06aaef57
王子凌 <sling1994@live.com>
2024-05-21 22:33:25 +0800

    chore(rust): better to use the more semantic method to ensure napi-bundler-wasm `current_dir` exists

--- crates/rolldown_binding/src/bundler.rs
+++ crates/rolldown_binding/src/bundler.rs
@@ -36,7 +36,7 @@ impl Bundler {
 
     #[cfg(target_family = "wasm")]
     // if we don't perform this warmup, the following call to `std::fs` will stuck
-    if let Ok(_) = std::fs::metadata(std::env::current_dir()?) {};
+    let _ = std::env::current_dir()?.exists();
 
     #[cfg(not(target_family = "wasm"))]
     let worker_count =

caa6526513dca908305ef9d88ad09345bf2abb14
underfin <likui.underfin@gmail.com>
2024-05-22 19:07:48 +0800

    fix: typing

--- packages/rolldown/src/options/bindingify-input-options.ts
+++ packages/rolldown/src/options/bindingify-input-options.ts
@@ -56,6 +56,7 @@ export function bindingifyInputOptions(
       : undefined,
     platform: options.platform,
     shimMissingExports: options.shimMissingExports,
+    // @ts-ignore TODO The typing should import from binding
     logLevel: bindingifyLogLevel(options.logLevel),
     onLog: options.onLog,
   }

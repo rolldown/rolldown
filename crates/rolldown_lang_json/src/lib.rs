f855beeeaf2fc01e8d059b3e3adb2b5e7ab648ab
Ethan Goh <54303040+7086cmd@users.noreply.github.com>
2024-05-23 08:56:15 +0000

    feat: handle `proposal-json-superset` with iterating strings contains specific chars

--- crates/rolldown_lang_json/src/lib.rs
+++ crates/rolldown_lang_json/src/lib.rs
@@ -1,11 +1,14 @@
+use std::borrow::Cow;
+
 use anyhow::Ok;
 use rolldown_utils::ecma_script::is_validate_identifier_name;
 use serde_json::Value;
-// TODO: handling https://github.com/tc39/proposal-json-superset
+// TODO: handling https://github.com/tc39/proposal-json-superset // Fixing by Ethan Goh (@7086cmd) in this issue.
 
 pub fn json_to_esm(json: &str) -> anyhow::Result<String> {
+  let json = escape_json_superset(json);
   // TODO: use zero-copy deserialization
-  let json_value: Value = serde_json::from_str(json)?;
+  let json_value: Value = serde_json::from_str(&json)?;
 
   match json_value {
     Value::Object(map) => {
@@ -37,5 +40,21 @@ pub fn json_to_esm(json: &str) -> anyhow::Result<String> {
       let json_str = serde_json::to_string(&json_value)?;
       Ok(format!("export default {json_str}"))
     }
+  }
+}
+
+fn escape_json_superset(json: &str) -> Cow<str> {
+  if json.contains('\u{2028}') || json.contains('\u{2029}') {
+    let mut escaped = String::with_capacity(json.len());
+    for c in json.chars() {
+      match c {
+        '\u{2028}' => escaped.push_str("\\u2028"),
+        '\u{2029}' => escaped.push_str("\\u2029"),
+        _ => escaped.push(c),
+      }
+    }
+    Cow::Owned(escaped)
+  } else {
+    Cow::Borrowed(json)
   }
 }

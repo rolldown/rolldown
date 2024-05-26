0b045912dc9ef31077bcc98b25cd92eb4853c5a6
Yunfei He <i.heyunfei@gmail.com>
2024-05-26 21:41:00 +0800

    feat(error): emit diagnostics for missing export (#1238)

---
+++ crates/rolldown/tests/fixtures/errors/missing_export/main.js
@@ -0,0 +1,6 @@
+import { importedA, importedB as b } from "./foo"
+export { importedC, importedD as d } from "./foo"
+
+import importedE from "./foo"
+import { default as f } from "./foo"
+export { default as g } from "./foo"
---
+++ crates/rolldown_error/src/events/missing_export.rs
@@ -0,0 +1,50 @@
+use oxc::span::Span;
+
+use crate::{event_kind::EventKind, types::diagnostic_options::DiagnosticOptions};
+use std::sync::Arc;
+
+use super::BuildEvent;
+
+#[derive(Debug)]
+pub struct MissingExport {
+  pub stable_importer: String,
+  pub stable_importee: String,
+  pub importer_source: Arc<str>,
+  pub imported_specifier: String,
+  pub imported_specifier_span: Span,
+}
+
+impl BuildEvent for MissingExport {
+  fn kind(&self) -> crate::event_kind::EventKind {
+    EventKind::MissingExport
+  }
+
+  fn code(&self) -> &'static str {
+    "MISSING_EXPORT"
+  }
+
+  fn message(&self, _opts: &DiagnosticOptions) -> String {
+    format!(
+      r#""{}" is not exported by "{}", imported by "{}"."#,
+      self.imported_specifier, &self.stable_importee, &self.stable_importer
+    )
+  }
+
+  fn on_diagnostic(
+    &self,
+    diagnostic: &mut crate::diagnostic::Diagnostic,
+    _opts: &DiagnosticOptions,
+  ) {
+    let importer_file =
+      diagnostic.add_file(self.stable_importer.clone(), Arc::clone(&self.importer_source));
+
+    diagnostic.title =
+      format!(r#""{}" is not exported by "{}"."#, self.imported_specifier, &self.stable_importee);
+
+    diagnostic.add_label(
+      &importer_file,
+      self.imported_specifier_span.start..self.imported_specifier_span.end,
+      "Missing export".to_string(),
+    );
+  }
+}

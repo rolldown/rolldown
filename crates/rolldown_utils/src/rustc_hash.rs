7c9650b3b5f779fa8001d041f9f155400cfcd8e9
Yunfei <i.heyunfei@gmail.com>
2024-05-28 18:16:09 +0800

    Fix deps

--- crates/rolldown_utils/src/rustc_hash.rs
+++ crates/rolldown_utils/src/rustc_hash.rs
@@ -1,6 +1,4 @@
-use std::hash::BuildHasherDefault;
-
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::{FxBuildHasher, FxHashMap, FxHashSet};
 
 pub trait FxHashSetExt {
   fn with_capacity(capacity: usize) -> Self;
@@ -12,13 +10,13 @@ pub trait FxHashMapExt {
 #[allow(clippy::implicit_hasher)]
 impl<K, V> FxHashMapExt for FxHashMap<K, V> {
   fn with_capacity(capacity: usize) -> Self {
-    FxHashMap::with_capacity_and_hasher(capacity, BuildHasherDefault::default())
+    FxHashMap::with_capacity_and_hasher(capacity, FxBuildHasher)
   }
 }
 
 #[allow(clippy::implicit_hasher)]
 impl<T> FxHashSetExt for FxHashSet<T> {
   fn with_capacity(capacity: usize) -> Self {
-    FxHashSet::with_capacity_and_hasher(capacity, BuildHasherDefault::default())
+    FxHashSet::with_capacity_and_hasher(capacity, FxBuildHasher)
   }
 }

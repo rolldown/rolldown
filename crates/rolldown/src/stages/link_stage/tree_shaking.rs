780a21eeda51f58175daa79532c1b112252d04f1
Yunfei <i.heyunfei@gmail.com>
2024-06-01 00:00:19 +0800

    Fix namespace

--- crates/rolldown/src/stages/link_stage/tree_shaking.rs
+++ crates/rolldown/src/stages/link_stage/tree_shaking.rs
@@ -32,6 +32,11 @@ fn include_module(ctx: &mut Context, module: &NormalModule) {
   if ctx.tree_shaking {
     let forced_no_treeshake = matches!(module.side_effects, DeterminedSideEffects::NoTreeshake);
     module.stmt_infos.iter_enumerated().for_each(|(stmt_info_id, stmt_info)| {
+      // Skip the first statement, which is the namespace object. It should included only if it is used no matter
+      // tree shaking is enabled or not.
+      if stmt_info_id.index() == 0 {
+        return;
+      }
       if stmt_info.side_effect && !forced_no_treeshake {
         include_statement(ctx, module, stmt_info_id);
       }

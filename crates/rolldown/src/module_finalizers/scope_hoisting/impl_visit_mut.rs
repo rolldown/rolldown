f26a81e243b4e7a989a6004aa378102b4ec2738a
Yunfei <i.heyunfei@gmail.com>
2024-06-05 18:15:35 +0800

    feat(rust): add `AllocatorExt` trait

--- crates/rolldown/src/module_finalizers/scope_hoisting/impl_visit_mut.rs
+++ crates/rolldown/src/module_finalizers/scope_hoisting/impl_visit_mut.rs
@@ -10,7 +10,7 @@ use oxc::{
   span::{GetSpan, Span, SPAN},
 };
 use rolldown_common::{ExportsKind, ModuleId, SymbolRef, WrapKind};
-use rolldown_oxc_utils::{ExpressionExt, IntoIn, StatementExt, TakeIn};
+use rolldown_oxc_utils::{AllocatorExt, ExpressionExt, IntoIn, StatementExt, TakeIn};
 
 use crate::utils::call_expression_ext::CallExpressionExt;
 
@@ -19,7 +19,7 @@ use super::ScopeHoistingFinalizer;
 impl<'me, 'ast> VisitMut<'ast> for ScopeHoistingFinalizer<'me, 'ast> {
   #[allow(clippy::too_many_lines)]
   fn visit_program(&mut self, program: &mut ast::Program<'ast>) {
-    let old_body = program.body.take_in(self.alloc);
+    let old_body = self.alloc.take(&mut program.body);
 
     let is_namespace_referenced = matches!(self.ctx.module.exports_kind, ExportsKind::Esm)
       && self.ctx.module.stmt_infos[0].is_included;
--- crates/rolldown_oxc_utils/src/lib.rs
+++ crates/rolldown_oxc_utils/src/lib.rs
@@ -8,6 +8,9 @@ pub use crate::{
   allocator_helpers::{from_in::FromIn, into_in::IntoIn, take_in::TakeIn},
   ast_snippet::AstSnippet,
   compiler::OxcCompiler,
-  ext::{BindingIdentifierExt, BindingPatternExt, ExpressionExt, StatementExt},
+  ext::{
+    allocator_ext::AllocatorExt, BindingIdentifierExt, BindingPatternExt, ExpressionExt,
+    StatementExt,
+  },
   oxc_ast::{OxcAst, WithFieldsMut},
 };

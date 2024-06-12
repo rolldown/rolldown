e80cad12d7dad10d3851f9cd80bb5d17f6819167
underfin <likui.underfin@gmail.com>
2024-06-12 17:24:31 +0800

    fix: test

--- packages/rolldown/src/types/sourcemap.ts
+++ packages/rolldown/src/types/sourcemap.ts
@@ -1,7 +1,7 @@
 import { BindingSourcemap } from '../binding'
 
 export interface SourceMapInputObject {
-  file?: string
+  file?: string | null
   mappings: string
   names?: string[]
   sources?: (string | null)[]
@@ -16,5 +16,17 @@ export function bidingSourcemap(
   map?: SourceMapInput,
 ): undefined | BindingSourcemap {
   if (map == null) return
-  return { inner: map }
+  return {
+    inner:
+      typeof map === 'string'
+        ? map
+        : {
+            file: map.file ?? undefined,
+            mappings: map.mappings,
+            sourceRoot: map.sourceRoot,
+            sources: map.sources?.map((s) => s ?? undefined),
+            sourcesContent: map.sourcesContent?.map((s) => s ?? undefined),
+            names: map.names,
+          },
+  }
 }

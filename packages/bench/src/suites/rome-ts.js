c0d2d76efc8b98ab50c9590f5adc657e9f4356f9
Yunfei <i.heyunfei@gmail.com>
2024-06-18 18:15:40 +0800

    chore(node/bench): track benchmark of transform typescript using rome-ts

--- packages/bench/src/suites/rome-ts.js
+++ packages/bench/src/suites/rome-ts.js
@@ -1,6 +1,5 @@
 import { defineSuite } from '../utils.js'
 import nodePath from 'node:path'
-import * as esbuild from 'esbuild'
 import { REPO_ROOT } from '../utils.js'
 import _ from 'lodash'
 import { default as parallelBabelPlugin } from '../parallel-babel-plugin/index.js'
@@ -64,44 +63,20 @@ export const suiteRomeTs = defineSuite({
   title: 'rome-ts',
   inputs,
   esbuildOptions,
-  rolldownOptions: [
-    {
-      name: 'esbuild',
-      options: {
-        logLevel: 'silent',
-        external: builtinModules,
-        // Need this due rome is not written with `isolatedModules: true`
-        shimMissingExports: true,
-        plugins: [
-          {
-            name: '@rolldown/plugin-esbuild',
-            async transform(code, id) {
-              const ext = nodePath.extname(id)
-              if (ext === '.ts' || ext === '.tsx') {
-                const ret = await esbuild.transform(code, {
-                  platform: 'node',
-                  loader: ext === '.tsx' ? 'tsx' : 'ts',
-                  format: 'esm',
-                  target: 'chrome80',
-                  sourcemap: true,
-                })
-
-                return {
-                  code: ret.code,
-                }
-              }
-            },
-          },
-        ],
-        resolve: {
-          extensions: ['.ts'],
-          tsconfigFilename: nodePath.join(
-            REPO_ROOT,
-            './tmp/bench/rome/src/tsconfig.json',
-          ),
-        },
-      },
+  rolldownOptions: {
+    logLevel: 'silent',
+    external: builtinModules,
+    // Need this due rome is not written with `isolatedModules: true`
+    shimMissingExports: true,
+    resolve: {
+      tsconfigFilename: nodePath.join(
+        REPO_ROOT,
+        './tmp/bench/rome/src/tsconfig.json',
+      ),
     },
-  ],
+  },
   disableBundler: ['rollup'],
+  derived: {
+    sourcemap: true,
+  },
 })

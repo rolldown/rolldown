3b5c526bb02787cce3b3ff2ec8eabb64aa7fe6ae
LongYinan <lynweklm@gmail.com>
2024-06-24 22:07:52 +0800

    fix(rolldown_fs): canonicalize polyfill on wasi platform

--- crates/rolldown_fs/src/os.rs
+++ crates/rolldown_fs/src/os.rs
@@ -47,6 +47,33 @@ impl OxcResolverFileSystem for OsFileSystem {
   }
 
   fn canonicalize(&self, path: &Path) -> io::Result<PathBuf> {
-    dunce::canonicalize(path)
+    #[cfg(not(target_os = "wasi"))]
+    {
+      dunce::canonicalize(path)
+    }
+    #[cfg(target_os = "wasi")]
+    {
+      let meta = std::fs::symlink_metadata(path)?;
+      if meta.file_type().is_symlink() {
+        let link = std::fs::read_link(path)?;
+        let mut path_buf = path.to_path_buf();
+        path_buf.pop();
+        for segment in link.iter() {
+          match segment.to_str() {
+            Some("..") => {
+              path_buf.pop();
+            }
+            Some(".") | None => {}
+            Some(seg) => {
+              // Need to trim the extra \0 introduces by rust std rust-lang/rust#123727
+              path_buf.push(seg.trim_end_matches('\0'));
+            }
+          }
+        }
+        Ok(path_buf)
+      } else {
+        Ok(path.to_path_buf())
+      }
+    }
   }
 }

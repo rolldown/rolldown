1300e78ec69006bf683044a657e256ac0ccd3790
Cameron Clark <cameron.clark@hey.com>
2024-06-25 19:40:14 +0100

    fix(resolver): ensure we always path an absolute path to oxc_resolver closes #1441

--- crates/rolldown_resolver/src/resolver.rs
+++ crates/rolldown_resolver/src/resolver.rs
@@ -66,9 +66,12 @@ impl<F: FileSystem + Default> Resolver<F> {
     };
 
     let resolve_options_with_default_conditions = OxcResolverOptions {
-      tsconfig: raw_resolve.tsconfig_filename.map(|p| TsconfigOptions {
-        config_file: p.into(),
-        references: oxc_resolver::TsconfigReferences::Disabled,
+      tsconfig: raw_resolve.tsconfig_filename.map(|p| {
+        let path = PathBuf::from(&p);
+        TsconfigOptions {
+          config_file: if path.is_relative() { cwd.join(path) } else { path },
+          references: oxc_resolver::TsconfigReferences::Disabled,
+        }
       }),
       alias: raw_resolve
         .alias

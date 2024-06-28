1efd5684548da188f870881df896b415239b0ddf
Boshen <boshenc@gmail.com>
2024-06-28 21:43:12 +0800

    perf: use jemalloc on systems that performs better, and mimalloc on windows
    I forgot how I ended up with this setting for `oxlint`, but it was from trial-and-error :sweat_and_smile:
    
    Due to the use of bumpalo arena allocator, my guess is that jemalloc performs better under less allocation pressure.

--- crates/rolldown_binding/src/lib.rs
+++ crates/rolldown_binding/src/lib.rs
@@ -13,9 +13,13 @@
 // Looks redundant
 #![allow(clippy::missing_transmute_annotations)]
 
-#[cfg(not(target_family = "wasm"))]
+#[cfg(not(target_env = "msvc"))]
 #[global_allocator]
-static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 pub mod bundler;
 pub mod options;

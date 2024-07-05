5d5af2661ae11d83ee5c43a46a5e654f71116c52
Ethan Goh <7086cmd@gmail.com>
2024-07-05 16:45:59 +0800

    refactor: remove `raw_text_to_esm` function

--- crates/rolldown_loader_utils/src/lib.rs
+++ crates/rolldown_loader_utils/src/lib.rs
@@ -1,9 +1,7 @@
 mod binary_to_esm;
 mod json_to_esm;
-mod raw_text_to_esm;
 mod text_to_esm;
 
 pub use binary_to_esm::binary_to_esm;
 pub use json_to_esm::json_to_esm;
-pub use raw_text_to_esm::raw_text_to_esm;
 pub use text_to_esm::text_to_esm;

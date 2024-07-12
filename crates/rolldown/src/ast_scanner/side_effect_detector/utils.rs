cd2d2185cd24a6d270fb0e88d9f296a8a1b39f63
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-07-12 18:13:51 +0800

    fix: 🐛 remove unused pure comment call
    ]

--- crates/rolldown/src/ast_scanner/side_effect_detector/utils.rs
+++ crates/rolldown/src/ast_scanner/side_effect_detector/utils.rs
@@ -22,7 +22,7 @@ impl<'a> SideEffectDetector<'a> {
   ///
   /// let c = 1;
   ///
-  /// let d = 1; /* invalid comment for `e` */
+  /// let d = 1; /* valid comment for `e` */
   /// let e = 2
   /// ```
   /// Derived from https://github.com/oxc-project/oxc/blob/147864cfeb112df526bb83d5b8671b465c005066/crates/oxc_linter/src/utils/tree_shaking.rs#L204
@@ -30,41 +30,15 @@ impl<'a> SideEffectDetector<'a> {
     let comment = self.trivias.comments_range(..span.start).next_back()?;
 
     let comment_text = comment.span.source_text(self.source);
-
-    // If there are non-whitespace characters between the `comment`` and the `span`,
+    // If there are non-whitespace characters between the `comment` and the `span`,
     // we treat the `comment` not belongs to the `span`.
-    let only_whitespace = Span::new(comment.span.end, span.start)
-      .source_text(self.source)
-      .strip_prefix("*/") // for multi-line comment
-      .is_some_and(|s| s.trim().is_empty());
-
+    let range_text = Span::new(comment.span.end, span.start).source_text(self.source);
+    let only_whitespace = match range_text.strip_prefix("*/") {
+      Some(str) => str.trim().is_empty(),
+      None => range_text.trim().is_empty(),
+    };
     if !only_whitespace {
       return None;
-    }
-
-    // Next step, we need make sure it's not the trailing comment of the previous line.
-    let mut current_line_start = span.start as usize;
-    for c in self.source[..span.start as usize].chars().rev() {
-      if c == '\n' {
-        break;
-      }
-
-      current_line_start -= c.len_utf8();
-    }
-    let Ok(current_line_start) = u32::try_from(current_line_start) else {
-      return None;
-    };
-
-    if comment.span.end < current_line_start {
-      let previous_line =
-        self.source[..comment.span.end as usize].lines().next_back().unwrap_or("");
-      let nothing_before_comment = previous_line
-        .trim()
-        .strip_prefix(if comment.kind == CommentKind::SingleLine { "//" } else { "/*" })
-        .is_some_and(|s| s.trim().is_empty());
-      if !nothing_before_comment {
-        return None;
-      }
     }
 
     Some((comment, comment_text))

2f53bce4ce7b47e98ca3419eec849893bde0e9c7
Yunfei He <i.heyunfei@gmail.com>
2024-07-14 04:30:23 +0800

    refactor(plugin_driver): tweak code of `render_chunks` and `augment_chunk_hash` (#1604)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/src/utils/augment_chunk_hash.rs
+++ crates/rolldown/src/utils/augment_chunk_hash.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use futures::future::try_join_all;
 use rolldown_common::{AssetMeta, PreliminaryAsset};
 use rolldown_plugin::SharedPluginDriver;
@@ -8,26 +8,18 @@ pub async fn augment_chunk_hash<'a>(
   plugin_driver: &SharedPluginDriver,
   chunks: Vec<PreliminaryAsset>,
 ) -> Result<Vec<PreliminaryAsset>> {
-  try_join_all(chunks.into_iter().map(|chunk| async move {
-    if let AssetMeta::Ecma(ecma_meta) = &chunk.meta {
-      plugin_driver.augment_chunk_hash(ecma_meta).await.map(|augment_chunk_hash| PreliminaryAsset {
-        content: chunk.content,
-        map: chunk.map,
-        meta: chunk.meta,
-        augment_chunk_hash,
-        file_dir: chunk.file_dir,
-        preliminary_filename: chunk.preliminary_filename,
-      })
+  try_join_all(chunks.into_iter().map(|mut asset| async move {
+    let augment_chunk_hash = if let AssetMeta::Ecma(ecma_meta) = &asset.meta {
+      plugin_driver.augment_chunk_hash(ecma_meta).await
     } else {
-      Ok(PreliminaryAsset {
-        content: chunk.content,
-        map: chunk.map,
-        meta: chunk.meta,
-        augment_chunk_hash: None,
-        file_dir: chunk.file_dir,
-        preliminary_filename: chunk.preliminary_filename,
-      })
+      Ok(None)
+    }?;
+
+    if let Some(augment_chunk_hash) = augment_chunk_hash {
+      asset.augment_chunk_hash = Some(augment_chunk_hash);
     }
+
+    Ok(asset)
   }))
   .await
 }
--- crates/rolldown/src/utils/render_chunks.rs
+++ crates/rolldown/src/utils/render_chunks.rs
@@ -9,38 +9,30 @@ pub async fn render_chunks<'a>(
   plugin_driver: &SharedPluginDriver,
   chunks: Vec<PreliminaryAsset>,
 ) -> Result<Vec<PreliminaryAsset>> {
-  try_join_all(chunks.into_iter().map(|chunk| async move {
-    if let AssetMeta::Ecma(ecma_meta) = &chunk.meta {
+  try_join_all(chunks.into_iter().map(|mut asset| async move {
+    // TODO(hyf0): To be refactor:
+    // - content should use ArcStr
+    // - plugin_driver.render_chunk should return Option<...> to be able to see if there is a return value by the plugin
+    let render_chunk_ret = if let AssetMeta::Ecma(ecma_meta) = &asset.meta {
       plugin_driver
-        .render_chunk(HookRenderChunkArgs { code: chunk.content, chunk: ecma_meta })
+        .render_chunk(HookRenderChunkArgs { code: asset.content.clone(), chunk: ecma_meta })
         .await
-        .map(|(code, render_chunk_sourcemap_chain)| PreliminaryAsset {
-          content: code,
-          map: chunk.map.and_then(|sourcemap| {
-            if render_chunk_sourcemap_chain.is_empty() {
-              Some(sourcemap)
-            } else {
-              let mut sourcemap_chain = Vec::with_capacity(render_chunk_sourcemap_chain.len() + 1);
-              sourcemap_chain.push(&sourcemap);
-              sourcemap_chain.extend(render_chunk_sourcemap_chain.iter());
-              collapse_sourcemaps(sourcemap_chain)
-            }
-          }),
-          meta: chunk.meta,
-          augment_chunk_hash: None,
-          file_dir: chunk.file_dir,
-          preliminary_filename: chunk.preliminary_filename,
-        })
     } else {
-      Ok(PreliminaryAsset {
-        content: chunk.content,
-        map: chunk.map,
-        meta: chunk.meta,
-        augment_chunk_hash: None,
-        file_dir: chunk.file_dir,
-        preliminary_filename: chunk.preliminary_filename,
-      })
+      Ok((asset.content.clone(), vec![]))
+    }?;
+
+    asset.content = render_chunk_ret.0;
+    if let Some(asset_map) = &asset.map {
+      if !render_chunk_ret.1.is_empty() {
+        let mut sourcemap_chain = Vec::with_capacity(render_chunk_ret.1.len() + 1);
+        sourcemap_chain.push(asset_map);
+        sourcemap_chain.extend(render_chunk_ret.1.iter());
+        let new_source_map = collapse_sourcemaps(sourcemap_chain);
+        asset.map = new_source_map;
+      }
     }
+
+    Ok(asset)
   }))
   .await
 }

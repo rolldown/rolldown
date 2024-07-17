f66e167d1884dd9af589af4fbba3553e9b2ffea4
underfin <likui.underfin@gmail.com>
2024-07-17 12:20:44 +0800

    refactor: separate render chunk logic for different format (#1644)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    The pr intend to make render chunk code clean. After this, we don't
    match the format many times if we add logic.
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ crates/rolldown/src/ecmascript/format/app.rs
@@ -0,0 +1,31 @@
+use rolldown_sourcemap::{ConcatSource, RawSource};
+
+use crate::{ecmascript::ecma_generator::RenderedModuleSources, types::generator::GenerateContext};
+
+pub fn render_app(
+  _ctx: &GenerateContext<'_>,
+  module_sources: RenderedModuleSources,
+  banner: Option<String>,
+  footer: Option<String>,
+) -> ConcatSource {
+  let mut concat_source = ConcatSource::default();
+
+  if let Some(banner) = banner {
+    concat_source.add_source(Box::new(RawSource::new(banner)));
+  }
+
+  // chunk content
+  module_sources.into_iter().for_each(|(_, _, module_render_output)| {
+    if let Some(emitted_sources) = module_render_output {
+      for source in emitted_sources {
+        concat_source.add_source(source);
+      }
+    }
+  });
+
+  if let Some(footer) = footer {
+    concat_source.add_source(Box::new(RawSource::new(footer)));
+  }
+
+  concat_source
+}

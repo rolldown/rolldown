ab8dda501664c71477f17e43a4bcf1230b3e8f67
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-07-22 18:44:29 +0800

    fix: reexport star of a external module should be considered have side effect. (#1675)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/src/stages/link_stage/mod.rs
+++ crates/rolldown/src/stages/link_stage/mod.rs
@@ -283,8 +283,7 @@ impl<'a> LinkStage<'a> {
             match &self.module_table.modules[rec.resolved_module] {
               Module::External(importee) => {
                 // Make sure symbols from external modules are included and de_conflicted
-                stmt_info.side_effect = importee.side_effects.has_side_effects();
-                match rec.kind {
+                let is_reexport_all = match rec.kind {
                   ImportKind::Import => {
                     if matches!(self.input_options.format, OutputFormat::Cjs)
                       && !rec.is_plain_import
@@ -303,9 +302,11 @@ impl<'a> LinkStage<'a> {
                         .referenced_symbols
                         .push(self.runtime.resolve_symbol("__reExport").into());
                     }
+                    is_reexport_all
                   }
-                  _ => {}
-                }
+                  _ => false,
+                };
+                stmt_info.side_effect = importee.side_effects.has_side_effects() || is_reexport_all;
               }
               Module::Ecma(importee) => {
                 let importee_linking_info = &self.metas[importee.idx];
--- crates/rolldown_common/src/inner_bundler_options/types/treeshake.rs
+++ crates/rolldown_common/src/inner_bundler_options/types/treeshake.rs
@@ -1,8 +1,17 @@
 use crate::types::js_regex::HybridRegex;
+#[cfg(feature = "deserialize_bundler_options")]
+use schemars::JsonSchema;
+#[cfg(feature = "deserialize_bundler_options")]
+use serde::{Deserialize, Deserializer};
 
 #[derive(Debug)]
+#[cfg_attr(
+  feature = "deserialize_bundler_options",
+  derive(Deserialize, JsonSchema),
+  serde(untagged)
+)]
 pub enum TreeshakeOptions {
-  False,
+  Boolean(bool),
   Option(InnerOptions),
 }
 
@@ -35,6 +44,28 @@ impl TreeshakeOptions {
 }
 
 #[derive(Debug)]
+#[cfg_attr(
+  feature = "deserialize_bundler_options",
+  derive(Deserialize, JsonSchema),
+  serde(rename_all = "camelCase", deny_unknown_fields)
+)]
 pub struct InnerOptions {
+  #[cfg_attr(
+    feature = "deserialize_bundler_options",
+    serde(deserialize_with = "deserialize_module_side_effects"),
+    schemars(with = "Option<bool>")
+  )]
   pub module_side_effects: ModuleSideEffects,
+}
+
+#[cfg(feature = "deserialize_bundler_options")]
+fn deserialize_module_side_effects<'de, D>(deserializer: D) -> Result<ModuleSideEffects, D::Error>
+where
+  D: Deserializer<'de>,
+{
+  let deserialized = Option::<bool>::deserialize(deserializer)?;
+  match deserialized {
+    Some(false) => Ok(ModuleSideEffects::Boolean(false)),
+    Some(true) | None => Ok(ModuleSideEffects::Boolean(true)),
+  }
 }

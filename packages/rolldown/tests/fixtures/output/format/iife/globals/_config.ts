5e22d9209c5aa44e55db211d60602c8b1ec43f20
underfin <likui.underfin@gmail.com>
2024-07-23 21:09:18 +0800

    chore: move js side test fold

---
+++ packages/rolldown/tests/fixtures/output/format/iife/globals/_config.ts
@@ -0,0 +1,29 @@
+import { defineTest } from '@tests'
+import { expect } from 'vitest'
+
+export default defineTest({
+  config: {
+    external: /node:path/,
+    output: {
+      format: 'iife',
+      globals: {
+        'node:path': 'path',
+      },
+    },
+  },
+  afterTest: (output) => {
+    expect(output.output[0].code).toMatchInlineSnapshot(`
+      "(function(node_path) {
+
+      const { join } = node_path;
+
+      //#region main.js
+      var main_default = join;
+
+      //#endregion
+      return main_default;
+      })(node_path);
+      "
+    `)
+  },
+})

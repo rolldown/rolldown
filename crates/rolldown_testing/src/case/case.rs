9b1c632336861b2a78374d83407d94f5637ba55f
Yunfei He <i.heyunfei@gmail.com>
2024-07-26 04:40:54 +0800

    test(rust/infra): function-complete rust side tests (#1737)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    Still need to be improved. But now we could test the plugin behavior in
    rust now.
    
    This also help us found the pain of using rolldown in rust.
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown_testing/src/case/case.rs
+++ crates/rolldown_testing/src/case/case.rs
@@ -1,12 +1,12 @@
 use std::{borrow::Cow, path::Path};
 
+use crate::utils::RUNTIME_MODULE_OUTPUT_RE;
+
 use super::fixture::Fixture;
-use regex::Regex;
 use rolldown::BundleOutput;
 use rolldown_common::Output;
 use rolldown_error::{BuildDiagnostic, DiagnosticOptions};
 use rolldown_sourcemap::SourcemapVisualizer;
-use std::sync::LazyLock;
 
 pub struct Case {
   fixture: Fixture,
@@ -177,8 +177,3 @@ impl Case {
     });
   }
 }
-
-static RUNTIME_MODULE_OUTPUT_RE: LazyLock<Regex> = LazyLock::new(|| {
-  Regex::new(r"(//#region rolldown:runtime[\s\S]*?//#endregion)")
-    .expect("invalid runtime module output regex")
-});

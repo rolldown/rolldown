04346003cd4945e8d76bc96734aa55ed49508287
hyf0 <49502170+hyf0@users.noreply.github.com>
2024-07-31 02:08:31 +0000

    chore(rust): rename `tests/fixture` to `tests/rolldown` (#1802)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info about the "what" this PR is solving -->

--- crates/rolldown/tests/integration_rolldown.rs
+++ crates/rolldown/tests/integration_rolldown.rs
@@ -8,10 +8,10 @@ use rolldown_testing::{
 };
 use sugar_path::SugarPath;
 use testing_macros::fixture;
-mod fixtures;
+mod rolldown;
 
 #[allow(clippy::needless_pass_by_value)]
-#[fixture("./tests/fixtures/**/_config.json")]
+#[fixture("./tests/rolldown/**/_config.json")]
 fn fixture_with_config(config_path: PathBuf) {
   Fixture::new(config_path.parent().unwrap()).run_integration_test();
 }

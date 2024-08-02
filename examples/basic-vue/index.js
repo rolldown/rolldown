rolldown/rolldown

commit ef8709ab233e76b5b05430e9abbabf9c5f3c9cd8

feat: simple binary expr folder (#1470)

examples/basic-vue/rolldown.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'rolldown'
+import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
   input: './index.js',
@@ -8,4 +9,9 @@ export default defineConfig({
     // aligns with Vite in the future.
     conditionNames: ['import'],
   },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
 })

pnpm-lock.yaml
@@ -57,6 +57,9 @@ importers:
         specifier: ^3.4.21
         version: 3.4.29(typescript@5.4.5)
     devDependencies:
+      '@rollup/plugin-replace':
+        specifier: ^5.0.7
+        version: 5.0.7(rollup@4.18.0)
       rolldown:
         specifier: workspace:*
         version: link:../../packages/rolldown
@@ -1811,7 +1814,6 @@ packages:
 
   '@ls-lint/ls-lint@2.2.3':
     resolution: {integrity: sha512-ekM12jNm/7O2I/hsRv9HvYkRdfrHpiV1epVuI2NP+eTIcEgdIdKkKCs9KgQydu/8R5YXTov9aHdOgplmCHLupw==}
-    cpu: [x64, arm64, s390x]
     os: [darwin, linux, win32]
     hasBin: true
 
@@ -1892,24 +1894,28 @@ packages:
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@napi-rs/lzma-linux-arm64-musl@1.3.1':
     resolution: {integrity: sha512-x4m/J3hRdFd3IoqKc+4aBcSUkcuDEP7hH6vdss8bKNLjPfXmnG/lcHJEQX3hNnNOULPP8xVRxgiY8jeL62cnsw==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@napi-rs/lzma-linux-x64-gnu@1.3.1':
     resolution: {integrity: sha512-riB+Xg72NfH8Kcpq07omshVu0QsLW0v2bhywBNYxvA+t2dGGWSIEN1U/zazUXys+IEA6pBQKqLVseurWE6Cl8g==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@napi-rs/lzma-linux-x64-musl@1.3.1':
     resolution: {integrity: sha512-JXLgJFwoeysxdSg7rdVYP8wrliCQVJlU5JcLYjRVSCL4P0mQTjnYi7R7VdaOkDACw/Fvlji7oIJXt0KiaDTcOw==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@napi-rs/lzma-wasm32-wasi@1.3.1':
     resolution: {integrity: sha512-XTA9XTUtj0KPKuzNS2oi3bWN6tJodk00wwMd/TiKoEqx9Fe/zu+OfDGuXAn+xs+P7S3YCDHqjYinuTQMZ0ucJw==}
@@ -1979,24 +1985,28 @@ packages:
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@napi-rs/tar-linux-arm64-musl@0.1.1':
     resolution: {integrity: sha512-R2e7YEILPKc447N1hUt1zQuyy0st83HX6nhmtFywS1aOj76/3hzxLVrebGiiS6kifyCrO82o66CNTYe7DixMZw==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@napi-rs/tar-linux-x64-gnu@0.1.1':
     resolution: {integrity: sha512-tmy9q2LXUB2GQ+IgUzJn3+us52leplMu96ORyFq1iHQEelGSqXt4xi5DzU07JhX3obJPqVqkybtcRS4enBfmBA==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@napi-rs/tar-linux-x64-musl@0.1.1':
     resolution: {integrity: sha512-j5ePbe6vihApl0cGPPDUVTWZbf2mF4NhxngAoG2jp7AeX3cWP2O4pjsvaOraW6QfvWEMFPeER7S0b6eBS7duvA==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@napi-rs/tar-wasm32-wasi@0.1.1':
     resolution: {integrity: sha512-se0+DlOiIRt7mcz7BeOJDoZtCrROpKLwiAOGmNgrjQ/SZS7TnOPuwm3wy9XN9WLoepttEjCEem6UP8yD5GJuQg==}
@@ -2063,24 +2073,28 @@ packages:
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@napi-rs/wasm-tools-linux-arm64-musl@0.0.2':
     resolution: {integrity: sha512-HkpZOID2U8P6pWqK3mqZ8bxU5xcuT3iA2fO+jrxn78h006iYgfNmdc5JaVhHnHazMmk32xKhSV4iV0VUh8UWDg==}
     engines: {node: '>= 10'}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@napi-rs/wasm-tools-linux-x64-gnu@0.0.2':
     resolution: {integrity: sha512-YksJWBCyOalB9ogtP9+/dZKP+vR6+h7BmzMXaXMT71WW/GvIsifMVgv+DY/FRSNJQupp5Y+ugjqVAOUOc/G65g==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@napi-rs/wasm-tools-linux-x64-musl@0.0.2':
     resolution: {integrity: sha512-sPtRxPMdw05KdTcxgSPMmSXG2+PiK3vJ/l2+g9hvjnnKtvslJN2Hr7j8zgzuoKRAUFPaJVe6+D2xVh5cpdqhww==}
     engines: {node: '>= 10'}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@napi-rs/wasm-tools-wasm32-wasi@0.0.2':
     resolution: {integrity: sha512-muRvZK7AIuo88G2AxYx3gA59rHMQgoN004saQkBvXnz3K/DVHKfTZ6TtUebss8zI3dURU6xExL8drxFWYxjEbQ==}
@@ -2187,21 +2201,25 @@ packages:
     resolution: {integrity: sha512-LTOtEliKPWJzVC6qs1M0IoND0yFchWkKS6agFSmzGmfe523geC+eATq3slFvUPdSzdLAgk6v2juol7JxaI2+vg==}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@oxlint/linux-arm64-musl@0.4.4':
     resolution: {integrity: sha512-hTmCi57YnUUxUxOWSdLEdN7IFkcJylsAZuWcVLjMCiPtTcyTeY6wPrUFqNSGLG+Gvqj/2g0aqzKt3eJBfDSOzg==}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@oxlint/linux-x64-gnu@0.4.4':
     resolution: {integrity: sha512-nqQr/Zj86F56EvpcbZMXEsYpUS+wpEAH1jahg70ApAEeq+EoVBqMZPv4sV1vAIy/R20f00a891fz13RGkorMnw==}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@oxlint/linux-x64-musl@0.4.4':
     resolution: {integrity: sha512-MdyAoQgLQf/ZzneV+cCWD++6ATKWEEp1VPa61664uCYtJIKRKiavPwT5BN+nQ197Bn7n/k6pOHmo/3n8+PCFNg==}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@oxlint/win32-arm64@0.4.4':
     resolution: {integrity: sha512-m1azJG8XD7Po1Las7ftYFvtSUaVju1V9ic771x6/Su+8vt1Iz2pDwG7jDK+5hHbS17w8qHCYcg/IDRFHuFsOeQ==}
@@ -2242,30 +2260,35 @@ packages:
     engines: {node: '>= 10.0.0'}
     cpu: [arm]
     os: [linux]
+    libc: [glibc]
 
   '@parcel/watcher-linux-arm64-glibc@2.4.1':
     resolution: {integrity: sha512-BJ7mH985OADVLpbrzCLgrJ3TOpiZggE9FMblfO65PlOCdG++xJpKUJ0Aol74ZUIYfb8WsRlUdgrZxKkz3zXWYA==}
     engines: {node: '>= 10.0.0'}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@parcel/watcher-linux-arm64-musl@2.4.1':
     resolution: {integrity: sha512-p4Xb7JGq3MLgAfYhslU2SjoV9G0kI0Xry0kuxeG/41UfpjHGOhv7UoUDAz/jb1u2elbhazy4rRBL8PegPJFBhA==}
     engines: {node: '>= 10.0.0'}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@parcel/watcher-linux-x64-glibc@2.4.1':
     resolution: {integrity: sha512-s9O3fByZ/2pyYDPoLM6zt92yu6P4E39a03zvO0qCHOTjxmt3GHRMLuRZEWhWLASTMSrrnVNWdVI/+pUElJBBBg==}
     engines: {node: '>= 10.0.0'}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@parcel/watcher-linux-x64-musl@2.4.1':
     resolution: {integrity: sha512-L2nZTYR1myLNST0O632g0Dx9LyMNHrn6TOt76sYxWLdff3cB22/GZX2UPtJnaqQPdCRoszoY5rcOj4oMTtp5fQ==}
     engines: {node: '>= 10.0.0'}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@parcel/watcher-win32-arm64@2.4.1':
     resolution: {integrity: sha512-Uq2BPp5GWhrq/lcuItCHoqxjULU1QYEcyjSO5jqqOK8RNFDBQnenMMx4gAl3v8GiWa59E9+uDM7yZ6LxwUIfRg==}
@@ -2453,21 +2476,25 @@ packages:
     resolution: {integrity: sha512-O8NqEgNKydAm/VFZfET1fPoGCQcxQR7q9XuAjUZxABP5dwiS1J0yalEUM0oI3l1fsM0EaCokl0065VC8vPMCJg==}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@rolldown/binding-linux-arm64-musl@0.10.4-snapshot-66e8482-20240616163535':
     resolution: {integrity: sha512-LsU7NeTB2H59+zk4NQDkW7zzf6czkvHVtpqeupNbd/G9lAeqoOgK+AWHWCkMGUh6O3KvjHPuS9RZ7kDdAOfojA==}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@rolldown/binding-linux-x64-gnu@0.10.4-snapshot-66e8482-20240616163535':
     resolution: {integrity: sha512-W3G5h1IOYtAacHd5+nxsrKyIeO2qEVE0Vkqncm9+N1g4jUEl0dre48/xfgU18glpgItPOz+PlSnhyNnh1lf/qA==}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@rolldown/binding-linux-x64-musl@0.10.4-snapshot-66e8482-20240616163535':
     resolution: {integrity: sha512-hyDX3VHA+gogB1fZMQvI2K0zMfXfLKv4RuSv59a6DoTM/ipSd2JAvOnpe9Pn9K5hXqduhHAtr+Yj6rTwWXqpGQ==}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@rolldown/binding-wasm32-wasi@0.10.4-snapshot-66e8482-20240616163535':
     resolution: {integrity: sha512-rD0xGpo2+BP0eQwE21ehg5Y8C7MRVSDvbl5MqFFuY3HX09DgUAp6y9MXFCtMmyl/PA794dDhQ7e2fUr3nwo1Ww==}
@@ -2576,46 +2603,55 @@ packages:
     resolution: {integrity: sha512-C/zbRYRXFjWvz9Z4haRxcTdnkPt1BtCkz+7RtBSuNmKzMzp3ZxdM28Mpccn6pt28/UWUCTXa+b0Mx1k3g6NOMA==}
     cpu: [arm]
     os: [linux]
+    libc: [glibc]
 
   '@rollup/rollup-linux-arm-musleabihf@4.18.0':
     resolution: {integrity: sha512-l3m9ewPgjQSXrUMHg93vt0hYCGnrMOcUpTz6FLtbwljo2HluS4zTXFy2571YQbisTnfTKPZ01u/ukJdQTLGh9A==}
     cpu: [arm]
     os: [linux]
+    libc: [musl]
 
   '@rollup/rollup-linux-arm64-gnu@4.18.0':
     resolution: {integrity: sha512-rJ5D47d8WD7J+7STKdCUAgmQk49xuFrRi9pZkWoRD1UeSMakbcepWXPF8ycChBoAqs1pb2wzvbY6Q33WmN2ftw==}
     cpu: [arm64]
     os: [linux]
+    libc: [glibc]
 
   '@rollup/rollup-linux-arm64-musl@4.18.0':
     resolution: {integrity: sha512-be6Yx37b24ZwxQ+wOQXXLZqpq4jTckJhtGlWGZs68TgdKXJgw54lUUoFYrg6Zs/kjzAQwEwYbp8JxZVzZLRepQ==}
     cpu: [arm64]
     os: [linux]
+    libc: [musl]
 
   '@rollup/rollup-linux-powerpc64le-gnu@4.18.0':
     resolution: {integrity: sha512-hNVMQK+qrA9Todu9+wqrXOHxFiD5YmdEi3paj6vP02Kx1hjd2LLYR2eaN7DsEshg09+9uzWi2W18MJDlG0cxJA==}
     cpu: [ppc64]
     os: [linux]
+    libc: [glibc]
 
   '@rollup/rollup-linux-riscv64-gnu@4.18.0':
     resolution: {integrity: sha512-ROCM7i+m1NfdrsmvwSzoxp9HFtmKGHEqu5NNDiZWQtXLA8S5HBCkVvKAxJ8U+CVctHwV2Gb5VUaK7UAkzhDjlg==}
     cpu: [riscv64]
     os: [linux]
+    libc: [glibc]
 
   '@rollup/rollup-linux-s390x-gnu@4.18.0':
     resolution: {integrity: sha512-0UyyRHyDN42QL+NbqevXIIUnKA47A+45WyasO+y2bGJ1mhQrfrtXUpTxCOrfxCR4esV3/RLYyucGVPiUsO8xjg==}
     cpu: [s390x]
     os: [linux]
+    libc: [glibc]
 
   '@rollup/rollup-linux-x64-gnu@4.18.0':
     resolution: {integrity: sha512-xuglR2rBVHA5UsI8h8UbX4VJ470PtGCf5Vpswh7p2ukaqBGFTnsfzxUBetoWBWymHMxbIG0Cmx7Y9qDZzr648w==}
     cpu: [x64]
     os: [linux]
+    libc: [glibc]
 
   '@rollup/rollup-linux-x64-musl@4.18.0':
     resolution: {integrity: sha512-LKaqQL9osY/ir2geuLVvRRs+utWUNilzdE90TpyoX0eNqPzWjRm14oMEE+YLve4k/NAqCdPkGYDaDF5Sw+xBfg==}
     cpu: [x64]
     os: [linux]
+    libc: [musl]
 
   '@rollup/rollup-win32-arm64-msvc@4.18.0':
     resolution: {integrity: sha512-7J6TkZQFGo9qBKH0pk2cEVSRhJbL6MtfWxth7Y5YmZs57Pi+4x6c2dStAUvaQkHQLnEQv1jzBUW43GvZW8OFqA==}
@@ -7566,6 +7602,13 @@ snapshots:
       magic-string: 0.30.10
     optionalDependencies:
       rollup: 3.29.4
+
+  '@rollup/plugin-replace@5.0.7(rollup@4.18.0)':
+    dependencies:
+      '@rollup/pluginutils': 5.1.0(rollup@4.18.0)
+      magic-string: 0.30.10
+    optionalDependencies:
+      rollup: 4.18.0
 
   '@rollup/pluginutils@5.1.0(rollup@3.29.4)':
     dependencies:

crates/rolldown/src/utils/fold_const_value.rs
@@ -1,0 +1,58 @@
+use oxc::allocator::Allocator;
+use oxc::ast::ast::{Expression, Program};
+use oxc::ast::{AstBuilder, VisitMut};
+use oxc::span::SPAN;
+use oxc::syntax::operator::BinaryOperator;
+
+/// This is a very basic version of binary expression folder, aiming to support converting `process.env.NODE_ENV === "production"` -> `true` or `false`.
+/// Used before dead branch elimination.
+pub struct BasicInlineBinaryValue<'a> {
+  ast: AstBuilder<'a>,
+}
+
+impl<'a> BasicInlineBinaryValue<'a> {
+  pub fn new(allocator: &'a Allocator) -> Self {
+    Self { ast: AstBuilder::new(allocator) }
+  }
+
+  pub fn build(&mut self, program: &mut Program<'a>) {
+    self.visit_program(program);
+  }
+
+  pub fn is_two_expr_equal_in_literal(
+    left: &Expression<'a>,
+    right: &Expression<'a>,
+  ) -> Option<bool> {
+    match (left, right) {
+      (Expression::BooleanLiteral(left), Expression::BooleanLiteral(right)) => {
+        Some(left.value == right.value)
+      }
+      (Expression::StringLiteral(left), Expression::StringLiteral(right)) => {
+        Some(left.value == right.value)
+      }
+      (Expression::NumericLiteral(left), Expression::NumericLiteral(right)) => {
+        // cargo clippy recommends use this method to compare float number instead of `==`
+        Some((left.value - right.value).abs() < f64::EPSILON)
+      }
+      (Expression::BigIntLiteral(left), Expression::BigIntLiteral(right)) => {
+        Some(left.raw == right.raw)
+      }
+      (Expression::NullLiteral(_), Expression::NullLiteral(_)) => Some(true),
+      _ => None,
+    }
+  }
+}
+
+impl<'a> VisitMut<'a> for BasicInlineBinaryValue<'a> {
+  fn visit_expression(&mut self, expr: &mut Expression<'a>) {
+    let Expression::BinaryExpression(bin) = expr else {
+      return;
+    };
+    if !matches!(bin.operator, BinaryOperator::Equality | BinaryOperator::StrictEquality) {
+      return;
+    }
+    if let Some(v) = Self::is_two_expr_equal_in_literal(&bin.left, &bin.right) {
+      *expr = Expression::BooleanLiteral(self.ast.alloc(self.ast.boolean_literal(SPAN, v)));
+    }
+  }
+}

crates/rolldown/src/utils/mod.rs
@@ -10,6 +10,7 @@ pub mod call_expression_ext;
 pub mod chunk;
 pub mod extract_hash_pattern;
 pub mod extract_meaningful_input_name_from_path;
+pub mod fold_const_value;
 pub mod hash_placeholder;
 pub mod load_source;
 pub mod make_ast_symbol_and_scope;

crates/rolldown/src/utils/pre_process_ast.rs
@@ -7,6 +7,7 @@ use rolldown_oxc_utils::OxcAst;
 
 use crate::types::oxc_parse_type::OxcParseType;
 
+use super::fold_const_value::BasicInlineBinaryValue;
 use super::tweak_ast_for_scanning::tweak_ast_for_scanning;
 
 // #[allow(clippy::match_same_arms)]: `OxcParseType::Tsx` will have special logic to deal with ts compared to `OxcParseType::Jsx`
@@ -47,6 +48,8 @@ pub fn pre_process_ast(
   }
 
   ast.program.with_mut(|fields| {
+    // Inline binary value . e.g. `process.env.NODE_ENV === "production"` -> `true` or `false`
+    BasicInlineBinaryValue::new(fields.allocator).build(fields.program);
     RemoveDeadCode::new(fields.allocator).build(fields.program);
   });
 

packages/rolldown/tests/fixtures/tree-shake/const-folding/_config.ts
@@ -1,0 +1,28 @@
+import type { RolldownOutputChunk } from 'rolldown'
+import { defineTest } from '@tests'
+import { expect } from 'vitest'
+
+export default defineTest({
+  config: {
+    plugins: [
+      {
+        transform: (code, id) => {
+          const res = code.replace(
+            'process.env.NODE_ENV',
+            JSON.stringify('development'),
+          )
+          return res
+        },
+      },
+    ],
+  },
+  afterTest: (output) => {
+    output.output
+      .filter(({ type }) => type === 'chunk')
+      .forEach((chunk) => {
+        let code = (chunk as RolldownOutputChunk).code
+        expect(code.includes(`production`)).toBe(false)
+        expect(code.includes(`development`)).toBe(true)
+      })
+  },
+})

commit 4750f555e66e871d7877f42a3e9764e75d91bb15

chore:  revert example basic vue (#1850)
<!-- Thank you for contributing! -->

### Description
1. the example is broken since
https://github.com/rolldown/rolldown/pull/1470/files#diff-e1477c04dd600c60e058bbf6b1d0dd47dd234120994bc23d0c3cf56efd5220e6,
now revert.
<!-- Please insert your description here and provide especially info
about the "what" this PR is solving -->

examples/basic-vue/index.js
@@ -1,5 +1,3 @@
-if (process.env.NODE_ENV === 'development') {
-  console.log('development')
-} else {
-  console.log('production')
-}
+import { createApp } from 'vue'
+
+export default createApp({})

examples/basic-vue/rolldown.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'rolldown'
-import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
   input: './index.js',
@@ -9,9 +8,4 @@ export default defineConfig({
     // aligns with Vite in the future.
     conditionNames: ['import'],
   },
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-  ],
 })

pnpm-lock.yaml
@@ -66,9 +66,6 @@ importers:
         specifier: ^3.4.21
         version: 3.4.34(typescript@5.5.4)
     devDependencies:
-      '@rollup/plugin-replace':
-        specifier: ^5.0.7
-        version: 5.0.7(rollup@4.19.1)
       rolldown:
         specifier: workspace:*
         version: link:../../packages/rolldown
@@ -7743,13 +7740,6 @@ snapshots:
       magic-string: 0.30.10
     optionalDependencies:
       rollup: 3.29.4
-
-  '@rollup/plugin-replace@5.0.7(rollup@4.19.1)':
-    dependencies:
-      '@rollup/pluginutils': 5.1.0(rollup@4.19.1)
-      magic-string: 0.30.10
-    optionalDependencies:
-      rollup: 4.19.1
 
   '@rollup/pluginutils@5.1.0(rollup@3.29.4)':
     dependencies:
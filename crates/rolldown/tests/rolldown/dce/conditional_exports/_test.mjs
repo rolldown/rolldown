d0feed7bfebbc15277ebf95653c7399f77ac18ee
Yunfei He <i.heyunfei@gmail.com>
2024-08-07 01:48:22 +0800

    test(rust/dce): add `conditional_exports` (#1883)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ crates/rolldown/tests/rolldown/dce/conditional_exports/_test.mjs
@@ -0,0 +1,9 @@
+import assert from 'node:assert'
+import nodeFs from 'node:fs'
+import nodePath from 'node:path'
+import { lib } from './dist/main.mjs'
+
+assert.strictEqual(lib, 'prod')
+const bundledFile = nodeFs.readFileSync(nodePath.resolve(import.meta.dirname, 'dist/main.mjs'))
+assert(bundledFile.includes('lib.prod.js'))
+assert(!bundledFile.includes('lib.dev.js'))
---
+++ crates/rolldown/tests/rolldown/dce/conditional_exports/lib.js
@@ -0,0 +1,5 @@
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./lib.prod');
+} else {
+  module.exports = require('./lib.dev');
+}

76d9798dedace1ae7a28ab7448604bb222b5fb39
shulaoda <laipichan@qq.com>
2024-08-09 18:42:17 +0800

    chore: follow some suggestions

--- crates/rolldown_common/src/types/js_regex.rs
+++ crates/rolldown_common/src/types/js_regex.rs
@@ -8,59 +8,26 @@ pub enum HybridRegex {
 }
 
 impl HybridRegex {
-  pub fn new(source: &str) -> anyhow::Result<Self> {
-    match regex::Regex::new(source).map(HybridRegex::Optimize) {
+  pub fn new(pattern: &str) -> anyhow::Result<Self> {
+    match regex::Regex::new(pattern).map(HybridRegex::Optimize) {
       Ok(reg) => Ok(reg),
-      Err(_) => regress::Regex::new(source).map(HybridRegex::Ecma).map_err(anyhow::Error::from),
+      Err(_) => regress::Regex::new(pattern).map(HybridRegex::Ecma).map_err(anyhow::Error::from),
     }
   }
 
-  pub fn with_flags(source: &str, chars: &str) -> anyhow::Result<Self> {
-    // Use an 8-bit bit mask to represent flags
-    // Each bit in the mask corresponds to a different flag
-    // If a flag appears multiple times, its bit will only be set once
-    let mut flags = 0u8;
-    for c in chars.bytes() {
+  pub fn with_flags(pattern: &str, flags: &str) -> anyhow::Result<Self> {
+    let mut result = String::new();
+    for c in flags.bytes() {
       match c {
-        b'i' => flags |= 1 << 0, // 0000_0001
-        b'm' => flags |= 1 << 1, // 0000_0010
-        b's' => flags |= 1 << 2, // 0000_0100
-        b'u' => flags |= 1 << 3, // 0000_1000
-        b'R' => flags |= 1 << 4, // 0001_0000
-        b'U' => flags |= 1 << 5, // 0010_0000
-        b'x' => flags |= 1 << 6, // 0100_0000
+        b'i' | b'm' | b's' | b'u' | b'R' | b'U' | b'x' => result.push(c as char),
         _ => {}
       }
     }
 
-    let mut result = String::new();
-    if flags & (1 << 0) != 0 {
-      result.push('i');
-    }
-    if flags & (1 << 1) != 0 {
-      result.push('m');
-    }
-    if flags & (1 << 2) != 0 {
-      result.push('s');
-    }
-    if flags & (1 << 3) != 0 {
-      result.push('u');
-    }
-    if flags & (1 << 4) != 0 {
-      result.push('R');
-    }
-    if flags & (1 << 5) != 0 {
-      result.push('U');
-    }
-    if flags & (1 << 6) != 0 {
-      result.push('x');
-    }
-
-    let pattern = if result.is_empty() { source } else { &format!("(?{result}){source}") };
-
-    match regex::Regex::new(pattern).map(HybridRegex::Optimize) {
+    let regex_pattern = if result.is_empty() { pattern } else { &format!("(?{result}){pattern}") };
+    match regex::Regex::new(regex_pattern).map(HybridRegex::Optimize) {
       Ok(reg) => Ok(reg),
-      Err(_) => regress::Regex::with_flags(source, chars)
+      Err(_) => regress::Regex::with_flags(pattern, flags)
         .map(HybridRegex::Ecma)
         .map_err(anyhow::Error::from),
     }

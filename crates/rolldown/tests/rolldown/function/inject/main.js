c5aafadfa5704cbe679bf6fb68e1810d08a642a3
Boshen <boshenc@gmail.com>
2024-08-11 14:14:10 +0800

    fix(rolldown): fix inject test where `alias` can be a member expression (#1934)
    ### Description
    
    In inject plugin https://www.npmjs.com/package/@rollup/plugin-inject,
    `imported` is optional and is defaulted to `default`
    https://github.com/rollup/plugins/blob/8550c4b1925b246adbd3af48ed0e5f74f822c951/packages/inject/src/index.js#L125
    
    Only `alias` can be a member expression.
    
    ---------
    
    Co-authored-by: Yunfei <i.heyunfei@gmail.com>

--- crates/rolldown/tests/rolldown/function/inject/main.js
+++ crates/rolldown/tests/rolldown/function/inject/main.js
@@ -4,5 +4,4 @@ assert.strictEqual(Promise, 'promise-shim')
 assert.strictEqual(P, 'promise-shim')
 assert.strictEqual($, 'jquery')
 assert.strictEqual(fs.default, 'node-fs')
-// FIXME: oxc injects invalid statements `import { default as 'Object.assign' from 'object-assign-shim'` }`, so it fails.
-// assert.strictEqual(Object.assign, 'object-assign-shim')
+assert.strictEqual(Object.assign, 'object-assign-shim')

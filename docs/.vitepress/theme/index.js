beaa7e65d57911fe1e48b1a2f55e46219d4acaa6
Leo <38490578+yuyinws@users.noreply.github.com>
2024-08-16 16:50:46 +0800

    docs: add code group icon (#1996)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    This PR add
    [vitepress-plugin-group-icons](https://github.com/yuyinws/vitepress-plugin-group-icons)
    to enhance the readability for `code-group`:
    
    #### Before
    
    ![before](https://github.com/user-attachments/assets/4133ad34-ac09-4efa-a529-4967e64ef9d5)
    
    #### After
    
    <img width="749" alt="image"
    src="https://github.com/user-attachments/assets/6aef390c-27a2-4ac4-9ae9-580fae89d68a">

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import { GroupIconComponent } from 'vitepress-plugin-group-icons/client'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.use(GroupIconComponent, {
+      homebrew: 'logos:homebrew',
+      cargo: 'vscode-icons:file-type-cargo',
+    })
+  },
+}

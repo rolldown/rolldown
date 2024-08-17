1af96d9ff4c1f7836c180d4c7d2a531f5c7b4db9
ikkz <ylei.mk@gmail.com>
2024-08-17 18:51:06 +0800

    feat: compose addon hooks

--- packages/rolldown/tests/compose-plugins.test.ts
+++ packages/rolldown/tests/compose-plugins.test.ts
@@ -20,4 +20,43 @@ describe('compose-plugins', () => {
     const composed = composePlugins(plugins)
     expect(composed.length).toBe(1)
   })
+
+  it('should compose `banner`, `footer`, `intro`, `outro` hooks', () => {
+    const plugins: Plugin[] = [
+      {
+        name: 'test-plugin-1',
+        banner: '/*banner1*/',
+      },
+      {
+        name: 'test-plugin-2',
+        banner: () => '/*banner2*/',
+      },
+      {
+        name: 'test-plugin-3',
+        footer: '/*footer1*/',
+      },
+      {
+        name: 'test-plugin-4',
+        footer: () => '/*footer2*/',
+      },
+      {
+        name: 'test-plugin-5',
+        intro: '/*intro1*/',
+      },
+      {
+        name: 'test-plugin-6',
+        intro: () => '/*intro2*/',
+      },
+      {
+        name: 'test-plugin-7',
+        outro: '/*outro1*/',
+      },
+      {
+        name: 'test-plugin-8',
+        outro: () => '/*outro2*/',
+      },
+    ]
+    const composed = composePlugins(plugins)
+    expect(composed.length).toBe(1)
+  })
 })

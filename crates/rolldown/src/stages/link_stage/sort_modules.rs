3b0665b2135a15e96a41ab6796dccd52342646c6
underfin <likui.underfin@gmail.com>
2024-08-30 16:34:03 +0800

    fix: debugger ci

--- crates/rolldown/src/stages/link_stage/sort_modules.rs
+++ crates/rolldown/src/stages/link_stage/sort_modules.rs
@@ -120,6 +120,13 @@ impl<'a> LinkStage<'a> {
       }
     }
 
+    println!(
+      "sorted_modules: {:?}",
+      sorted_modules
+        .iter()
+        .map(|idx| self.module_table.modules[*idx].as_ecma().unwrap().id.to_string())
+        .collect::<Vec<_>>()
+    );
     self.sorted_modules = sorted_modules;
     debug_assert_eq!(
       self.sorted_modules.first().copied(),

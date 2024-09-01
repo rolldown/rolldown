723cf99d92ab3af3421c6e62ff12f4dd409e36ec
Yunfei He <i.heyunfei@gmail.com>
2024-09-02 03:04:38 +0800

    refactor(rust/code-splitting): add `ChunkGraph#add_module_to_chunk` (#2114)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/src/chunk_graph.rs
+++ crates/rolldown/src/chunk_graph.rs
@@ -1,9 +1,9 @@
 use arcstr::ArcStr;
-use oxc::index::IndexVec;
-use rolldown_common::{Chunk, ChunkIdx, ChunkTable, ModuleIdx};
+use oxc::index::{index_vec, IndexVec};
+use rolldown_common::{Chunk, ChunkIdx, ChunkTable, ModuleIdx, ModuleTable};
 use rustc_hash::FxHashMap;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ChunkGraph {
   pub chunk_table: ChunkTable,
   pub sorted_chunk_idx_vec: Vec<ChunkIdx>,
@@ -14,6 +14,16 @@ pub struct ChunkGraph {
 }
 
 impl ChunkGraph {
+  pub fn new(module_table: &ModuleTable) -> Self {
+    Self {
+      chunk_table: ChunkTable::default(),
+      module_to_chunk: index_vec![None; module_table.modules.len()],
+      sorted_chunk_idx_vec: Vec::new(),
+      entry_module_to_entry_chunk: FxHashMap::default(),
+      chunk_idx_by_name: FxHashMap::default(),
+    }
+  }
+
   #[allow(unused)]
   pub fn sorted_chunks(&self) -> impl Iterator<Item = &Chunk> {
     self.sorted_chunk_idx_vec.iter().map(move |&id| &self.chunk_table.chunks[id])
@@ -30,5 +40,10 @@ impl ChunkGraph {
       self.chunk_idx_by_name.insert(name.clone(), idx);
     }
     idx
+  }
+
+  pub fn add_module_to_chunk(&mut self, module_idx: ModuleIdx, chunk_idx: ChunkIdx) {
+    self.chunk_table.chunks[chunk_idx].modules.push(module_idx);
+    self.module_to_chunk[module_idx] = Some(chunk_idx);
   }
 }

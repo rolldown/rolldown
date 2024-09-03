37ec3d2bf0e19f86d2b158f3599e99bdc9d12684
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-09-03 19:45:39 +0800

    refactor: 💡 transform plugin config

--- packages/rolldown/src/options/normalized-ecma-transform-plugin-config.ts
+++ packages/rolldown/src/options/normalized-ecma-transform-plugin-config.ts
@@ -1,7 +1,7 @@
-import { isRegExp } from 'node:util/types'
 import { BindingTransformPluginConfig } from '../binding'
+import { normalizedStringOrRegex } from './utils'
 
-type TransformPattern = string | RegExp | RegExp[] | string[]
+type TransformPattern = string | RegExp | (RegExp | string)[]
 // A temp config type for giving better user experience
 export type TransformPluginConfig = Omit<
   BindingTransformPluginConfig,
@@ -19,44 +19,9 @@ export function normalizeEcmaTransformPluginConfig(
   }
   let normalizedConfig: BindingTransformPluginConfig = {
     jsxInject: config?.jsxInject,
+    exclude: normalizedStringOrRegex(config.exclude),
+    include: normalizedStringOrRegex(config.include),
   }
 
-  if (config?.exclude) {
-    let exclude: (string | RegExp)[] = []
-    if (isRegExp(config.exclude)) {
-      exclude = [config.exclude]
-    } else if (typeof config.exclude === 'string') {
-      exclude = [config.exclude]
-    } else {
-      exclude = config.exclude
-    }
-    normalizedConfig.exclude = []
-    for (let item of exclude) {
-      if (isRegExp(item)) {
-        normalizedConfig.exclude.push({ value: item.source, flag: item.flags })
-      } else {
-        normalizedConfig.exclude.push({ value: item })
-      }
-    }
-  }
-
-  if (config?.include) {
-    let include: (string | RegExp)[] = []
-    if (isRegExp(config.include)) {
-      include = [config.include]
-    } else if (typeof config.include === 'string') {
-      include = [config.include]
-    } else {
-      include = config.include
-    }
-    normalizedConfig.include = []
-    for (let item of include) {
-      if (isRegExp(item)) {
-        normalizedConfig.include.push({ value: item.source, flag: item.flags })
-      } else {
-        normalizedConfig.include.push({ value: item })
-      }
-    }
-  }
   return normalizedConfig
 }

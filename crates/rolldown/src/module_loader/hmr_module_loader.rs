4ed23632140488d7d6f5e1e4291eae5f27c91083
underfin <likui.underfin@gmail.com>
2024-09-03 19:29:40 +0800

    feat: store pervious build module info and using it at next hmr module loader

--- crates/rolldown/src/module_loader/hmr_module_loader.rs
+++ crates/rolldown/src/module_loader/hmr_module_loader.rs
@@ -1,11 +1,8 @@
 use super::module_task::{ModuleTask, ModuleTaskOwner};
-use super::runtime_module_task::RuntimeModuleTask;
 use super::task_context::TaskContextMeta;
 use super::task_result::NormalModuleTaskResult;
 use super::Msg;
-use crate::module_loader::runtime_module_task::RuntimeModuleTaskResult;
 use crate::module_loader::task_context::TaskContext;
-use crate::runtime::{RuntimeModuleBrief, RUNTIME_MODULE_ID};
 use crate::type_alias::IndexEcmaAst;
 use crate::types::symbols::Symbols;
 use arcstr::ArcStr;
@@ -14,76 +11,68 @@ use oxc::minifier::ReplaceGlobalDefinesConfig;
 use oxc::span::Span;
 use rolldown_common::side_effects::{DeterminedSideEffects, HookSideEffects};
 use rolldown_common::{
-  EntryPoint, EntryPointKind, ExternalModule, ImportKind, ImportRecordIdx, ImporterRecord, Module,
-  ModuleIdx, ModuleTable, ResolvedId,
+  ExternalModule, ImportRecordIdx, Module, ModuleDefFormat, ModuleIdx, ModuleTable, ResolvedId,
 };
 use rolldown_error::{BuildDiagnostic, DiagnosableResult};
 use rolldown_fs::OsFileSystem;
 use rolldown_plugin::SharedPluginDriver;
-use rolldown_utils::rustc_hash::FxHashSetExt;
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::FxHashMap;
 use std::sync::Arc;
 
 use crate::{SharedOptions, SharedResolver};
 
-pub struct IntermediateNormalModules {
+pub struct HmrIntermediateNormalModules {
   pub modules: IndexVec<ModuleIdx, Option<Module>>,
-  pub importers: IndexVec<ModuleIdx, Vec<ImporterRecord>>,
   pub index_ecma_ast: IndexEcmaAst,
 }
 
-impl IntermediateNormalModules {
-  pub fn new() -> Self {
+impl HmrIntermediateNormalModules {
+  pub fn new(last_module_table: ModuleTable, index_ecma_ast: IndexEcmaAst) -> Self {
     Self {
-      modules: IndexVec::new(),
-      importers: IndexVec::new(),
-      index_ecma_ast: IndexVec::default(),
+      modules: last_module_table.modules.into_iter().map(Some).collect::<IndexVec<_, _>>(),
+      index_ecma_ast,
     }
   }
 
   pub fn alloc_ecma_module_idx(&mut self, symbols: &mut Symbols) -> ModuleIdx {
     let id = self.modules.push(None);
-    self.importers.push(Vec::new());
     symbols.alloc_one();
     id
   }
 }
 
-pub struct ModuleLoader {
+pub struct HmrModuleLoader {
   options: SharedOptions,
   shared_context: Arc<TaskContext>,
   rx: tokio::sync::mpsc::Receiver<Msg>,
   visited: FxHashMap<ArcStr, ModuleIdx>,
-  runtime_id: ModuleIdx,
   remaining: u32,
-  intermediate_normal_modules: IntermediateNormalModules,
+  intermediate_normal_modules: HmrIntermediateNormalModules,
   symbols: Symbols,
 }
 
-pub struct ModuleLoaderOutput {
+pub struct HmrModuleLoaderOutput {
   // Stored all modules
   pub module_table: ModuleTable,
   pub module_id_to_modules: FxHashMap<ArcStr, ModuleIdx>,
   pub index_ecma_ast: IndexEcmaAst,
-  pub symbols: Symbols,
-  // Entries that user defined + dynamic import entries
-  pub entry_points: Vec<EntryPoint>,
-  pub runtime: RuntimeModuleBrief,
+  //   pub symbols: Symbols,
   pub warnings: Vec<BuildDiagnostic>,
 }
 
-impl ModuleLoader {
+impl HmrModuleLoader {
   pub fn new(
     options: SharedOptions,
     plugin_driver: SharedPluginDriver,
     fs: OsFileSystem,
     resolver: SharedResolver,
+    last_module_id_to_modules: FxHashMap<ArcStr, ModuleIdx>,
+    last_module_table: ModuleTable,
+    last_index_ecma_ast: IndexEcmaAst,
   ) -> anyhow::Result<Self> {
     // 1024 should be enough for most cases
     // over 1024 pending tasks are insane
     let (tx, rx) = tokio::sync::mpsc::channel::<Msg>(1024);
-
-    let tx_to_runtime_module = tx.clone();
 
     let meta = TaskContextMeta {
       replace_global_define_config: if options.define.is_empty() {
@@ -109,32 +98,16 @@ impl ModuleLoader {
       meta,
     });
 
-    let mut intermediate_normal_modules = IntermediateNormalModules::new();
-    let mut symbols = Symbols::default();
-    let runtime_id = intermediate_normal_modules.alloc_ecma_module_idx(&mut symbols);
-
-    let task = RuntimeModuleTask::new(runtime_id, tx_to_runtime_module);
-
-    #[cfg(target_family = "wasm")]
-    {
-      task.run().unwrap();
-    }
-    // task is sync, but execution time is too short at the moment
-    // so we are using spawn instead of spawn_blocking here to avoid an additional blocking thread creation within tokio
-    #[cfg(not(target_family = "wasm"))]
-    {
-      let handle = tokio::runtime::Handle::current();
-      handle.spawn(async { task.run() });
-    }
+    let intermediate_normal_modules =
+      HmrIntermediateNormalModules::new(last_module_table, last_index_ecma_ast);
+    let symbols = Symbols::default();
 
     Ok(Self {
       shared_context: common_data,
       rx,
       options,
-      visited: FxHashMap::from_iter([(RUNTIME_MODULE_ID.into(), runtime_id)]),
-      runtime_id,
-      // runtime module is always there
-      remaining: 1,
+      visited: last_module_id_to_modules,
+      remaining: 0,
       intermediate_normal_modules,
       symbols,
     })
@@ -200,39 +173,48 @@ impl ModuleLoader {
   }
 
   #[tracing::instrument(level = "debug", skip_all)]
-  pub async fn fetch_all_modules(
+  pub async fn fetch_changed_modules(
     mut self,
-    user_defined_entries: Vec<(Option<ArcStr>, ResolvedId)>,
-  ) -> anyhow::Result<DiagnosableResult<ModuleLoaderOutput>> {
+    changed_modules: Vec<String>,
+  ) -> anyhow::Result<DiagnosableResult<HmrModuleLoaderOutput>> {
     if self.options.input.is_empty() {
       return Err(anyhow::format_err!("You must supply options.input to rolldown"));
     }
 
+    // spawn valid changed modules
+    changed_modules
+      .into_iter()
+      .filter_map(|m| self.visited.get(m.as_str()).map(|idx| (m, idx)))
+      .for_each(|(m, idx)| {
+        self.remaining += 1;
+
+        let task = ModuleTask::new(
+          Arc::clone(&self.shared_context),
+          *idx,
+          ResolvedId {
+            id: m.into(),
+            ignored: false,
+            module_def_format: ModuleDefFormat::Unknown,
+            is_external: false,
+            package_json: None,
+            side_effects: None,
+          },
+          None,
+        );
+        #[cfg(target_family = "wasm")]
+        {
+          let handle = tokio::runtime::Handle::current();
+          // could not block_on/spawn the main thread in WASI
+          std::thread::spawn(move || {
+            handle.spawn(task.run());
+          });
+        }
+        #[cfg(not(target_family = "wasm"))]
+        tokio::spawn(task.run());
+      });
+
     let mut errors = vec![];
     let mut all_warnings: Vec<BuildDiagnostic> = vec![];
-
-    let entries_count = user_defined_entries.len() + /* runtime */ 1;
-    self.intermediate_normal_modules.modules.reserve(entries_count);
-    self.intermediate_normal_modules.index_ecma_ast.reserve(entries_count);
-
-    // Store the already consider as entry module
-    let mut user_defined_entry_ids = FxHashSet::with_capacity(user_defined_entries.len());
-
-    let mut entry_points = user_defined_entries
-      .into_iter()
-      .map(|(name, info)| EntryPoint {
-        name,
-        id: self.try_spawn_new_task(info, /* is_user_defined_entry */ None),
-        kind: EntryPointKind::UserDefined,
-      })
-      .inspect(|e| {
-        user_defined_entry_ids.insert(e.id);
-      })
-      .collect::<Vec<_>>();
-
-    let mut dynamic_import_entry_ids = FxHashSet::default();
-
-    let mut runtime_brief: Option<RuntimeModuleBrief> = None;
 
     while self.remaining > 0 {
       let Some(msg) = self.rx.recv().await else {
@@ -262,16 +244,6 @@ impl ModuleLoader {
                   Span::new(raw_rec.module_request_start, raw_rec.module_request_end()),
                 );
                 let id = self.try_spawn_new_task(info, Some(owner));
-                // Dynamic imported module will be considered as an entry
-                self.intermediate_normal_modules.importers[id].push(ImporterRecord {
-                  kind: raw_rec.kind,
-                  importer_path: module.id().to_string().into(),
-                });
-                if matches!(raw_rec.kind, ImportKind::DynamicImport)
-                  && !user_defined_entry_ids.contains(&id)
-                {
-                  dynamic_import_entry_ids.insert(id);
-                }
                 raw_rec.into_import_record(id)
               })
               .collect::<IndexVec<ImportRecordIdx, _>>();
@@ -284,14 +256,8 @@ impl ModuleLoader {
           }
           self.intermediate_normal_modules.modules[module_idx] = Some(module);
         }
-        Msg::RuntimeNormalModuleDone(task_result) => {
-          let RuntimeModuleTaskResult { ast_symbols, mut module, runtime, ast } = task_result;
-          let ast_idx = self.intermediate_normal_modules.index_ecma_ast.push((ast, module.idx));
-          module.ecma_ast_idx = Some(ast_idx);
-          self.intermediate_normal_modules.modules[self.runtime_id] = Some(module.into());
-
-          self.symbols.add_ast_symbols(self.runtime_id, ast_symbols);
-          runtime_brief = Some(runtime);
+        Msg::RuntimeNormalModuleDone(_) => {
+          unreachable!("Runtime module should not be done at hmr module loader");
         }
         Msg::BuildErrors(e) => {
           errors.extend(e);
@@ -318,47 +284,14 @@ impl ModuleLoader {
       return Ok(Err(errors));
     }
 
-    let modules: IndexVec<ModuleIdx, Module> = self
-      .intermediate_normal_modules
-      .modules
-      .into_iter()
-      .flatten()
-      .enumerate()
-      .map(|(id, mut module)| {
-        if let Some(module) = module.as_ecma_mut() {
-          // Note: (Compat to rollup)
-          // The `dynamic_importers/importers` should be added after `module_parsed` hook.
-          for importer in std::mem::take(&mut self.intermediate_normal_modules.importers[id]) {
-            if importer.kind.is_static() {
-              module.importers.push(importer.importer_path);
-            } else {
-              module.dynamic_importers.push(importer.importer_path);
-            }
-          }
-        }
-        module
-      })
-      .collect();
+    let modules: IndexVec<ModuleIdx, Module> =
+      self.intermediate_normal_modules.modules.into_iter().flatten().collect();
 
-    // if `inline_dynamic_imports` is set to be true, here we should not put dynamic imports to entries
-    if !self.options.inline_dynamic_imports {
-      let mut dynamic_import_entry_ids = dynamic_import_entry_ids.into_iter().collect::<Vec<_>>();
-      dynamic_import_entry_ids.sort_unstable_by_key(|id| modules[*id].stable_id());
-
-      entry_points.extend(dynamic_import_entry_ids.into_iter().map(|id| EntryPoint {
-        name: None,
-        id,
-        kind: EntryPointKind::DynamicImport,
-      }));
-    }
-
-    Ok(Ok(ModuleLoaderOutput {
+    Ok(Ok(HmrModuleLoaderOutput {
       module_table: ModuleTable { modules },
       module_id_to_modules: self.visited,
-      symbols: self.symbols,
+      //   symbols: self.symbols,
       index_ecma_ast: self.intermediate_normal_modules.index_ecma_ast,
-      entry_points,
-      runtime: runtime_brief.expect("Failed to find runtime module. This should not happen"),
       warnings: all_warnings,
     }))
   }

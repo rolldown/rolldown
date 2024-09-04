4992f66de98e36c917e29c68a015067bda861f60
underfin <likui.underfin@gmail.com>
2024-09-04 19:53:27 +0800

    chore: disable IsolatingModuleFinalizer (#2149)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/src/module_finalizers/isolating/mod.rs
+++ crates/rolldown/src/module_finalizers/isolating/mod.rs
@@ -4,11 +4,13 @@ use rolldown_ecmascript::AstSnippet;
 
 mod impl_visit_mut;
 
+#[allow(dead_code)]
 pub struct IsolatingModuleFinalizerContext<'me> {
   pub module: &'me EcmaModule,
   pub modules: &'me IndexModules,
 }
 
+#[allow(dead_code)]
 pub struct IsolatingModuleFinalizer<'me, 'ast> {
   pub ctx: &'me IsolatingModuleFinalizerContext<'me>,
   // pub scope: &'me AstScopes,

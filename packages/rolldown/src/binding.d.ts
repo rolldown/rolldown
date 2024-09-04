9e2f4bf1fc343b7d721a882c5ea38d438f0354a9
underfin <likui.underfin@gmail.com>
2024-09-04 19:52:39 +0800

    feat: enable app format at js side (#2148)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- packages/rolldown/src/binding.d.ts
+++ packages/rolldown/src/binding.d.ts
@@ -274,7 +274,7 @@ export interface BindingOutputOptions {
   extend?: boolean
   externalLiveBindings?: boolean
   footer?: (chunk: RenderedChunk) => MaybePromise<VoidNullable<string>>
-  format?: 'es' | 'cjs' | 'iife'
+  format?: 'es' | 'cjs' | 'iife' | 'app'
   globals?: Record<string, string>
   inlineDynamicImports?: boolean
   intro?: (chunk: RenderedChunk) => MaybePromise<VoidNullable<string>>

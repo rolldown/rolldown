e5025f4f58a985a32f71424798178ddb3fd5204a
underfin <likui.underfin@gmail.com>
2024-09-05 10:59:14 +0800

    feat: add react-refresh and make transform to commonjs work (#2150)
    <!-- Thank you for contributing! -->
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- examples/react-hmr/rolldown.config.js
+++ examples/react-hmr/rolldown.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'rolldown'
 import { babel } from '@rollup/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   input: './main.js',
+  plugins: [
+    react(),
+    babel({
+      extensions: ['.js', '.jsx', ''],
+      include: ['/@react-refresh', '*.js', '*.jsx'],
+      babelHelpers: 'inline',
+      skipPreflightCheck: true,
+      presets: ['@babel/preset-react'],
+      plugins: ['@babel/plugin-transform-modules-commonjs'],
+    }),
+  ],
   output: {
     format: 'app',
-    plugins: [
-      babel({
-        babelHelpers: 'bundled',
-        plugins: ['@babel/plugin-transform-modules-commonjs'],
-      }),
-    ],
   },
 })

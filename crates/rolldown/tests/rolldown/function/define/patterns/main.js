e23f4ffbd0fd0c0e82043a595c1709001b084869
Boshen <boshenc@gmail.com>
2024-09-11 22:03:49 +0800

    update

--- crates/rolldown/tests/rolldown/function/define/patterns/main.js
+++ crates/rolldown/tests/rolldown/function/define/patterns/main.js
@@ -5,3 +5,9 @@ const objProp = Obj.prop
 
 assert.strictEqual(id, 'ok')
 assert.strictEqual(objProp, 'ok')
+
+// It should not inject shadowed variables.
+;(function (Id, Obj) {
+  assert.strictEqual(Id, undefined)
+  assert.strictEqual(Obj.prop, undefined)
+})(undefined, {});
--- packages/rolldown/tests/fixtures/function/define/main.js
+++ packages/rolldown/tests/fixtures/function/define/main.js
@@ -4,6 +4,6 @@ console.log(process.env.NODE_ENV);
 assert.strictEqual(process.env.NODE_ENV, "production");
 
 // It should not define shadowed variables.
-(function (process) {
-  assert.strictEqual(process.env.NODE_ENV, undefined);
-})({ process: { env: {} } });
+;(function (process) {
+  assert.strictEqual(process.env.NODE_ENV, undefined)
+})({ env: {} })

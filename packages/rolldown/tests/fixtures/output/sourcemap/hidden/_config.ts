e5b7bc0bb7027a211b8e111987849bbfbcf90c9f
underfin <likui.underfin@gmail.com>
2024-09-14 16:36:00 +0800

    fix: update test

--- packages/rolldown/tests/fixtures/output/sourcemap/hidden/_config.ts
+++ packages/rolldown/tests/fixtures/output/sourcemap/hidden/_config.ts
@@ -11,12 +11,7 @@ export default defineTest({
     },
   },
   afterTest: function (output) {
-    expect(getOutputFileNames(output)).toMatchInlineSnapshot(`
-      [
-        "main.js",
-        "main.js.map",
-      ]
-    `)
+    expect(getOutputFileNames(output)).toStrictEqual(['main.js', 'main.js.map'])
     // not include map comment
     expect(output.output[0].code).not.contains('//# sourceMappingURL=')
     expect(output.output[0].sourcemapFileName).toBe('main.js.map')

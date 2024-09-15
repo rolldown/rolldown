4c74eb9701167c787421e44b8bcccafe9d645590
coderwei <916606569@qq.com>
2024-09-15 14:44:34 +0800

    fix: Match hash correctly

--- crates/rolldown_common/src/inner_bundler_options/types/filename_template.rs
+++ crates/rolldown_common/src/inner_bundler_options/types/filename_template.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 #[derive(Debug)]
 pub struct FilenameTemplate {
   template: String,
@@ -33,7 +35,8 @@ impl FilenameTemplate {
       tmp = tmp.replace("[name]", name);
     }
     if let Some(hash) = options.hash {
-      tmp = tmp.replace("[hash]", hash);
+      let re = Regex::new(r"\[hash(?::(\d+))?\]").unwrap();
+      tmp = re.replace(&tmp, hash).to_string();
     }
     if let Some(ext) = options.ext {
       tmp = tmp.replace("[ext]", ext).replace("[extname]", &format!(".{ext}"));
@@ -43,6 +46,9 @@ impl FilenameTemplate {
 }
 
 #[test]
-fn basic() {
-  FilenameTemplate::new("[name]-[hash:8].js".to_string());
+fn test_basic_replacement() {
+  let template = FilenameTemplate::new("[name]-[hash:8].js".to_string());
+  let options = FileNameRenderOptions { name: Some("file"), hash: Some("abcdef12"), ext: None };
+  let result = template.render(&options);
+  assert_eq!(result, "file-abcdef12.js");
 }

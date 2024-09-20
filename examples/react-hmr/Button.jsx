6b67da51b9844a1932d29f12346e6ddc64a4a693
underfin <likui.underfin@gmail.com>
2024-09-20 16:55:24 +0800

    chore: remove unless change

--- examples/react-hmr/Button.jsx
+++ examples/react-hmr/Button.jsx
@@ -5,7 +5,7 @@ export default function Button() {
   return (
     <p>
       <button id="state-button" onClick={() => setCount((count) => count + 1)}>
-        count is: {count}12332123521
+        count is: {count}
       </button>
     </p>
   )

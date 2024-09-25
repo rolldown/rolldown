4ca8668a0db0e5d7b0e5637cfc3efefcf89257fe
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-09-26 00:13:35 +0800

    fix: 🐛 fmt

--- packages/rolldown/tests/fixtures/builtin-plugin/glob-import/absolute-glob-expr/assert.mjs
+++ packages/rolldown/tests/fixtures/builtin-plugin/glob-import/absolute-glob-expr/assert.mjs
@@ -2,7 +2,6 @@
 import assert from 'node:assert'
 import { m1, m2, m3 } from './dist/main'
 
-
 assert.strictEqual(m1['./dir/a.js'].default, 'a')
 assert.strictEqual(m2['./dir/a.js'].default, 'a')
 

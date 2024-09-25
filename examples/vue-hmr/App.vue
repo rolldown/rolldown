6d2b8958269d7ddd43e34c7d4731ae02b5314092
underfin <likui.underfin@gmail.com>
2024-09-25 14:23:09 +0800

    feat: add vue hmr example (#2291)
    <!-- Thank you for contributing! -->
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ examples/vue-hmr/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div className="App">
+    <header className="App-header">
+      <h1>Hello Rolldown + Vue</h1>
+      <p>Edit <code>App.vue</code> and save to test HMR updates.1</p>
+      <a
+        className="App-link"
+        href="https://vuejs.org/guide/introduction.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Vue
+      </a>
+    </header>
+  </div>
+</template>
---
+++ examples/vue-hmr/rolldown.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'rolldown'
+import vuePlugin from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  input: './main.js',
+  define: {
+    'process.env.NODE_ENV': '"development"',
+  },
+  plugins: [
+    vuePlugin(),
+    {
+      name: 'emit-html',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'index.html',
+          source: `<div id="app"></div><script src="./main.js"></script>`,
+        })
+      },
+    },
+  ],
+  output: {
+    format: 'app',
+  },
+  dev: true,
+})

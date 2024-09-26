4aeb79b2b9511da41ca61428f64f269be8b52214
Alice Peng <1399789151@qq.com>
2024-09-26 17:18:48 +0800

    feat: expose `resolve#extensionAlias` option

--- crates/rolldown_common/src/inner_bundler_options/types/resolve_options.rs
+++ crates/rolldown_common/src/inner_bundler_options/types/resolve_options.rs
@@ -17,6 +17,7 @@ pub struct ResolveOptions {
   pub condition_names: Option<Vec<String>>,
   pub exports_fields: Option<Vec<Vec<String>>>,
   pub extensions: Option<Vec<String>>,
+  pub extension_alias: Option<Vec<(String, Vec<String>)>>,
   pub main_fields: Option<Vec<String>>,
   pub main_files: Option<Vec<String>>,
   pub modules: Option<Vec<String>>,

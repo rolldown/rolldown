f9e2295b8cc79c55bfa88b720b7ae99f2d2039b8
Alice Peng <1399789151@qq.com>
2024-09-29 21:45:43 +0800

    feat: update result of extensionAlias test on node side

--- packages/rolldown/tests/fixtures/resolve/extension_alias/_config.ts
+++ packages/rolldown/tests/fixtures/resolve/extension_alias/_config.ts
@@ -1,7 +1,10 @@
 import { defineTest } from '@tests'
+import path from 'node:path'
+const entry = path.join(__dirname, './main.ts')
 
 export default defineTest({
   config: {
+    input: entry,
     resolve: {
       extensionAlias: { '.ts': ['.ts', '.js'] },
     },

88bcdcf140da7b729cf2a739d8f60bcd62e4cfe8
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-10-04 20:21:15 +0800

    chore: enable esbuild packagejson test (#2370)
    <!-- Thank you for contributing! -->
    
    ### Description
    1. enable all tests prefixy with `.` in `esbuild packagejson`
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- scripts/snap-diff/index.ts
+++ scripts/snap-diff/index.ts
@@ -1,6 +1,11 @@
+// cSpell:ignore packagejson
 import { run } from './runner'
 
 const args = process.argv.slice(2)
 const debug = args.includes('--debug')
-const includeList = ['snapshots_importstar.txt', 'snapshots_default.txt']
+const includeList = [
+  'snapshots_importstar.txt',
+  'snapshots_default.txt',
+  'snapshots_packagejson.txt',
+]
 run(includeList, debug)

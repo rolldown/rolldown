248125117f048b113bb9cd3d23b7d318ccb8aefc
Yunfei He <i.heyunfei@gmail.com>
2024-10-05 14:57:52 +0800

    feat(rust): add `rolldown_std_utils` crate to store utils/extensions for the rust std library (#2372)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    `rolldown_utils` is kind of swell. We need a crate to store utils code
    that is `std` only. These code would be used widely, so it would be
    better don't put much third-party dependecies in `rolldown_std_utils` as
    `rolldown_utils` did.
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ crates/rolldown_std_utils/src/option_ext.rs
@@ -0,0 +1,34 @@
+use crate::pretty_type_name;
+
+pub trait OptionExt<T> {
+  fn unpack(self) -> T;
+
+  fn unpack_ref(&self) -> &T;
+}
+
+impl<T> OptionExt<T> for Option<T> {
+  /// Similar to `unwrap`, but with a more descriptive panic message.
+  ///
+  /// ```ignore
+  /// None::<usize>.unwrap();
+  /// // called `Option::unwrap()` on a `None` value
+  ///
+  /// None::<usize>.unpack();
+  /// // Got `None` value when calling `OptionExt::unpack()` on `Option<usize>`
+  /// ```
+  #[track_caller]
+  fn unpack(self) -> T {
+    match self {
+      Some(v) => v,
+      None => panic!(
+        "Got `None` value when calling `OptionExt::unpack()` on `{type_name}`",
+        type_name = pretty_type_name::<Self>()
+      ),
+    }
+  }
+
+  /// Shorthand for `self.as_ref().unpack()`.
+  fn unpack_ref(&self) -> &T {
+    self.as_ref().unpack()
+  }
+}

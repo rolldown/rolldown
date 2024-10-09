22a8afec82ff17bb0ec74ed1d9687325c8e3e3d0
shulaoda <laipichan@qq.com>
2024-10-10 01:11:13 +0800

    finish

--- crates/rolldown_binding/src/options/binding_output_options/types/binding_advanced_chunks_options.rs
+++ crates/rolldown_binding/src/options/binding_output_options/types/binding_advanced_chunks_options.rs
@@ -1,5 +1,8 @@
 use derivative::Derivative;
+use napi::Either;
 use serde::Deserialize;
+
+use crate::options::plugin::types::binding_js_or_regex::JsRegExp;
 
 #[napi_derive::napi(object, object_to_js = false)]
 #[derive(Deserialize, Derivative)]
@@ -17,7 +20,9 @@ pub struct BindingAdvancedChunksOptions {
 #[derivative(Debug)]
 pub struct BindingMatchGroup {
   pub name: String,
-  pub test: Option<String>,
+  #[napi(ts_type = "RegExp | String")]
+  #[serde(skip_deserializing)]
+  pub test: Option<Either<JsRegExp, String>>,
   // pub share_count: Option<u32>,
   pub priority: Option<u32>,
   pub min_size: Option<f64>,
--- packages/rolldown/src/binding.d.ts
+++ packages/rolldown/src/binding.d.ts
@@ -269,7 +269,7 @@ export interface BindingManifestPluginConfig {
 
 export interface BindingMatchGroup {
   name: string
-  test?: string
+  test?: RegExp | String
   priority?: number
   minSize?: number
   minShareCount?: number

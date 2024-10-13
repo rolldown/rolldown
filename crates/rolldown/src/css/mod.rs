c5e829d4b3016bc7623de4930f5df7520e60f9ce
Yunfei <i.heyunfei@gmail.com>
2024-10-14 02:26:17 +0800

    refactor(css): replace `lightningcss` with `css-module-lexer`

--- crates/rolldown/src/css/mod.rs
+++ crates/rolldown/src/css/mod.rs
@@ -1,13 +1,7 @@
 pub mod css_generator;
 
-use std::convert::Infallible;
-
 use arcstr::ArcStr;
-use lightningcss::{
-  stylesheet::{ParserOptions, StyleSheet},
-  visit_types,
-  visitor::{Visit, VisitTypes, Visitor},
-};
+
 use oxc::{
   index::{Idx, IndexVec},
   semantic::SymbolId,
@@ -17,47 +11,27 @@ use rolldown_common::{
 };
 
 pub fn create_css_view(
-  id: String,
+  _id: &str,
   source: &ArcStr,
-) -> anyhow::Result<(CssView, IndexVec<ImportRecordIdx, RawImportRecord>)> {
-  let options = ParserOptions { filename: id, ..Default::default() };
-  let mut stylesheet =
-    StyleSheet::parse(source, options).map_err(lightningcss::error::Error::into_owned)?;
-  let mut scanner = CssAstScanner::default();
+) -> (CssView, IndexVec<ImportRecordIdx, RawImportRecord>) {
+  let (lexed_deps, _warnings) =
+    css_module_lexer::collect_dependencies(source, css_module_lexer::Mode::Css);
 
-  stylesheet.visit(&mut scanner)?;
+  let mut dependencies: IndexVec<ImportRecordIdx, RawImportRecord> = IndexVec::default();
 
-  Ok((
-    CssView { source: source.clone(), import_records: IndexVec::default() },
-    scanner.dependencies,
-  ))
-}
-
-#[derive(Default)]
-pub struct CssAstScanner {
-  pub dependencies: IndexVec<ImportRecordIdx, RawImportRecord>,
-}
-
-impl<'i> Visitor<'i> for CssAstScanner {
-  type Error = Infallible;
-
-  fn visit_types(&self) -> VisitTypes {
-    visit_types!(URLS | RULES)
-  }
-
-  fn visit_rule(&mut self, rule: &mut lightningcss::rules::CssRule<'i>) -> Result<(), Self::Error> {
-    match rule {
-      lightningcss::rules::CssRule::Import(import_rule) => {
-        self.dependencies.push(RawImportRecord::new(
-          import_rule.url.to_string().into(),
+  for lexed_dep in lexed_deps {
+    match lexed_dep {
+      css_module_lexer::Dependency::Import { request, range, .. } => {
+        dependencies.push(RawImportRecord::new(
+          request.into(),
           ImportKind::AtImport,
-          // FIXME: this is meaningless for CSS's at import
           SymbolRef::from((ModuleIdx::from_raw(0), SymbolId::from_usize(0))),
-          0,
+          range.start,
         ));
       }
       _ => {}
     }
-    Ok(())
   }
+
+  (CssView { source: source.clone(), import_records: IndexVec::default() }, dependencies)
 }

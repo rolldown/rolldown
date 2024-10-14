5b6497d675675a807a85ea7908422fbc46fcb389
Yunfei He <i.heyunfei@gmail.com>
2024-10-14 10:55:45 +0800

    feat(css): should remove `at-import` statements in output (#2423)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown_common/src/css/css_view.rs
+++ crates/rolldown_common/src/css/css_view.rs
@@ -1,10 +1,27 @@
 use arcstr::ArcStr;
 use oxc::index::IndexVec;
 
-use crate::{ImportRecordIdx, ResolvedImportRecord};
+use crate::{
+  types::source_mutation::BoxedSourceMutation, ImportRecordIdx, ResolvedImportRecord,
+  SourceMutation,
+};
 
 #[derive(Debug)]
 pub struct CssView {
   pub source: ArcStr,
   pub import_records: IndexVec<ImportRecordIdx, ResolvedImportRecord>,
+  pub mutations: Vec<BoxedSourceMutation>,
+}
+
+#[derive(Debug, Default)]
+pub struct CssRenderer {
+  pub at_import_ranges: Vec<(usize, usize)>,
+}
+
+impl SourceMutation for CssRenderer {
+  fn apply(&self, magic_string: &mut string_wizard::MagicString<'_>) {
+    for range in &self.at_import_ranges {
+      magic_string.remove(range.0, range.1);
+    }
+  }
 }

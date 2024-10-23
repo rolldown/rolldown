4c0c084930fc4131fa6ab8f63c7f2b90194ce201
underfin <likui.underfin@gmail.com>
2024-10-23 18:23:55 +0800

    chore: fix ci

--- packages/rolldown/tests/fixtures/output/format/umd/_config.ts
+++ packages/rolldown/tests/fixtures/output/format/umd/_config.ts
@@ -4,13 +4,12 @@ export default defineTest({
   config: {
     external: /node:path/,
     output: {
-      entryFileNames: '[name].mjs',
       exports: 'named',
       name: 'module',
       format: 'umd',
     },
   },
   afterTest: async () => {
-    await import('./assert.mjs')
+    await import('./assert.js')
   },
 })
--- packages/rolldown/tests/fixtures/output/format/umd/assert.mjs
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-import assert from 'node:assert'
-import './dist/main.mjs'
-assert(globalThis.module.default === 'default')

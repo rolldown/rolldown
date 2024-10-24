a18cd5e60beb91ce89ef6d7418292129d4010c27
Yunfei He <i.heyunfei@gmail.com>
2024-10-25 00:33:00 +0800

    refactor(error/dx): make handling outside errors more easier (#2499)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown_error/src/lib.rs
+++ crates/rolldown_error/src/lib.rs
@@ -5,6 +5,8 @@ mod events;
 mod type_aliases;
 mod types;
 
+pub use types::result_ext::ResultExt;
+
 pub use crate::{
   build_error::{BuildDiagnostic, BuildResult},
   event_kind::EventKind,
@@ -16,3 +18,8 @@ pub use crate::{
   type_aliases::{DiagnosableResult, UnhandleableResult},
   types::diagnostic_options::DiagnosticOptions,
 };
+
+fn _usage_should_able_to_auto_convert_outside_errors() -> DiagnosableResult<()> {
+  let _: usize = 0u32.try_into().map_error_to_unhandleable()?;
+  Ok(())
+}
---
+++ crates/rolldown_error/src/types/result_ext.rs
@@ -0,0 +1,21 @@
+use crate::{BuildDiagnostic, BuildResult};
+use std::error::Error as StdError;
+
+pub trait ResultExt<Val> {
+  /// This method is used to make converting outside errors to `BuildDiagnostic` easier.
+  /// For example, handling errors like converting u64 to usize in a platform that usize is 32-bit is meaningless for
+  /// rolldown. So we just convert them to `BuildDiagnostic::unhandleable_error` to provide better dx around errors.
+  fn map_error_to_unhandleable(self) -> BuildResult<Val>;
+}
+
+impl<Val, Err> ResultExt<Val> for Result<Val, Err>
+where
+  Err: StdError + Send + Sync + 'static,
+{
+  fn map_error_to_unhandleable(self) -> BuildResult<Val> {
+    self.map_err(|err| {
+      let err = anyhow::Error::new(err);
+      BuildDiagnostic::unhandleable_error(err)
+    })
+  }
+}

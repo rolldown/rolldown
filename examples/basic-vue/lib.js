e164f5874a21b7eaa7b5e5368efa3175d82fa53c
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-10-29 22:56:18 +0800

    fix: 🐛 fix concurrent write

--- examples/basic-vue/lib.js
+++ examples/basic-vue/lib.js
@@ -1,8 +1,15 @@
 import { rolldown } from 'rolldown'
 
 const bundle = await rolldown({
-  input: ['./index.js'],
+  input: ['./input.js'],
 })
-await bundle.write({ format: 'esm', dir: './dist' })
+await write()
 // Execute twice
-await bundle.write({ format: 'esm', dir: './dist' })
+await write()
+
+async function write() {
+  await Promise.all([
+    bundle.write({ format: 'esm', dir: './dist' }),
+    bundle.write({ format: 'cjs', dir: './dist', entryFileNames: 'input.cjs' }),
+  ])
+}

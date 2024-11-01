0dee1eec99aa666531339126f680c888ea20dfc7
Yunfei <i.heyunfei@gmail.com>
2024-11-01 14:12:53 +0800

    chore(node/hook-filter): improve jsdoc of id filter

--- packages/rolldown/src/plugin/hook-filter.ts
+++ packages/rolldown/src/plugin/hook-filter.ts
@@ -16,6 +16,37 @@ interface FormalModuleTypeFilter {
 type ModuleTypeFilter = ModuleType[] | FormalModuleTypeFilter
 
 export interface HookFilter {
+  /**
+   * This filter is used to do a pre-test to determine whether the hook should be called.
+   * @example
+   * // Filter out all `id`s that contain `node_modules` in the path.
+   * ```js
+   * { id: 'node_modules' }
+   * ```
+   * @example
+   * // Filter out all `id`s that contain `node_modules` or `src` in the path.
+   * ```js
+   * { id: ['node_modules', 'src'] }
+   * ```
+   * @example
+   * // Filter out all `id`s that start with `http`
+   * ```js
+   * { id: /^http/ }
+   * ```
+   * @example
+   * // Exclude all `id`s that contain `node_modules` in the path.
+   * ```js
+   * { id: { exclude: 'node_modules' } }
+   * ```
+   * @example
+   * // Formal pattern
+   * ```
+   * { id : {
+   *   include: ["foo", /bar/],
+   *   exclude: ["baz", /qux/]
+   * }}
+   * ```
+   */
   id?: StringFilter
   moduleType?: ModuleTypeFilter
   code?: StringFilter

112288dcda51b615fdf2c24d182c54e1702202a9
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-11-08 00:52:07 +0800

    chore:  support regex pattern in esbuild diff tool resolve (#2650)
    <!-- Thank you for contributing! -->
    
    ### Description
    1. Closed #2649
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/tests/esbuild/loader/loader_file_common_js_and_es6/diff.config.js
+++ crates/rolldown/tests/esbuild/loader/loader_file_common_js_and_es6/diff.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   resolver: {
-    "/y-YE5AYNFB.txt": "assets/y-wSWe67SA.txt",
-    "/x-LSAMBFUD.txt": "assets/x-QPVb1P9U.txt"
+    "/y-YE5AYNFB.txt": /assets\/y-.*.txt/,
+    "/x-LSAMBFUD.txt": /assets\/x-.*.txt/
   }
 }

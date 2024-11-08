b739329778550bdf3ad6018a79854806e68cc7d1
Yunfei He <i.heyunfei@gmail.com>
2024-11-09 00:13:50 +0800

    fix(rust): ensure filenames are valid for file systems (#2664)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    Fixes https://github.com/rolldown/rolldown/issues/2659.
    Closes https://github.com/rolldown/rolldown/pull/2663.
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ packages/rolldown/tests/fixtures/topics/virtual-module/virtual-entry/_config.ts
@@ -0,0 +1,28 @@
+// cSpell:disable
+import { defineTest } from '@tests'
+import { expect } from 'vitest'
+
+export default defineTest({
+  config: {
+    input: 'test',
+    plugins: [
+      {
+        resolveId(id) {
+          if (id === 'test') {
+            return '\0virtual:test'
+          }
+        },
+        load(id) {
+          if (id === '\0virtual:test') {
+            return 'export default "test"'
+          }
+        },
+      },
+    ],
+  },
+  async afterTest() {
+    // @ts-ignore Ther will be a MODULE NOT FOUND error before the test is executed
+    const exports = await import('./dist/virtualtest.js')
+    expect(exports.default).toBe('test')
+  },
+})

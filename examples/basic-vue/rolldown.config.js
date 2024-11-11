b63a0c4d0423498584ba6186987f5f42de1fedd9
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-11-11 21:19:51 +0800

    update

--- examples/basic-vue/rolldown.config.js
+++ examples/basic-vue/rolldown.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'rolldown'
+import { defineConfig } from "rolldown";
 
 export default defineConfig({
-  input: './index.js',
+  input: "./main.js",
   resolve: {
     // This needs to be explicitly set for now because oxc resolver doesn't
     // assume default exports conditions. Rolldown will ship with a default that
     // aligns with Vite in the future.
-    conditionNames: ['import'],
+    conditionNames: ["import"],
   },
-})
+  treeshake: {
+    moduleSideEffects: [
+      {
+        test: /a\.mjs/,
+        sideEffects: true,
+      },
+      {
+        test: /\.js/,
+        sideEffects: false,
+      },
+      // {
+      //   test: /b\.mjs/,
+      //   sideEffects: true,
+      // },
+    ],
+  },
+});
---
+++ packages/rolldown/tests/fixtures/tree-shake/module-side-effects-rules-priority/_config.ts
@@ -0,0 +1,32 @@
+import type { RolldownOutputChunk } from "rolldown";
+import { defineTest } from "@tests";
+import { expect } from "vitest";
+
+export default defineTest({
+  config: {
+    treeshake: {
+      moduleSideEffects: [
+        {
+          test: /a\.mjs/,
+          sideEffects: true,
+        },
+        {
+          test: /\.js/,
+          sideEffects: false,
+        },
+        {
+          test: /b\.js/,
+          sideEffects: true,
+        },
+      ],
+    },
+  },
+  afterTest: (output) => {
+    let chunk = output.output.filter(({ type }) => type === "chunk")[0];
+    let code = (chunk as RolldownOutputChunk).code;
+    // a.mjs -> module.sideEffects is `true` 
+    expect(code.includes(`console.log("a")`)).toBe(true);
+    // b.js -> module.sideEffects is `false`, `/\.js/`  has higher priority than `/b\.js/`
+    expect(code.includes(`console.log("b")`)).toBe(false);
+  },
+});

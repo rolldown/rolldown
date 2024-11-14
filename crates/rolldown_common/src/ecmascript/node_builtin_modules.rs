1462a6eca69416b2ed24613150c305d8d1e59486
翠 / green <green@sapphi.red>
2024-11-14 17:24:54 +0900

    fix: prefix-only module is a builtin module (#2729)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    The list does not include prefix-only modules.
    
    https://nodejs.org/api/module.html#modulebuiltinmodules:~:text=Note%3A%20the%20list%20doesn%27t%20contain%20prefix%2Donly%20modules%20like%20node%3Atest.
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown_common/src/ecmascript/node_builtin_modules.rs
+++ crates/rolldown_common/src/ecmascript/node_builtin_modules.rs
@@ -1,9 +1,33 @@
 use oxc_resolver::NODEJS_BUILTINS;
 
+/// A list of prefix-only modules
+const NODEJS_PREFIXED_BUILTINS: &[&str] = &[
+  // https://nodejs.org/api/modules.html#built-in-modules-with-mandatory-node-prefix
+  "node:sea",
+  "node:test",
+  "node:test/reporters",
+  // https://nodejs.org/api/sqlite.html
+  "node:sqlite",
+];
+
 /// Using `phf` should be faster, but it would increase the compile time, since this function is
 /// not frequently used, we use `binary_search` instead.
-pub fn is_builtin_modules(specifier: &str) -> bool {
-  let normalized_specifier =
-    if let Some(specifier) = specifier.strip_prefix("node:") { specifier } else { specifier };
-  NODEJS_BUILTINS.binary_search(&normalized_specifier).is_ok()
+pub fn is_existing_node_builtin_modules(specifier: &str) -> bool {
+  if let Some(stripped) = specifier.strip_prefix("node:") {
+    return NODEJS_BUILTINS.binary_search(&stripped).is_ok()
+      || NODEJS_PREFIXED_BUILTINS.binary_search(&specifier).is_ok();
+  }
+  NODEJS_BUILTINS.binary_search(&specifier).is_ok()
+}
+
+#[test]
+fn test_is_builtin_modules() {
+  // not prefix-only modules
+  assert!(is_existing_node_builtin_modules("fs"));
+  assert!(is_existing_node_builtin_modules("node:fs"));
+  // prefix-only modules
+  assert!(is_existing_node_builtin_modules("node:test"));
+  // not a builtin module
+  assert!(!is_existing_node_builtin_modules("unknown"));
+  assert!(!is_existing_node_builtin_modules("node:unknown"));
 }

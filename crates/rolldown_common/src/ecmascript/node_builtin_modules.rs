45cbbdc26a8b513904946730f4330ede440e8e68
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-14 11:55:05 +0900

    fix: prefix-only module is a builtin module

--- crates/rolldown_common/src/ecmascript/node_builtin_modules.rs
+++ crates/rolldown_common/src/ecmascript/node_builtin_modules.rs
@@ -76,7 +76,16 @@ pub const NODEJS_BUILTINS: &[&str] = &[
 /// Using `phf` should be faster, but it would increase the compile time, since this function is
 /// not frequently used, we use `binary_search` instead.
 pub fn is_builtin_modules(specifier: &str) -> bool {
-  let normalized_specifier =
-    if let Some(specifier) = specifier.strip_prefix("node:") { specifier } else { specifier };
-  NODEJS_BUILTINS.binary_search(&normalized_specifier).is_ok()
+  specifier.starts_with("node:") || NODEJS_BUILTINS.binary_search(&specifier).is_ok()
+}
+
+#[test]
+fn test_is_builtin_modules() {
+  // not prefix-only modules
+  assert!(is_builtin_modules("fs"));
+  assert!(is_builtin_modules("node:fs"));
+  // prefix-only modules
+  assert!(is_builtin_modules("node:test"));
+  // not a builtin module
+  assert!(!is_builtin_modules("unknown"));
 }

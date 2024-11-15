4b8ed6fa11ba6829501aa5e2af76cb1ba860a789
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-15 16:04:12 +0900

    chore: keep `this`

--- packages/rolldown/src/plugin/builtin-plugin.ts
+++ packages/rolldown/src/plugin/builtin-plugin.ts
@@ -208,12 +208,40 @@ export function isCallableCompatibleBuiltinPlugin(
   )
 }
 
+type BindingCallableBuiltinPluginLike = {
+  [K in keyof BindingCallableBuiltinPlugin]: BindingCallableBuiltinPlugin[K]
+}
+
 export function makeBuiltinPluginCallable(plugin: BuiltinPlugin) {
-  return new BindingCallableBuiltinPlugin(bindingifyBuiltInPlugin(plugin))
+  let callablePlugin = new BindingCallableBuiltinPlugin(
+    bindingifyBuiltInPlugin(plugin),
+  )
+
+  const wrappedPlugin: Partial<BindingCallableBuiltinPluginLike> & {
+    _original: BindingCallableBuiltinPlugin
+  } = {
+    _original: callablePlugin,
+  }
+  for (const [key, value] of Object.entries(callablePlugin)) {
+    if (key === 'name') {
+      wrappedPlugin[key] = value
+    } else {
+      // @ts-expect-error
+      wrappedPlugin[key] = function (...args) {
+        return value(...args)
+      }
+    }
+  }
+  return wrappedPlugin as BindingCallableBuiltinPluginLike & {
+    _original: BindingCallableBuiltinPlugin
+  }
 }
 
 export function isCallableBuiltinPlugin(plugin: any): boolean {
-  return plugin instanceof BindingCallableBuiltinPlugin
+  return (
+    '_original' in plugin &&
+    plugin._original instanceof BindingCallableBuiltinPlugin
+  )
 }
 
 export function bindingifyBuiltInPlugin(

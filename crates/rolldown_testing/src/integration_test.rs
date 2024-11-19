8ef1b106a4796c54a04d018e317a29c41cb75009
Yunfei He <i.heyunfei@gmail.com>
2024-11-19 15:00:29 +0800

    test(rust): allow to run `_test.mjs` in any format, prepare to deprecate `_test.cjs` (#2788)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    - To reduce complexity, `_test.cjs` is gonna be removed.
    - Only 16 tests is using `_test.cjs`, it won't be big affect and can be
    replaced with `_test.mjs`
    - `_test.mjs` is more powerful than `_test.cjs`.
      - You could `import './foo.cjs'` directly
      - Or using TLA: `await import('./foo.cjs')`
      - Or using `createRequire`: `require('./foo.cjs')`
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown_testing/src/integration_test.rs
+++ crates/rolldown_testing/src/integration_test.rs
@@ -462,7 +462,16 @@ impl IntegrationTest {
       package_json.write_all(serde_json::to_string_pretty(&json).unwrap().as_bytes()).unwrap();
     }
 
-    let test_script = if is_output_cjs { cwd.join("_test.cjs") } else { cwd.join("_test.mjs") };
+    let mut test_script = cwd.join("_test.mjs");
+
+    let mut is_cjs_test_script = false;
+    if is_output_cjs {
+      let cjs_test_script = cwd.join("_test.cjs");
+      if cjs_test_script.is_dir() {
+        test_script = cjs_test_script;
+        is_cjs_test_script = true;
+      }
+    }
 
     let mut node_command = Command::new("node");
 
@@ -482,12 +491,12 @@ impl IntegrationTest {
         .collect::<Vec<_>>();
 
       compiled_entries.iter().for_each(|entry| {
-        if is_output_cjs {
+        if is_cjs_test_script {
           node_command.arg("--require");
         } else {
           node_command.arg("--import");
         }
-        if cfg!(target_os = "windows") && !is_output_cjs {
+        if cfg!(target_os = "windows") && !is_cjs_test_script {
           // Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. On Windows, absolute paths must be valid file:// URLs.
           node_command.arg(format!("file://{}", entry.to_str().expect("should be valid utf8")));
         } else {

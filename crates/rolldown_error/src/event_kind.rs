6ff6d87c541c8b153b1cf33e009a621e53c44034
dalaoshu <laipichan@qq.com>
2024-12-02 17:57:20 +0800

    chore: improve rolldown error (#3010)
    ### Description
    
    - [x]  remove unnecessary `error_code.rs`
    - [x]  improve `event_kind.rs`
    
    If needed, we can click the link below to view the rollup error code and
    its context.
    
    https://github.com/rolldown/rolldown/blob/1d07c4fab5bce030f0f908a92bfd5650527fd219/crates/rolldown_error/src/event_kind.rs#L4

--- crates/rolldown_error/src/event_kind.rs
+++ crates/rolldown_error/src/event_kind.rs
@@ -2,78 +2,84 @@ use std::fmt::Display;
 
 pub enum EventKind {
   // --- These kinds are copied from rollup: https://github.com/rollup/rollup/blob/0b665c31833525c923c0fc20f43ebfca748c6670/src/utils/logs.ts#L102-L179
-  UnresolvedEntry,
-  UnresolvedImport,
   AmbiguousExternalNamespace,
-  MixedExport,
+  CircularDependency,
+  Eval,
+  IllegalIdentifierAsName,
+  InvalidExportOption,
+  InvalidOption,
+  MissingExport,
   MissingGlobalName,
   MissingNameOptionForIifeExport,
   MissingNameOptionForUmdExport,
-  IllegalIdentifierAsName,
+  MixedExport,
   ParseError,
-  InvalidOption,
-
-  Eval,
-  CircularDependency,
   SourcemapError,
-  MissingExport,
-  InvalidExportOption,
-  // --- These kinds are rolldown specific
-  IllegalReassignment,
-  UnloadableDependency,
-  ResolveError(Option<&'static str>),
+  UnresolvedEntry,
+  UnresolvedImport,
   // !! Only add new kind if it's not covered by the kinds from rollup !!
 
-  // TODO remove following kinds
-  NapiError,
-  IoError,
-  // Derive from esbuild
+  // --- These kinds are derived from esbuild
+  AssignToImport,
   CommonJsVariableInEsm,
   ExportUndefinedVariable,
   ImportIsUndefined,
   UnsupportedFeature,
-  UnhandleableError,
-  AssignToImport,
+
+  // --- These kinds are rolldown specific
   JsonParse,
+  IllegalReassignment,
   InvalidDefineConfig,
+  ResolveError(Option<&'static str>),
+  UnhandleableError,
+  UnloadableDependency,
+
+  // TODO remove following kinds
+  IoError,
+  NapiError,
 }
 
 impl Display for EventKind {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
       // --- Copied from rollup
-      EventKind::UnloadableDependency => write!(f, "UNLOADABLE_DEPENDENCY"),
-      EventKind::UnresolvedEntry => write!(f, "UNRESOLVED_ENTRY"),
-      EventKind::UnresolvedImport => write!(f, "UNRESOLVED_IMPORT"),
       EventKind::AmbiguousExternalNamespace => write!(f, "AMBIGUOUS_EXTERNAL_NAMESPACES"),
-      EventKind::ParseError => write!(f, "PARSE_ERROR"),
-      EventKind::IllegalReassignment => write!(f, "ILLEGAL_REASSIGNMENT"),
+      EventKind::CircularDependency => write!(f, "CIRCULAR_DEPENDENCY"),
       EventKind::Eval => write!(f, "EVAL"),
-      EventKind::SourcemapError => write!(f, "SOURCEMAP_ERROR"),
+      EventKind::IllegalIdentifierAsName => write!(f, "ILLEGAL_IDENTIFIER_AS_NAME"),
+      EventKind::InvalidExportOption => write!(f, "INVALID_EXPORT_OPTION"),
+      EventKind::InvalidOption => write!(f, "INVALID_OPTION"),
       EventKind::MixedExport => write!(f, "MIXED_EXPORT"),
       EventKind::MissingGlobalName => write!(f, "MISSING_GLOBAL_NAME"),
       EventKind::MissingNameOptionForIifeExport => write!(f, "MISSING_NAME_OPTION_FOR_IIFE_EXPORT"),
       EventKind::MissingNameOptionForUmdExport => write!(f, "MISSING_NAME_OPTION_FOR_UMD_EXPORT"),
-      EventKind::IllegalIdentifierAsName => write!(f, "ILLEGAL_IDENTIFIER_AS_NAME"),
-      EventKind::CircularDependency => write!(f, "CIRCULAR_DEPENDENCY"),
       EventKind::MissingExport => write!(f, "MISSING_EXPORT"),
-      EventKind::InvalidExportOption => write!(f, "INVALID_EXPORT_OPTION"),
-      EventKind::InvalidOption => write!(f, "INVALID_OPTION"),
-      // --- Rolldown specific
-      EventKind::NapiError => write!(f, "NAPI_ERROR"),
-      EventKind::IoError => write!(f, "IO_ERROR"),
+      EventKind::ParseError => write!(f, "PARSE_ERROR"),
+      EventKind::SourcemapError => write!(f, "SOURCEMAP_ERROR"),
+      EventKind::UnresolvedEntry => write!(f, "UNRESOLVED_ENTRY"),
+      EventKind::UnresolvedImport => write!(f, "UNRESOLVED_IMPORT"),
+
+      // --- Derived from esbuild
+      EventKind::AssignToImport => write!(f, "ASSIGN_TO_IMPORT"),
       EventKind::CommonJsVariableInEsm => write!(f, "COMMONJS_VARIABLE_IN_ESM"),
       EventKind::ExportUndefinedVariable => write!(f, "EXPORT_UNDEFINED_VARIABLE"),
+      EventKind::ImportIsUndefined => write!(f, "IMPORT_IS_UNDEFINED"),
+      EventKind::UnsupportedFeature => write!(f, "UNSUPPORTED_FEATURE"),
+
+      // --- Rolldown specific
+      EventKind::JsonParse => write!(f, "JSON_PARSE"),
+      EventKind::IllegalReassignment => write!(f, "ILLEGAL_REASSIGNMENT"),
+      EventKind::InvalidDefineConfig => write!(f, "INVALID_DEFINE_CONFIG"),
       EventKind::ResolveError(title) => match title {
         Some(title) => write!(f, "{title}"),
         None => write!(f, "RESOLVE_ERROR"),
       },
-      EventKind::ImportIsUndefined => write!(f, "IMPORT_IS_UNDEFINED"),
       EventKind::UnhandleableError => write!(f, "UNHANDLEABLE_ERROR"),
-      EventKind::UnsupportedFeature => write!(f, "UNSUPPORTED_FEATURE"),
-      EventKind::AssignToImport => write!(f, "ASSIGN_TO_IMPORT"),
-      EventKind::JsonParse => write!(f, "JSON_PARSE"),
-      EventKind::InvalidDefineConfig => write!(f, "INVALID_DEFINE_CONFIG"),
+      EventKind::UnloadableDependency => write!(f, "UNLOADABLE_DEPENDENCY"),
+
+      // TODO remove following kinds
+      EventKind::IoError => write!(f, "IO_ERROR"),
+      EventKind::NapiError => write!(f, "NAPI_ERROR"),
     }
   }
 }

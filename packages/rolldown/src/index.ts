87050f9b597fbb75ad3bf168df461a499cf28f6c
underfin <likui.underfin@gmail.com>
2024-12-02 15:44:41 +0800

    refactor: watch

--- packages/rolldown/src/index.ts
+++ packages/rolldown/src/index.ts
@@ -56,7 +56,7 @@ import { ExistingRawSourceMap, SourceMapInput } from './types/sourcemap'
 import { OutputBundle } from './types/output-bundle'
 import { version } from '../package.json'
 import { WatchOptions } from './options/watch-options'
-import { Watcher } from './api/watch/watcher'
+import { RolldownWatcher } from './api/watch/watch/watch-emitter'
 import { build, type BuildOptions } from './api/build'
 
 export { defineConfig, rolldown, watch, build }
@@ -109,7 +109,6 @@ export type {
   JsxOptions,
   WatchOptions,
   Watcher,
-  BuildOptions,
 }
 
 // Exports for compatibility
@@ -121,5 +120,6 @@ export type {
   RolldownOutputChunk as OutputChunk,
   RolldownOutputAsset as OutputAsset,
   RolldownRenderedChunk as RenderedChunk,
+  RolldownWatcher as RollupWatcher,
 }
 export type { RollupError, RollupLog, LoggingFunction } from './rollup'
---
+++ packages/rolldown/src/watch/watcher.ts
@@ -0,0 +1,57 @@
+import { BindingWatcher } from '../binding'
+import { WatchOptions } from '../options/watch-options'
+import { createBundler } from '../utils/create-bundler'
+import { WatcherEmitter } from './watch-emitter'
+
+export class Watcher {
+  closed: boolean
+  controller: AbortController
+  inner: BindingWatcher
+  emitter: WatcherEmitter
+  stopWorkers?: () => Promise<void>
+
+  constructor(
+    emitter: WatcherEmitter,
+    inner: BindingWatcher,
+    stopWorkers?: () => Promise<void>,
+  ) {
+    this.closed = false
+    this.controller = new AbortController()
+    this.inner = inner
+    this.emitter = emitter
+    emitter.close = this.close.bind(this)
+    this.stopWorkers = stopWorkers
+  }
+
+  async close() {
+    if (this.closed) return
+    this.closed = true
+    await this.stopWorkers?.()
+    await this.inner.close()
+    this.controller.abort()
+  }
+
+  // The rust side already create a thread for watcher, but it isn't at main thread.
+  // So here we need to avoid main process exit util the user call `watcher.close()`.
+  start() {
+    const timer = setInterval(() => {}, 1e9 /* Low power usage */)
+    this.controller.signal.addEventListener('abort', () => {
+      clearInterval(timer)
+    })
+    // run first build after listener is attached
+    process.nextTick(() => this.inner.start(this.emitter.onEvent))
+  }
+}
+
+export async function createWatcher(
+  emitter: WatcherEmitter,
+  input: WatchOptions,
+) {
+  const { bundler, stopWorkers } = await createBundler(
+    input,
+    input.output || {},
+  )
+  const bindingWatcher = await bundler.watch()
+  const watcher = new Watcher(emitter, bindingWatcher, stopWorkers)
+  watcher.start()
+}

039c9fbfabb3b91977b68d53081ef6be48caea16
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-12-03 19:15:43 +0800

    feat: keep name for ClassDecl (#3023)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/tests/rolldown/topics/keep_names/basic/main.js
+++ crates/rolldown/tests/rolldown/topics/keep_names/basic/main.js
@@ -1,11 +1,16 @@
 // MULTIPLE ENTRY MODULES
-import { test as s, a as b } from './a.js';
+import { test as s, a as b, Foo as Bar } from './a.js';
 import assert from 'assert'
 
 
 s();
 b();
 const test = 10;
+class Foo extends Bar {
+
+}
 console.log(`test: `, test)
+assert.strictEqual(Foo.name, "Foo")
+assert.strictEqual(Bar.name, "Foo")
 
 assert.strictEqual(s.name, "test")

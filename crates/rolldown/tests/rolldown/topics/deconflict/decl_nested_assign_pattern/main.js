06f895007871ff487e267f98f95da1372258db9c
Yunfei He <i.heyunfei@gmail.com>
2024-12-04 17:10:33 +0800

    test: fix incorrect test `topics/deconflict/decl_nested_assign_pattern` (#3035)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    Fixes https://github.com/rolldown/rolldown/issues/3034
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/tests/rolldown/topics/deconflict/decl_nested_assign_pattern/main.js
+++ crates/rolldown/tests/rolldown/topics/deconflict/decl_nested_assign_pattern/main.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import { baz as baz2 } from './names.js'
-export const { foo: { bar: { baz = '' } = {} } = {} } = { foo: { bar: { baz: 'baz' } } }
+const { foo: { bar: { baz = '' } = {} } = {} } = { foo: { bar: { baz: 'baz' } } }
 assert.strictEqual(baz, 'baz')
 assert.strictEqual(baz2, 'baz2')
 

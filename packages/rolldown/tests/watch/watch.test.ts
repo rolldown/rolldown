3ba0e8369448e26e44820f21c6f259546c002c7a
underfin <likui.underfin@gmail.com>
2024-12-07 22:03:00 +0800

    chore: fix ci

--- packages/rolldown/tests/watch/watch.test.ts
+++ packages/rolldown/tests/watch/watch.test.ts
@@ -211,7 +211,9 @@ test.sequential('PluginContext addWatchFile', async () => {
 
   // edit file
   // TODO: not sure the update event is not triggered at windows, but add it success
-  ensureWriteFileSync(foo, 'console.log("update")\n')
+  // ref https://github.com/rolldown/rolldown/actions/runs/12213020539/job/34072162527?pr=3032 windows node 18/20
+  console.log(foo, 'console.log("update")\n')
+  fs.writeFileSync(foo, 'console.log("update")\n')
   await waitUtil(() => {
     expect(changeFn).toBeCalled()
   })
@@ -361,9 +363,7 @@ async function waitBuildFinished(
 // TODO:
 // The windows maybe cannot emit the change event, write the file twice to ensure the change event emit.
 // ref https://github.com/rolldown/rolldown/actions/runs/12212639717/job/34071323644 windows node 22
-// ref https://github.com/rolldown/rolldown/actions/runs/12213020539/job/34072162527?pr=3032 windows node 18
 async function ensureWriteFileSync(filePath: string, content: string) {
-  fs.writeFileSync(filePath, 'console.log(1)\n' + content)
-  fs.writeFileSync(filePath, content + 'console.log(2)\n')
+  fs.writeFileSync(filePath, '\n' + content + '\n')
   fs.writeFileSync(filePath, content)
 }

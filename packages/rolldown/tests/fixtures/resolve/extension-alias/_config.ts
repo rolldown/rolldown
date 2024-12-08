6591ff33164d3b0e90a43cac455927d0797c4d21
Yunfei <i.heyunfei@gmail.com>
2024-12-08 18:24:32 +0800

    test(node): fix `extension-alias` test, part of #3075

--- packages/rolldown/tests/fixtures/resolve/extension-alias/_config.ts
+++ packages/rolldown/tests/fixtures/resolve/extension-alias/_config.ts
@@ -3,11 +3,10 @@ import path from 'node:path'
 const entry = path.join(__dirname, './main.ts')
 
 export default defineTest({
-  skip: true, // FIXME(hyf0): this test is not working already.
   config: {
     input: entry,
     resolve: {
-      extensionAlias: { '.ts': ['.ts', '.js'] },
+      extensionAlias: { '.js': ['.ts', '.js'] },
     },
   },
 })

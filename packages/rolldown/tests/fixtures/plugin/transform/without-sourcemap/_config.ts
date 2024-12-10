b30cd890d92c9b6cf00823c3436250baa8222e99
underfin <likui.underfin@gmail.com>
2024-12-10 22:17:14 +0800

    fix: should create one sourcemap if transform hook not return (#3107)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    Partially closed https://github.com/rolldown/rolldown/issues/3092. The
    `oxc_sourcemap#lookup_source_view_token` issue i will fix it at next.
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ packages/rolldown/tests/fixtures/plugin/transform/without-sourcemap/_config.ts
@@ -0,0 +1,44 @@
+// cSpell:disable
+import { expect } from 'vitest'
+import { defineTest } from '@tests'
+import { getLocation, getOutputAsset, getOutputChunk } from '@tests/utils'
+import { SourceMapConsumer } from 'source-map'
+
+export default defineTest({
+  skipComposingJsPlugin: true,
+  config: {
+    plugins: [
+      {
+        name: 'test-plugin',
+        transform(code) {
+          return {
+            code: code + '\nconsole.log("added")',
+          }
+        },
+      },
+    ],
+    output: {
+      sourcemap: true,
+    },
+  },
+  afterTest: async (output) => {
+    const code = getOutputChunk(output)[0].code
+    const map = getOutputAsset(output)[0].source as string
+    const smc = await new SourceMapConsumer(JSON.parse(map))
+
+    const generatedLoc = getLocation(code, code.indexOf(`"main"`))
+    const originalLoc = smc.originalPositionFor(generatedLoc)
+    expect(originalLoc.line).toBe(1)
+    expect(originalLoc.column).toBe(12)
+    expect(smc.sourceContentFor(originalLoc.source!)).toBe(
+      "console.log('main')",
+    )
+
+    // TODO: fix oxc source map lookup_source_view_token
+    // const generatedLoc2 = getLocation(code, code.indexOf(`"added"`))
+    // const originalLoc2 = smc.originalPositionFor(generatedLoc2)
+    // expect(originalLoc2.line).toBe(null)
+    // expect(originalLoc2.column).toBe(null)
+    // expect(originalLoc2.source).toBe(null)
+  },
+})

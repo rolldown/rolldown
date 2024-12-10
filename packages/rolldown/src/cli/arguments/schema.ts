22785abfdb78a3d29add5668adf801a616819746
Yunfei He <i.heyunfei@gmail.com>
2024-12-10 15:15:00 +0800

    chore(types): enable `isolatedDeclarations` (#3096)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- packages/rolldown/src/cli/arguments/schema.ts
+++ packages/rolldown/src/cli/arguments/schema.ts
@@ -1,10 +1,20 @@
 import { zodToJsonSchema } from 'zod-to-json-schema'
 import { inputCliOptionsSchema } from '../../options/input-options-schema'
+import { InputCliOptions } from '../../options/input-options'
+import { OutputCliOptions } from '../../options/output-options'
 import { outputCliOptionsSchema } from '../../options/output-options-schema'
 import type { ObjectSchema } from './types'
+import type Z from 'zod'
 import { z } from 'zod'
 
-export const cliOptionsSchema = z
+export interface CliOptions extends InputCliOptions, OutputCliOptions {
+  config?: string | boolean
+  help?: boolean
+  version?: boolean
+  watch?: boolean
+}
+
+export const cliOptionsSchema: Z.ZodType<CliOptions> = z
   .strictObject({
     config: z
       .string()
@@ -18,10 +28,8 @@ export const cliOptionsSchema = z
       .describe('Watch files in bundle and rebuild on changes')
       .optional(),
   })
-  .merge(inputCliOptionsSchema)
-  .merge(outputCliOptionsSchema)
-
-export type CliOptions = z.infer<typeof cliOptionsSchema>
+  .merge(inputCliOptionsSchema as Z.AnyZodObject)
+  .merge(outputCliOptionsSchema as Z.AnyZodObject) as any // We already explicitly defined the type of `cliOptionsSchema` as `Z.ZodType<CliOptions>`, so we can safely cast it to `any` here.
 
 export const schema = zodToJsonSchema(
   cliOptionsSchema,

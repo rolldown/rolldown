2904efd251484c03809d5fbaa55fedd79bef3d5f
Boshen <boshenc@gmail.com>
2024-12-11 16:31:48 +0800

    chore(rolldown): `oxc-resolver` v3.0.0

--- crates/rolldown_fs/src/os.rs
+++ crates/rolldown_fs/src/os.rs
@@ -1,4 +1,4 @@
-use oxc_resolver::{FileMetadata, FileSystem as OxcResolverFileSystem};
+use oxc_resolver::{FileMetadata, FileSystem as OxcResolverFileSystem, FileSystemOs};
 
 use std::{
   io,
@@ -35,52 +35,18 @@ impl FileSystem for OsFileSystem {
 
 impl OxcResolverFileSystem for OsFileSystem {
   fn read_to_string(&self, path: &Path) -> io::Result<String> {
-    // `simdutf8` is faster than `std::str::from_utf8` which `fs::read_to_string` uses internally
-    let bytes = std::fs::read(path)?;
-    if simdutf8::basic::from_utf8(&bytes).is_err() {
-      // Same error as `fs::read_to_string` produces (`io::Error::INVALID_UTF8`)
-      return Err(io::Error::new(io::ErrorKind::InvalidData, "stream did not contain valid UTF-8"));
-    }
-    // SAFETY: `simdutf8` has ensured it's a valid UTF-8 string
-    Ok(unsafe { String::from_utf8_unchecked(bytes) })
+    FileSystemOs::read_to_string(path)
   }
 
   fn metadata(&self, path: &Path) -> io::Result<FileMetadata> {
-    std::fs::metadata(path).map(FileMetadata::from)
+    FileSystemOs::metadata(path)
   }
 
   fn symlink_metadata(&self, path: &Path) -> io::Result<FileMetadata> {
-    std::fs::symlink_metadata(path).map(FileMetadata::from)
+    FileSystemOs::symlink_metadata(path)
   }
 
-  fn canonicalize(&self, path: &Path) -> io::Result<PathBuf> {
-    #[cfg(not(target_os = "wasi"))]
-    {
-      dunce::canonicalize(path)
-    }
-    #[cfg(target_os = "wasi")]
-    {
-      let meta = std::fs::symlink_metadata(path)?;
-      if meta.file_type().is_symlink() {
-        let link = std::fs::read_link(path)?;
-        let mut path_buf = path.to_path_buf();
-        path_buf.pop();
-        for segment in link.iter() {
-          match segment.to_str() {
-            Some("..") => {
-              path_buf.pop();
-            }
-            Some(".") | None => {}
-            Some(seg) => {
-              // Need to trim the extra \0 introduces by rust std rust-lang/rust#123727
-              path_buf.push(seg.trim_end_matches('\0'));
-            }
-          }
-        }
-        Ok(path_buf)
-      } else {
-        Ok(path.to_path_buf())
-      }
-    }
+  fn read_link(&self, path: &Path) -> io::Result<PathBuf> {
+    FileSystemOs::read_link(path)
   }
 }

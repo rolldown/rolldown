03a142652dcc648b09f389bd3c6c3279e423c61c
Boshen <boshenc@gmail.com>
2024-12-12 14:49:39 +0800

    perf(rolldown): remove `AppendOnlyVec` in `GenerateStage::collect_depended_symbols`

--- crates/rolldown/src/stages/generate_stage/compute_cross_chunk_links.rs
+++ crates/rolldown/src/stages/generate_stage/compute_cross_chunk_links.rs
@@ -141,7 +141,6 @@ impl GenerateStage<'_> {
     index_cross_chunk_dynamic_imports: &mut IndexCrossChunkDynamicImports,
   ) {
     let symbols = &self.link_output.symbol_db;
-    let chunk_id_to_symbols_vec = append_only_vec::AppendOnlyVec::new();
 
     let chunks_iter = multizip((
       chunk_graph.chunk_table.iter_enumerated(),
@@ -150,125 +149,128 @@ impl GenerateStage<'_> {
       index_cross_chunk_dynamic_imports.iter_mut(),
     ));
 
-    chunks_iter.par_bridge().for_each(
-      |(
-        (chunk_id, chunk),
-        depended_symbols,
-        imports_from_external_modules,
-        cross_chunk_dynamic_imports,
-      )| {
-        let mut symbol_needs_to_assign = vec![];
-        chunk.modules.iter().copied().for_each(|module_id| {
-          let Module::Normal(module) = &self.link_output.module_table.modules[module_id] else {
-            return;
-          };
-          module
-            .import_records
-            .iter()
-            .inspect(|rec| {
-              if let Module::Normal(importee_module) =
+    let chunk_id_to_symbols_vec = chunks_iter
+      .par_bridge()
+      .map(
+        |(
+          (chunk_id, chunk),
+          depended_symbols,
+          imports_from_external_modules,
+          cross_chunk_dynamic_imports,
+        )| {
+          let mut symbol_needs_to_assign = vec![];
+          chunk.modules.iter().copied().for_each(|module_id| {
+            let Module::Normal(module) = &self.link_output.module_table.modules[module_id] else {
+              return;
+            };
+            module
+              .import_records
+              .iter()
+              .inspect(|rec| {
+                if let Module::Normal(importee_module) =
+                  &self.link_output.module_table.modules[rec.resolved_module]
+                {
+                  // the the resolved module is not included in module graph, skip
+                  // TODO: Is that possible that the module of the record is a external module?
+                  if !importee_module.meta.is_included() {
+                    return;
+                  }
+                  if matches!(rec.kind, ImportKind::DynamicImport) {
+                    let importee_chunk = chunk_graph.module_to_chunk[importee_module.idx]
+                      .expect("importee chunk should exist");
+                    cross_chunk_dynamic_imports.insert(importee_chunk);
+                  }
+                }
+              })
+              .filter(|rec| {
+                matches!(rec.kind, ImportKind::Import)
+                  && !rec.meta.contains(ImportRecordMeta::IS_EXPORT_STAR)
+              })
+              .filter_map(|rec| {
+                self.link_output.module_table.modules[rec.resolved_module].as_external()
+              })
+              .for_each(|importee| {
+                // Ensure the external module is imported in case it has side effects.
+                imports_from_external_modules.entry(importee.idx).or_default();
+              });
+
+            module.named_imports.iter().for_each(|(_, import)| {
+              let rec = &module.import_records[import.record_id];
+              if let Module::External(importee) =
                 &self.link_output.module_table.modules[rec.resolved_module]
               {
-                // the the resolved module is not included in module graph, skip
-                // TODO: Is that possible that the module of the record is a external module?
-                if !importee_module.meta.is_included() {
-                  return;
-                }
-                if matches!(rec.kind, ImportKind::DynamicImport) {
-                  let importee_chunk = chunk_graph.module_to_chunk[importee_module.idx]
-                    .expect("importee chunk should exist");
-                  cross_chunk_dynamic_imports.insert(importee_chunk);
-                }
+                imports_from_external_modules.entry(importee.idx).or_default().push(import.clone());
               }
-            })
-            .filter(|rec| {
-              matches!(rec.kind, ImportKind::Import)
-                && !rec.meta.contains(ImportRecordMeta::IS_EXPORT_STAR)
-            })
-            .filter_map(|rec| {
-              self.link_output.module_table.modules[rec.resolved_module].as_external()
-            })
-            .for_each(|importee| {
-              // Ensure the external module is imported in case it has side effects.
-              imports_from_external_modules.entry(importee.idx).or_default();
             });
 
-          module.named_imports.iter().for_each(|(_, import)| {
-            let rec = &module.import_records[import.record_id];
-            if let Module::External(importee) =
-              &self.link_output.module_table.modules[rec.resolved_module]
-            {
-              imports_from_external_modules.entry(importee.idx).or_default().push(import.clone());
-            }
-          });
+            module.stmt_infos.iter().for_each(|stmt_info| {
+              if !stmt_info.is_included {
+                return;
+              }
+              stmt_info.declared_symbols.iter().for_each(|declared| {
+                symbol_needs_to_assign.push(*declared);
+              });
 
-          module.stmt_infos.iter().for_each(|stmt_info| {
-            if !stmt_info.is_included {
-              return;
-            }
-            stmt_info.declared_symbols.iter().for_each(|declared| {
-              symbol_needs_to_assign.push(*declared);
-            });
-
-            stmt_info.referenced_symbols.iter().for_each(|reference_ref| {
-              match reference_ref {
-                rolldown_common::SymbolOrMemberExprRef::Symbol(referenced) => {
-                  let mut canonical_ref = symbols.canonical_ref_for(*referenced);
-                  if let Some(namespace_alias) = &symbols.get(canonical_ref).namespace_alias {
-                    canonical_ref = namespace_alias.namespace_ref;
-                  }
-                  depended_symbols.insert(canonical_ref);
-                }
-                rolldown_common::SymbolOrMemberExprRef::MemberExpr(member_expr) => {
-                  if let Some(sym_ref) = member_expr.resolved_symbol_ref(
-                    &self.link_output.metas[module.idx].resolved_member_expr_refs,
-                  ) {
-                    let mut canonical_ref = self.link_output.symbol_db.canonical_ref_for(sym_ref);
-                    let symbol = symbols.get(canonical_ref);
-                    if let Some(ref ns_alias) = symbol.namespace_alias {
-                      canonical_ref = ns_alias.namespace_ref;
+              stmt_info.referenced_symbols.iter().for_each(|reference_ref| {
+                match reference_ref {
+                  rolldown_common::SymbolOrMemberExprRef::Symbol(referenced) => {
+                    let mut canonical_ref = symbols.canonical_ref_for(*referenced);
+                    if let Some(namespace_alias) = &symbols.get(canonical_ref).namespace_alias {
+                      canonical_ref = namespace_alias.namespace_ref;
                     }
                     depended_symbols.insert(canonical_ref);
-                  } else {
-                    // `None` means the member expression resolve to a ambiguous export, which means it actually resolve to nothing.
-                    // It would be rewrite to `undefined` in the final code, so we don't need to include anything to make `undefined` work.
                   }
-                }
-              };
+                  rolldown_common::SymbolOrMemberExprRef::MemberExpr(member_expr) => {
+                    if let Some(sym_ref) = member_expr.resolved_symbol_ref(
+                      &self.link_output.metas[module.idx].resolved_member_expr_refs,
+                    ) {
+                      let mut canonical_ref = self.link_output.symbol_db.canonical_ref_for(sym_ref);
+                      let symbol = symbols.get(canonical_ref);
+                      if let Some(ref ns_alias) = symbol.namespace_alias {
+                        canonical_ref = ns_alias.namespace_ref;
+                      }
+                      depended_symbols.insert(canonical_ref);
+                    } else {
+                      // `None` means the member expression resolve to a ambiguous export, which means it actually resolve to nothing.
+                      // It would be rewrite to `undefined` in the final code, so we don't need to include anything to make `undefined` work.
+                    }
+                  }
+                };
+              });
             });
           });
-        });
 
-        if let Some(entry_id) = &chunk.entry_module_idx() {
-          let entry = &self.link_output.module_table.modules[*entry_id].as_normal().unwrap();
-          let entry_meta = &self.link_output.metas[entry.idx];
+          if let Some(entry_id) = &chunk.entry_module_idx() {
+            let entry = &self.link_output.module_table.modules[*entry_id].as_normal().unwrap();
+            let entry_meta = &self.link_output.metas[entry.idx];
 
-          if !matches!(entry_meta.wrap_kind, WrapKind::Cjs) {
-            for export_ref in entry_meta.resolved_exports.values() {
-              let mut canonical_ref = symbols.canonical_ref_for(export_ref.symbol_ref);
-              let symbol = symbols.get(canonical_ref);
-              if let Some(ns_alias) = &symbol.namespace_alias {
-                canonical_ref = ns_alias.namespace_ref;
+            if !matches!(entry_meta.wrap_kind, WrapKind::Cjs) {
+              for export_ref in entry_meta.resolved_exports.values() {
+                let mut canonical_ref = symbols.canonical_ref_for(export_ref.symbol_ref);
+                let symbol = symbols.get(canonical_ref);
+                if let Some(ns_alias) = &symbol.namespace_alias {
+                  canonical_ref = ns_alias.namespace_ref;
+                }
+                depended_symbols.insert(canonical_ref);
               }
-              depended_symbols.insert(canonical_ref);
+            }
+
+            if !matches!(entry_meta.wrap_kind, WrapKind::None) {
+              depended_symbols
+                .insert(entry_meta.wrapper_ref.expect("cjs should be wrapped in esm output"));
+            }
+
+            if matches!(self.options.format, OutputFormat::Cjs)
+              && matches!(entry.exports_kind, ExportsKind::Esm)
+            {
+              depended_symbols.insert(self.link_output.runtime.resolve_symbol("__toCommonJS"));
+              depended_symbols.insert(entry.namespace_object_ref);
             }
           }
-
-          if !matches!(entry_meta.wrap_kind, WrapKind::None) {
-            depended_symbols
-              .insert(entry_meta.wrapper_ref.expect("cjs should be wrapped in esm output"));
-          }
-
-          if matches!(self.options.format, OutputFormat::Cjs)
-            && matches!(entry.exports_kind, ExportsKind::Esm)
-          {
-            depended_symbols.insert(self.link_output.runtime.resolve_symbol("__toCommonJS"));
-            depended_symbols.insert(entry.namespace_object_ref);
-          }
-        }
-        chunk_id_to_symbols_vec.push((chunk_id, symbol_needs_to_assign));
-      },
-    );
+          (chunk_id, symbol_needs_to_assign)
+        },
+      )
+      .collect::<Vec<_>>();
     // shadowing previous immutable borrow
     let symbols = &mut self.link_output.symbol_db;
     for (chunk_id, symbol_list) in chunk_id_to_symbols_vec {

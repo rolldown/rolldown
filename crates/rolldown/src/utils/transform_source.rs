e77ad7b78b7bf59a387bdf5de7d187b4768881e9
underfin <likui.underfin@gmail.com>
2024-12-13 15:41:29 +0800

    refactor: transform source

--- crates/rolldown/src/utils/transform_source.rs
+++ crates/rolldown/src/utils/transform_source.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use rolldown_common::ModuleType;
 use rolldown_common::{side_effects::HookSideEffects, ResolvedId};
-use rolldown_plugin::{HookTransformArgs, PluginDriver};
+use rolldown_plugin::PluginDriver;
 use rolldown_sourcemap::SourceMap;
 
+#[inline]
 pub async fn transform_source(
   plugin_driver: &PluginDriver,
   resolved_id: &ResolvedId,
@@ -12,13 +13,5 @@ pub async fn transform_source(
   side_effects: &mut Option<HookSideEffects>,
   module_type: &mut ModuleType,
 ) -> Result<String> {
-  plugin_driver
-    .transform(
-      &HookTransformArgs { id: &resolved_id.id, code: &source, module_type: &ModuleType::Empty },
-      sourcemap_chain,
-      side_effects,
-      &source,
-      module_type,
-    )
-    .await
+  plugin_driver.transform(&resolved_id.id, source, sourcemap_chain, side_effects, module_type).await
 }

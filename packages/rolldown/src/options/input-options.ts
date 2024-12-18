53c8a0c83708f38d1ebaff464db15a8fd789891d
Yunfei <i.heyunfei@gmail.com>
2024-12-18 13:11:26 +0800

    Improve

--- packages/rolldown/src/options/input-options.ts
+++ packages/rolldown/src/options/input-options.ts
@@ -64,9 +64,6 @@ export interface ChecksOptions {
   circularDependency?: boolean
 }
 
-/**
- * @group ConfigOptions
- */
 export interface InputOptions {
   input?: InputOption
   plugins?: RolldownPluginOption
@@ -124,6 +121,53 @@ export interface InputOptions {
     viteMode?: boolean
     resolveNewUrlToAsset?: boolean
   }
+  /**
+   * Replace global variables or [property accessors](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors) with the provided values.
+   *
+   * # Examples
+   *
+   * ## Replace the global variable `IS_PROD` with `true`
+   *
+   * ```js rolldown.config.js
+   * export default defineConfig({ define: { IS_PROD: 'true' // or JSON.stringify(true) } })
+   * ```
+   *
+   * Result:
+   *
+   * ```js
+   * // Input
+   * if (IS_PROD) {
+   *   console.log('Production mode')
+   * }
+   *
+   * // After bundling
+   * if (true) {
+   *   console.log('Production mode')
+   * }
+   *```
+   *
+   * ## Replace the property accessor `process.env.NODE_ENV` with `'production'`
+   *
+   * ```js rolldown.config.js
+   * export default defineConfig({ define: { 'process.env.NODE_ENV': "'production'" } })
+   * ```
+   *
+   * Result:
+   *
+   * ```js
+   *
+   * // Input
+   * if (process.env.NODE_ENV === 'production') {
+   *  console.log('Production mode')
+   * }
+   *
+   * // After bundling
+   * if ('production' === 'production') {
+   * console.log('Production mode')
+   * }
+   *
+   * ```
+   */
   define?: Record<string, string>
   /**
    * Inject import statements on demand.

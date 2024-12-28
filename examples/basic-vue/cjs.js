85627ff4b73de838a51cefeac75f70ff55601596
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-12-28 20:45:47 +0800

    chore: esm_flag analyzer

---
+++ examples/basic-vue/cjs.js
@@ -0,0 +1,7 @@
+// cjsModuleWithEsModule.js
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.default = function () {
+  console.log('Hello from default export')
+}
+exports.name = 'Jack'
+exports.age = 16

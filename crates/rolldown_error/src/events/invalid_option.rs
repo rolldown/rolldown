359d003ec9db3d3ae3397e9d46047dcafba973fa
shulaoda <laipichan@qq.com>
2025-01-02 19:30:29 +0800

    refactor: validate file and dir option in rust side

--- crates/rolldown_error/src/events/invalid_option.rs
+++ crates/rolldown_error/src/events/invalid_option.rs
@@ -5,6 +5,7 @@ use crate::{DiagnosticOptions, EventKind};
 pub enum InvalidOptionType {
   UnsupportedCodeSplittingFormat(String),
   InvalidOutputFile,
+  InvalidOutputDirOption,
 }
 
 #[derive(Debug)]
@@ -23,6 +24,7 @@ impl BuildEvent for InvalidOption {
         format!("Invalid value \"{format}\" for option \"output.format\" - UMD and IIFE are not supported for code splitting. You may set `output.inlineDynamicImports` to `true` when using dynamic imports.")
       }
       InvalidOptionType::InvalidOutputFile => "Invalid value for option \"output.file\" - When building multiple chunks, the \"output.dir\" option must be used, not \"output.file\". You may set `output.inlineDynamicImports` to `true` when using dynamic imports.".to_string(),
+      InvalidOptionType::InvalidOutputDirOption => "Invalid value for option \"output.dir\" - you must set either \"output.file\" for a single-file build or \"output.dir\" when generating multiple chunks.".to_string()
     }
   }
 }

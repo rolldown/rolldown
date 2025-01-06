d1d4e0b04e6427a427b831005be738dae6f6f179
dalaoshu <laipichan@qq.com>
2025-01-06 12:44:43 +0800

    chore(infra): remove all zod-related code (#3299)
    ### Description
    
    Related to #3099

--- packages/rolldown/src/log/logging.ts
+++ packages/rolldown/src/log/logging.ts
@@ -1,22 +1,10 @@
-import type Z from 'zod'
-import { z } from 'zod'
-
 export type LogLevel = 'info' | 'debug' | 'warn'
 export type LogLevelOption = LogLevel | 'silent'
 export type LogLevelWithError = LogLevel | 'error'
 
+// TODO RollupLog Fields
 export type RollupLog = any
 export type RollupLogWithString = RollupLog | string
-
-export const LogLevelSchema: Z.ZodType<LogLevel> = z
-  .literal('info')
-  .or(z.literal('debug'))
-  .or(z.literal('warn')) satisfies z.ZodType<LogLevel>
-
-export const LogLevelOptionSchema: Z.ZodType<LogLevelOption> =
-  LogLevelSchema.or(z.literal('silent'))
-export const LogLevelWithErrorSchema: Z.ZodType<LogLevelWithError> =
-  LogLevelSchema.or(z.literal('error'))
 
 export const LOG_LEVEL_SILENT: LogLevelOption = 'silent'
 export const LOG_LEVEL_ERROR = 'error'
@@ -30,8 +18,3 @@ export const logLevelPriority: Record<LogLevelOption, number> = {
   [LOG_LEVEL_WARN]: 2,
   [LOG_LEVEL_SILENT]: 3,
 }
-
-// TODO RollupLog Fields
-export const RollupLogSchema: Z.ZodAny = z.any() satisfies z.ZodType<RollupLog>
-export const RollupLogWithStringSchema: Z.ZodUnion<[Z.ZodAny, Z.ZodString]> =
-  RollupLogSchema.or(z.string()) satisfies z.ZodType<RollupLogWithString>

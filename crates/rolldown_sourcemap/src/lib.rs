217d4e3035a113576d81fbf05867ce528e3034eb
shulaoda <laipichan@qq.com>
2025-01-08 01:35:10 +0800

    refactor(rolldown_sourcemap): tweak line_count related code

--- crates/rolldown_sourcemap/src/lib.rs
+++ crates/rolldown_sourcemap/src/lib.rs
@@ -1,14 +1,13 @@
-// cSpell:disable
-pub use oxc_sourcemap::SourceMapBuilder;
-use oxc_sourcemap::Token;
-pub use oxc_sourcemap::{JSONSourceMap, SourceMap, SourcemapVisualizer};
-pub use source_joiner::SourceJoiner;
-mod lines_count;
-pub use lines_count::lines_count;
+mod source;
 mod source_joiner;
+
+use oxc_sourcemap::Token;
 use rolldown_utils::rayon::{IntoParallelRefIterator, ParallelIterator};
 use rustc_hash::FxHashMap;
-mod source;
+
+pub use oxc_sourcemap::SourceMapBuilder;
+pub use oxc_sourcemap::{JSONSourceMap, SourceMap, SourcemapVisualizer};
+pub use source_joiner::SourceJoiner;
 
 pub use crate::source::{Source, SourceMapSource};
 

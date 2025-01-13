b8f0e19b2e6b0024e61ae07496a0f988bc195925
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-13 15:37:31 +0900

    fix(plugin/json): tree shake json named exports (#3346)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    This was once fixed in https://github.com/rolldown/rolldown/pull/2029,
    but https://github.com/rolldown/rolldown/pull/3103 added another
    `JSON.parse` without pure comment.
    
    Someone noticed the same bug on Vite
    https://github.com/vitejs/vite/pull/19189, so I ported to here.

---
+++ packages/rolldown/tests/fixtures/builtin-plugin/json/side-effects-named/_config.ts
@@ -0,0 +1,19 @@
+import { jsonPlugin } from 'rolldown/experimental'
+import { defineTest } from '@tests'
+import { expect } from 'vitest'
+
+export default defineTest({
+  config: {
+    plugins: [
+      jsonPlugin({
+        stringify: true,
+        namedExports: true,
+        isBuild: false,
+      }),
+    ],
+  },
+  async afterTest(output) {
+    expect(output.output[0].code).not.toContain(`JSON.parse`)
+    await import('./assert.mjs')
+  },
+})

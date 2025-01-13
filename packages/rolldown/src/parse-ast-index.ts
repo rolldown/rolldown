21c23cc79e310684e4cf36041925139ea7f8dbd7
underfin <likui.underfin@gmail.com>
2025-01-13 15:33:11 +0800

    feat: give a code frame error before read program

--- packages/rolldown/src/parse-ast-index.ts
+++ packages/rolldown/src/parse-ast-index.ts
@@ -1,9 +1,12 @@
 import { parseSync, parseAsync } from './binding'
 import type { ParseResult, ParserOptions } from './binding'
+import { locate } from './log/locate-character'
+import { error, logParseError } from './log/logs'
+import { getCodeFrame } from './utils/code-frame'
 
 // The oxc program is a string in the result, we need to parse it to a object.
 // Copy from https://github.com/oxc-project/oxc/blob/main/napi/parser/index.js#L12
-function wrap(result: ParseResult) {
+function wrap(result: ParseResult, sourceText: string) {
   let program: ParseResult['program'],
     module: ParseResult['module'],
     comments: ParseResult['comments'],
@@ -11,6 +14,10 @@ function wrap(result: ParseResult) {
     magicString: ParseResult['magicString']
   return {
     get program() {
+      if (!errors) errors = result.errors
+      if (errors.length > 0) {
+        return normalizeParseError(sourceText, errors)
+      }
       // @ts-expect-error the result.program typing is `Program`
       if (!program) program = JSON.parse(result.program)
       return program
@@ -34,6 +41,34 @@ function wrap(result: ParseResult) {
   }
 }
 
+function normalizeParseError(
+  sourceText: string,
+  errors: ParseResult['errors'],
+) {
+  let message = `Parse failed with ${errors.length} error${errors.length < 2 ? '' : 's'}:\n`
+  for (let i = 0; i < errors.length; i++) {
+    if (i >= 5) {
+      message += '\n...'
+      break
+    }
+    const e = errors[i]
+    message +=
+      e.message +
+      '\n' +
+      e.labels
+        .map((label) => {
+          const location = locate(sourceText, label.start, { offsetLine: 1 })
+          if (!location) {
+            return
+          }
+          return getCodeFrame(sourceText, location.line, location.column)
+        })
+        .filter(Boolean)
+        .join('\n')
+  }
+  return error(logParseError(message))
+}
+
 // The api compat to rollup `parseAst` and `parseAstAsync`.
 
 export function parseAst(
@@ -41,7 +76,7 @@ export function parseAst(
   sourceText: string,
   options?: ParserOptions | undefined | null,
 ): ParseResult {
-  return wrap(parseSync(filename, sourceText, options))
+  return wrap(parseSync(filename, sourceText, options), sourceText)
 }
 
 export async function parseAstAsync(
@@ -49,7 +84,7 @@ export async function parseAstAsync(
   sourceText: string,
   options?: ParserOptions | undefined | null,
 ): Promise<ParseResult> {
-  return wrap(await parseAsync(filename, sourceText, options))
+  return wrap(await parseAsync(filename, sourceText, options), sourceText)
 }
 
 export type { ParseResult, ParserOptions }
--- packages/rolldown/tests/parse-ast/parse-ast.test.ts
+++ packages/rolldown/tests/parse-ast/parse-ast.test.ts
@@ -10,3 +10,18 @@ test('rolldown/parseAst parseAstAsync', async () => {
   const result = await parseAstAsync('test.js', 'console.log("hello")')
   expect(result.program.type).toBe('Program')
 })
+
+test('rolldown/parseAst parseSync + error', async () => {
+  const result = parseAst('test.js', '\nconso le.log("hello")')
+  try {
+    result.program
+  } catch (error: any) {
+    expect(error.message).toMatchInlineSnapshot(`
+      "Parse failed with 1 error:
+      Expected a semicolon or an implicit semicolon after a statement, but found none
+      1: 
+      2: conso le.log("hello")
+              ^"
+    `)
+  }
+})

2008cd6b93f51218af694559ec60c86dadbb5ceb
Yunfei <i.heyunfei@gmail.com>
2025-01-24 21:57:34 +0800

    feat(node/dev-server): serve output files

---
+++ packages/test-dev-server/src/define-dev-config.ts
@@ -0,0 +1,12 @@
+import type { BuildOptions } from 'rolldown'
+
+export interface ServeOptions {}
+
+export interface DevConfig {
+  build?: BuildOptions
+  serve?: ServeOptions
+}
+
+export function defineDevConfig(config: DevConfig): DevConfig {
+  return config
+}
--- packages/test-dev-server/src/index.ts
+++ packages/test-dev-server/src/index.ts
@@ -1,1 +1,45 @@
-export function serve(): void {}
+import { build } from 'rolldown'
+import nodePath from 'node:path'
+import connect from 'connect'
+import serveStatic from 'serve-static'
+import http from 'node:http'
+import { DevConfig, defineDevConfig } from './define-dev-config.js'
+import { createDevServerPlugin } from './create-dev-server-plugin.js'
+
+async function loadDevConfig(): Promise<DevConfig> {
+  const exports = await import(nodePath.join(process.cwd(), 'dev.config.mjs'))
+  return exports.default
+}
+
+export async function serve(): Promise<void> {
+  console.log('Starting dev server...')
+  const devConfig = await loadDevConfig()
+  const buildOptions = devConfig.build ?? {}
+  if (buildOptions.plugins == null || Array.isArray(buildOptions.plugins)) {
+    buildOptions.plugins = [
+      ...(buildOptions.plugins || []),
+      createDevServerPlugin(),
+    ]
+  } else {
+    throw new Error('Plugins must be an array')
+  }
+  buildOptions.write = true
+
+  await build(buildOptions)
+
+  const app = connect()
+
+  console.log(`Serving ${nodePath.join(process.cwd(), 'dist')}`)
+  app.use(
+    serveStatic(nodePath.join(process.cwd(), 'dist'), {
+      index: ['index.html'],
+      extensions: ['html'],
+    }),
+  )
+
+  //create node.js http server and listen on port
+  http.createServer(app).listen(3000)
+  console.log('Server listening on http://localhost:3000')
+}
+
+export { defineDevConfig }

8655a95035ff4c9f00a9862d4c035ad503025aee
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-01-30 16:10:21 +0900

    test: add test

---
+++ crates/rolldown/tests/rolldown/issues/3438/main.js
@@ -0,0 +1,15 @@
+import { clear as repro1_clear } from "./repro1.js"
+import { clear as repro2_clear, clear$1 as repro2_clear$1 } from "./repro2.js"
+import { clear as repro3_clear, clear$1 as repro3_clear$1, clear$2 as repro3_clear$2 } from "./repro3.js"
+
+export default [
+  repro1_clear,
+  repro2_clear,
+  repro2_clear$1,
+  repro3_clear,
+  repro3_clear$1,
+  repro3_clear$2,
+  () => import("./repro1.js"),
+  () => import("./repro2.js"),
+  () => import("./repro3.js"),
+]
---
+++ crates/rolldown/tests/rolldown/issues/3438/repro3.js
@@ -0,0 +1,3 @@
+export const clear$2 = "repro3_clear$2";
+export const clear$1 = "repro3_clear$1";
+export const clear = "repro3_clear";

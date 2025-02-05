4490b26c21457d644b9b548fd0356e478dab96ce
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-02-05 09:34:19 +0900

    Revert "refactor: minor"
    This reverts commit 6ee6688f74f74ce994162b2428cc6059991c41cc.

--- crates/rolldown_common/src/chunk/mod.rs
+++ crates/rolldown_common/src/chunk/mod.rs
@@ -86,19 +86,15 @@ impl Chunk {
   }
 
   pub fn import_path_for(&self, importee: &Chunk) -> String {
-    let importee_filename = importee.absolute_preliminary_filename.as_ref().unwrap();
-    self.relative_path_to(importee_filename.as_path())
-  }
-
-  pub fn relative_path_to(&self, target: &std::path::Path) -> String {
     let importer_dir =
       self.absolute_preliminary_filename.as_ref().unwrap().as_path().parent().unwrap();
-    let relative_path = target.relative(importer_dir).as_path().expect_to_slash();
+    let importee_filename = importee.absolute_preliminary_filename.as_ref().unwrap();
+    let import_path = importee_filename.relative(importer_dir).as_path().expect_to_slash();
 
-    if relative_path.starts_with('.') {
-      relative_path
+    if import_path.starts_with('.') {
+      import_path
     } else {
-      format!("./{relative_path}")
+      format!("./{import_path}")
     }
   }
 

a12bdfff24587c2fe3d6014da454abdb410fef59
underfin <likui.underfin@gmail.com>
2025-02-11 17:34:11 +0800

    chore: using import

--- packages/rolldown/tests/module-federation/module-federation.test.ts
+++ packages/rolldown/tests/module-federation/module-federation.test.ts
@@ -64,7 +64,7 @@ describe('module-federation', () => {
     })
 
     // Test the remote manifest json
-    const remoteManifest = require('./dist/remote/mf-manifest.json')
+    const remoteManifest = await import('./dist/remote/mf-manifest.json')
     expect(remoteManifest).toMatchSnapshot()
 
     // Test the remote entry

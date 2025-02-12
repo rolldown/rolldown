5e09058228d1a20a72087e04845c4868a2abdaa4
Boshen <boshenc@gmail.com>
2025-02-12 15:18:46 +0800

    chore(rolldown): oxc v0.50.0 (#3557)
    closes #3556
    
    ---------
    
    Co-authored-by: IWANABETHATGUY <iwanabethatguy@qq.com>

--- crates/rolldown/tests/rolldown/tree_shaking/dynamic_import_await_destruct/main.js
+++ crates/rolldown/tests/rolldown/tree_shaking/dynamic_import_await_destruct/main.js
@@ -7,8 +7,6 @@ console.log(x);
 
 
 async function test() {
-  // FIXME: this is sub optimal, should not reference `barbarbar`
-  // since the `await` is unreachable
   const {thing: a, bar: barbarbar} = await import("./lib.js")
   barbarbar
 }

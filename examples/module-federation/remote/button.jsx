a56e0e9e4ba3a996160c50aa0f651a219f37ffc5
underfin <likui.underfin@gmail.com>
2025-02-13 10:44:59 +0800

    chore: update example

--- examples/module-federation/remote/button.jsx
+++ examples/module-federation/remote/button.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 export default function Button() {
   const [count, setCount] = useState(0)
-  return  <button
-  onClick={() => setCount((count) => count + 1)}
->
-  count is: {count}
-</button>
+  return (
+    <button  onClick={() => setCount((count) => count + 1)} >
+      count is: {count}
+    </button>
+  )
 }

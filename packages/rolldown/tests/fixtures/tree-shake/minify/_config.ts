a07c99f0e1476566ff64f6f969fb850eb9e29752
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-02-19 14:31:29 +0800

    chore: explicit config oxc-minify

---
+++ packages/rolldown/tests/fixtures/tree-shake/minify/_config.ts
@@ -0,0 +1,20 @@
+import type { OutputChunk as RolldownOutputChunk } from 'rolldown'
+import { defineTest } from 'rolldown-tests'
+import { expect } from 'vitest'
+
+export default defineTest({
+  config: {
+    output: {
+      minify: true,
+    },
+  },
+  afterTest: (output) => {
+    output.output
+      .filter(({ type }) => type === 'chunk')
+      .forEach((chunk) => {
+        let code = (chunk as RolldownOutputChunk).code
+        // should be mangled, oxc-minify doesn't enable `toplevel` mangle by default
+        expect(code.includes(`test`)).toBe(false)
+      })
+  },
+})

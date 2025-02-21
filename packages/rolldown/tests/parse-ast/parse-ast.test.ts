930d487fdd3c2734d703b65b796b6876f5ac8d81
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-02-21 15:28:13 +0900

    test: update

--- packages/rolldown/tests/parse-ast/parse-ast.test.ts
+++ packages/rolldown/tests/parse-ast/parse-ast.test.ts
@@ -2,18 +2,18 @@ import { parseAst, parseAstAsync } from 'rolldown/parseAst'
 import { test, expect } from 'vitest'
 
 test('rolldown/parseAst parseSync', async () => {
-  const result = parseAst('test.js', 'console.log("hello")')
+  const result = parseAst('console.log("hello")')
   expect(result.type).toBe('Program')
 })
 
 test('rolldown/parseAst parseAstAsync', async () => {
-  const result = await parseAstAsync('test.js', 'console.log("hello")')
+  const result = await parseAstAsync('console.log("hello")')
   expect(result.type).toBe('Program')
 })
 
 test('rolldown/parseAst parseSync + error', async () => {
   try {
-    parseAst('test.js', '\nconso le.log("hello")')
+    parseAst('\nconso le.log("hello")')
     expect.unreachable()
   } catch (error: any) {
     expect(error.message).toMatchInlineSnapshot(`

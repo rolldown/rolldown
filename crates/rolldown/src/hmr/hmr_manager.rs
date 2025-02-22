6d2f8e33e96a193c4a1c19d4757233374dc19507
Yunfei <i.heyunfei@gmail.com>
2025-02-23 04:10:34 +0800

    feat(hmr): compute affected modules

--- crates/rolldown/src/hmr/hmr_manager.rs
+++ crates/rolldown/src/hmr/hmr_manager.rs
@@ -1,14 +1,42 @@
 use arcstr::ArcStr;
-use rolldown_common::{ModuleIdx, ModuleTable};
+use rolldown_common::{EcmaModuleAstUsage, Module, ModuleIdx, ModuleTable};
 use rolldown_utils::indexmap::FxIndexMap;
 
 pub struct HmrManager {
-  _modules: ModuleTable,
-  pub _module_idx_by_abs_path: FxIndexMap<ArcStr, ModuleIdx>,
+  module_db: ModuleTable,
+  pub module_idx_by_abs_path: FxIndexMap<ArcStr, ModuleIdx>,
 }
 
 impl HmrManager {
-  pub fn generate_hmr_patch(&self, _changed_file_paths: Vec<String>) -> String {
+  #[allow(clippy::dbg_macro)] // FIXME: Remove dbg! macro once the feature is stable
+  pub fn generate_hmr_patch(&self, changed_file_paths: Vec<String>) -> String {
+    let mut changed_modules = vec![];
+    for changed_file_path in changed_file_paths {
+      let changed_file_path = ArcStr::from(changed_file_path);
+      if let Some(module_idx) = self.module_idx_by_abs_path.get(&changed_file_path) {
+        changed_modules.push(*module_idx);
+      } else {
+        dbg!("No corresponding module found for changed file path: {:?}", changed_file_path);
+      }
+    }
+
+    // Only changed modules might introduce new modules, we run a new module loader to fetch possible new modules and updated content of changed modules
+    // TODO(hyf0): Run module loader
+
+    let mut affected_modules = vec![];
+    while let Some(changed_module_idx) = changed_modules.pop() {
+      let Module::Normal(changed_module) = &self.module_db.modules[changed_module_idx] else {
+        continue;
+      };
+
+      if changed_module.ast_usage.contains(EcmaModuleAstUsage::HmrSelfAccept) {
+        affected_modules.push(changed_module_idx);
+        continue;
+      }
+
+      // TODO(hyf0): If it's not a self-accept module, we should traverse its dependents recursively
+    }
+
     todo!()
   }
 }

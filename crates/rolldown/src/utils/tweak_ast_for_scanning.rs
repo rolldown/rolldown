6016b3cfd91eba5fe0945ed28927b706a0f0d959
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-02-27 00:55:54 +0800

    fix: `keepNames` with function declaration panic (#3687)
    <!-- Thank you for contributing! -->
    
    ### Description
    1. fix
    https://discord.com/channels/1171783886200643635/1172053084042690590/threads/1344223929539100683
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- crates/rolldown/src/utils/tweak_ast_for_scanning.rs
+++ crates/rolldown/src/utils/tweak_ast_for_scanning.rs
@@ -1,20 +1,21 @@
 use itertools::Itertools;
-use oxc::allocator::Allocator;
+use oxc::allocator::{Address, Allocator};
 use oxc::ast::ast::{self, BindingPatternKind, Declaration, ImportOrExportKind, Statement};
 use oxc::ast::visit::walk_mut;
 use oxc::ast::{NONE, VisitMut};
 use oxc::span::{SPAN, Span};
 use rolldown_ecmascript_utils::{AstSnippet, StatementExt, TakeIn};
+use rustc_hash::FxHashMap;
 
 /// Pre-process is a essential step to make rolldown generate correct and efficient code.
 pub struct PreProcessor<'ast> {
   snippet: AstSnippet<'ast>,
   pub contains_use_strict: bool,
-  /// For top level statements, this is used to store none_hoisted statements.
-  /// For none top level statements, this is used to store split `VarDeclaration`.
-  stmt_temp_storage: Vec<Statement<'ast>>,
-  need_push_ast: bool,
+  /// used to store none_hoisted statements.
+  top_level_stmt_temp_storage: Vec<Statement<'ast>>,
   keep_names: bool,
+  statement_stack: Vec<Address>,
+  statement_replace_map: FxHashMap<Address, Vec<Statement<'ast>>>,
 }
 
 impl<'ast> PreProcessor<'ast> {
@@ -22,9 +23,10 @@ impl<'ast> PreProcessor<'ast> {
     Self {
       snippet: AstSnippet::new(alloc),
       contains_use_strict: false,
-      stmt_temp_storage: vec![],
-      need_push_ast: false,
+      top_level_stmt_temp_storage: vec![],
       keep_names,
+      statement_stack: vec![],
+      statement_replace_map: FxHashMap::default(),
     }
   }
 
@@ -77,33 +79,39 @@ impl<'ast> VisitMut<'ast> for PreProcessor<'ast> {
     });
     let original_body = program.body.take_in(self.snippet.alloc());
     program.body.reserve_exact(original_body.len());
-    self.stmt_temp_storage = Vec::with_capacity(
+    self.top_level_stmt_temp_storage = Vec::with_capacity(
       original_body.iter().filter(|stmt| !stmt.is_module_declaration_with_source()).count(),
     );
 
     for mut stmt in original_body {
-      self.need_push_ast = true;
+      let stmt_addr = Address::from_ptr(&stmt);
+      self.statement_stack.push(stmt_addr);
       walk_mut::walk_statement(self, &mut stmt);
-      if self.need_push_ast {
-        if stmt.is_module_declaration_with_source() {
-          program.body.push(stmt);
-        } else {
-          self.stmt_temp_storage.push(stmt);
-        }
+      self.statement_stack.pop();
+      if let Some(stmts) = self.statement_replace_map.remove(&stmt_addr) {
+        self.top_level_stmt_temp_storage.extend(stmts);
+      } else if stmt.is_module_declaration_with_source() {
+        program.body.push(stmt);
+      } else {
+        self.top_level_stmt_temp_storage.push(stmt);
       }
     }
-    program.body.extend(std::mem::take(&mut self.stmt_temp_storage));
+    program.body.extend(std::mem::take(&mut self.top_level_stmt_temp_storage));
   }
 
   fn visit_statements(&mut self, it: &mut oxc::allocator::Vec<'ast, Statement<'ast>>) {
     if self.keep_names {
       let stmts = it.take_in(self.snippet.alloc());
       for mut stmt in stmts {
+        let stmt_addr = Address::from_ptr(&stmt);
+        self.statement_stack.push(stmt_addr);
         walk_mut::walk_statement(self, &mut stmt);
-        if self.stmt_temp_storage.is_empty() {
-          it.push(stmt);
+        self.statement_stack.pop();
+
+        if let Some(stmts) = self.statement_replace_map.remove(&stmt_addr) {
+          it.extend(stmts);
         } else {
-          it.extend(self.stmt_temp_storage.drain(..));
+          it.push(stmt);
         }
       }
     } else {
@@ -115,8 +123,9 @@ impl<'ast> VisitMut<'ast> for PreProcessor<'ast> {
     match it {
       Declaration::VariableDeclaration(decl) => {
         if decl.declarations.len() > 1 && self.keep_names {
-          self.stmt_temp_storage.extend(self.split_var_declaration(decl, None));
-          self.need_push_ast = false;
+          let stmt_addr = self.statement_stack.last().copied().unwrap();
+          let new_stmts = self.split_var_declaration(decl, None);
+          self.statement_replace_map.insert(stmt_addr, new_stmts);
         }
       }
       Declaration::FunctionDeclaration(_) | Declaration::ClassDeclaration(_) => {}
@@ -145,8 +154,7 @@ impl<'ast> VisitMut<'ast> for PreProcessor<'ast> {
       .any(|declarator| matches!(declarator.id.kind, BindingPatternKind::BindingIdentifier(_)))
     {
       let rewritten = self.split_var_declaration(var_decl, Some(named_decl_span));
-      self.stmt_temp_storage.extend(rewritten);
-      self.need_push_ast = false;
+      self.statement_replace_map.insert(self.statement_stack.last().copied().unwrap(), rewritten);
     }
   }
 

1f2dc85542a14e1d1ba8035ad026fab50ceae1a7
Jeremiasz Major <jrh.mjr@gmail.com>
2025-02-27 15:59:58 +0100

    chore: ensure `@codspeed/vitest-plugin` is installed

--- packages/bench/vitest.config.ts
+++ packages/bench/vitest.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitest/config'
-// @ts-ignore: `@codspeed/vitest-plugin` doesn't specify `types` in `package.json#exports`.
 import codspeedPlugin from '@codspeed/vitest-plugin'
 
 export default defineConfig({

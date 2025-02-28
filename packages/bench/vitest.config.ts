45bb6f339cf5361ebc0bbcb3cee53067b4ffd98b
Jeremiasz Major <jrh.mjr@gmail.com>
2025-02-28 04:37:54 +0100

    chore: remove unused vitest config (#3704)
    ### Description
    
    Leftover from #718. Prevents Vitest plugin for VS Code from erroring on
    startup.

--- packages/bench/vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitest/config'
-// @ts-ignore: `@codspeed/vitest-plugin` doesn't specify `types` in `package.json#exports`.
-import codspeedPlugin from '@codspeed/vitest-plugin'
-
-export default defineConfig({
-  plugins: process.env.CI ? [codspeedPlugin()] : [],
-})

adf28d07091dadb9908f341f663dd6abb319bad5
shulaoda <laipichan@qq.com>
2025-03-01 02:31:20 +0800

    test(node): remove unnecessary file test

--- packages/rolldown/tests/fixtures/verify-options/_config.ts
+++ packages/rolldown/tests/fixtures/verify-options/_config.ts
@@ -4,12 +4,14 @@ import { defineTest } from 'rolldown-tests'
 export default defineTest({
   config: {
     output: {
+      file: 'dist/main.js',
       target: 'es2015',
     },
     plugins: [
       {
         name: 'test-plugin',
         outputOptions: function (options) {
+          expect(options.file).toBe('dist/main.js')
           expect(options.target).toBe('es2015')
         },
       },

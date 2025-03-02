f3e341feadc1485f61cbab9411a143261ba6d8eb
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-03-02 16:02:33 +0800

    fix: minify with jsx preserve

---
+++ packages/rolldown/tests/fixtures/jsx/preserve-minify/_config.ts
@@ -0,0 +1,19 @@
+import { defineTest } from 'rolldown-tests'
+import { expect } from 'vitest'
+import { getOutputChunk } from 'rolldown-tests/utils'
+
+export default defineTest({
+  config: {
+    input: 'main.jsx',
+    jsx: {
+      mode: 'preserve',
+    },
+    output: {
+      minify: true,
+    },
+  },
+  afterTest: (output) => {
+    const chunk = getOutputChunk(output)[0]
+    expect(chunk.code.includes('<div>Hello World!</div>')).toBe(true)
+  },
+})

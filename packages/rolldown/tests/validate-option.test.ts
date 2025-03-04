b62607931924ffbb21f33be68d7e630f3ade4dcd
underfin <likui.underfin@gmail.com>
2025-03-04 10:51:23 +0800

    feat: validate option (#3748)
    <!-- Thank you for contributing! -->
    
    ### Description
    related https://github.com/rolldown/rolldown/issues/3697
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ packages/rolldown/tests/validate-option.test.ts
@@ -0,0 +1,45 @@
+import { test, expect } from 'vitest'
+import { rolldown } from 'rolldown'
+
+test('validate input option', async () => {
+  try {
+    await rolldown({
+      // @ts-ignore invalid value
+      input: 1,
+      cwd: import.meta.dirname,
+      // @ts-ignore invalid key
+      foo: 'bar',
+      resolve: {
+        // @ts-ignore nested invalid key
+        foo: 'bar',
+      },
+    })
+    expect.unreachable()
+  } catch (error: any) {
+    expect(error.message).toMatchInlineSnapshot(`
+          "Failed validate input options.
+          - For the "input". Invalid type: Expected (string | Array | Object) but received 1. 
+          - For the "resolve.foo". Invalid key: Expected never but received "foo". 
+          - For the "foo". Invalid key: Expected never but received "foo". "
+        `)
+  }
+})
+
+test('validate output option', async () => {
+  try {
+    const bundle = await rolldown({
+      input: './build-api/main.js',
+      cwd: import.meta.dirname,
+    })
+    await bundle.write({
+      // @ts-ignore  invalid key
+      foo: 'bar',
+    })
+    expect.unreachable()
+  } catch (error: any) {
+    expect(error.message).toMatchInlineSnapshot(`
+        "Failed validate output options.
+        - For the "foo". Invalid key: Expected never but received "foo". "
+      `)
+  }
+})

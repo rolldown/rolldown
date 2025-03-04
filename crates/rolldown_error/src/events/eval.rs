d4e0c3ab4cd99bda1cbf2311d3bb8cdb7c6fa7d3
Ulrich Stark <github@ustark.de>
2025-03-04 21:29:37 +0100

    fix: make diagnostic message for eval build event easier to understand (#3759)

--- crates/rolldown_error/src/events/eval.rs
+++ crates/rolldown_error/src/events/eval.rs
@@ -19,7 +19,7 @@ impl BuildEvent for Eval {
 
   fn message(&self, _opts: &DiagnosticOptions) -> String {
     format!(
-      "Use of eval in '{}' is strongly discouraged as it poses security risks and may cause issues with minification.",
+      "Use of `eval` function in '{}' is strongly discouraged as it poses security risks and may cause issues with minification.",
       self.filename
     )
   }
@@ -27,14 +27,14 @@ impl BuildEvent for Eval {
   fn on_diagnostic(&self, diagnostic: &mut Diagnostic, opts: &DiagnosticOptions) {
     let filename = opts.stabilize_path(&self.filename);
 
-    diagnostic.title = "Use of eval is strongly discouraged as it poses security risks and may cause issues with minification.".to_string();
+    diagnostic.title = "Use of `eval` function is strongly discouraged as it poses security risks and may cause issues with minification.".to_string();
 
     let file_id = diagnostic.add_file(filename, self.source.clone());
 
     diagnostic.add_label(
       &file_id,
       self.span.start..self.span.end,
-      "Use `eval` function here.".to_string(),
+      "Use of `eval` function here.".to_string(),
     );
   }
 }

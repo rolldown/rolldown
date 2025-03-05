cf305cee3ae69426cb855ea528452d0422f97824
AliceLanniste <1399789151@qq.com>
2025-03-05 14:37:48 +0800

    test(node): remove unnecessary footer test (#3758)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->
    Description
    Related to https://github.com/rolldown/rolldown/issues/3256

---
+++ packages/rolldown/tests/fixtures/function/footer/async_function/_config.ts
@@ -0,0 +1,16 @@
+import { expect } from 'vitest'
+import { defineTest } from 'rolldown-tests'
+
+const footerTxt = '// footer test\n'
+const footer = async () => footerTxt
+
+export default defineTest({
+  config: {
+    output: {
+      footer,
+    },
+  },
+  afterTest: (output) => {
+    expect(output.output[0].code.endsWith(footerTxt)).toBe(true)
+  },
+})

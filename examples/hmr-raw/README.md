77ef379d5c97eb5deaa19f025d5133611a1da77c
Yunfei He <i.heyunfei@gmail.com>
2025-03-09 23:29:38 +0800

    chore(exmaple): use a more realistic example (#3796)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

---
+++ examples/hmr-raw/README.md
@@ -0,0 +1,1 @@
+Example code come from https://github.com/OmarShehata/vite-hot-reload-example.
---
+++ examples/hmr-raw/src/main.js
@@ -0,0 +1,28 @@
+import HotModuleReloadSetup from './HotModuleReloadSetup.js'
+
+async function init() {
+  // Setup the canvas & render loop
+  const canvas = document.createElement('canvas')
+  document.body.appendChild(canvas)
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+  }
+  resizeCanvas()
+  window.addEventListener('resize', resizeCanvas)
+
+  // Setup HMR
+  const hmr = new HotModuleReloadSetup()
+  // Load a module that will be updated dynamically
+  hmr.import(await import('./Draw.js'), canvas)
+  // Now we access it through hmr.instances['Draw']
+  // which will point to the new module when it gets swapped
+  function draw() {
+    hmr.instances['Draw'].draw()
+    requestAnimationFrame(draw)
+  }
+  draw()
+}
+
+init()

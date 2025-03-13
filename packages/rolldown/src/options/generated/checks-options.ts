410038cbfbf5958cb8c14edfa42c1837ccd49e36
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-03-13 12:48:07 +0800

    chore: update

---
+++ packages/rolldown/src/options/generated/checks-options.ts
@@ -0,0 +1,15 @@
+// Auto-generated code, DO NOT EDIT DIRECTLY!
+// To edit this generated file you have to edit `tasks/gen_options/src/generators/checks.rs`
+
+export interface ChecksOptions {
+  circularDependency?: boolean
+  eval?: boolean
+  missingGlobalName?: boolean
+  missingNameOptionForIifeExport?: boolean
+  mixedExport?: boolean
+  unresolvedEntry?: boolean
+  unresolvedImport?: boolean
+  filenameConflict?: boolean
+  commonJsVariableInEsm?: boolean
+  importIsUndefined?: boolean
+}

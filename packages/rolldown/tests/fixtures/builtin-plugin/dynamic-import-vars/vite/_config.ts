e8c2eac48508a3c3b3630ee40d55066ec2c46e58
dalaoshu <laipichan@qq.com>
2025-03-21 01:13:46 +0800

    test(builtin-plugin): migrate `dynamicImportVars` test cases from `Vite` (#3912)
    ### Description
    
    Related to #3892
    
    Based on the snapshot results, there seem to be some issues with the
    current `dynamicImportVars` plugin.

---
+++ packages/rolldown/tests/fixtures/builtin-plugin/dynamic-import-vars/vite/_config.ts
@@ -0,0 +1,19 @@
+import path from 'node:path'
+import { expect } from 'vitest'
+import { defineTest } from 'rolldown-tests'
+import { dynamicImportVarsPlugin } from 'rolldown/experimental'
+
+export default defineTest({
+  config: {
+    plugins: [dynamicImportVarsPlugin()],
+  },
+  async afterTest(output) {
+    for (const chunk of output.output) {
+      if (chunk.type === 'chunk') {
+        await expect(chunk.code).toMatchFileSnapshot(
+          path.resolve(import.meta.dirname, 'main.js.snap'),
+        )
+      }
+    }
+  },
+})

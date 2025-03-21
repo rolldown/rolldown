d5d7e5ab1857634e2f2a31b646189dd069d1bf00
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-21 18:31:32 +0900

    feat: support pkg.pr.new

--- packages/rolldown/src/webcontainer-fallback.js
+++ packages/rolldown/src/webcontainer-fallback.js
@@ -1,17 +1,32 @@
 const fs = require('node:fs')
 const childProcess = require('node:child_process')
 
-// TODO: support pkg.pr.new?
 const rolldownPkg = JSON.parse(
   fs.readFileSync(require.resolve('rolldown/package.json'), 'utf-8'),
 )
 const version = rolldownPkg.version
-
 const baseDir = `/tmp/rolldown-${version}`
-const bindingPkg = `@rolldown/binding-wasm32-wasi@${version}`
 const bindingEntry = `${baseDir}/node_modules/@rolldown/binding-wasm32-wasi/rolldown-binding.wasi.cjs`
 
 if (!fs.existsSync(bindingEntry)) {
+  let bindingPkg = `@rolldown/binding-wasm32-wasi@${version}`
+
+  try {
+    // check if pkg.pr.new
+    const info = JSON.parse(
+      childProcess.execFileSync('npm', ['why', 'rolldown'], {
+        encoding: 'utf-8',
+      }),
+    )
+    const spec = info[0].dependents[0].spec
+    if (spec.starts('https://pkg.pr.new/')) {
+      const commit = spec.split('@').at(-1)
+      bindingPkg = `https://pkg.pr.new/@rolldown/binding-wasm32-wasi@${commit}`
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
   fs.rmSync(baseDir, { recursive: true, force: true })
   fs.mkdirSync(baseDir, { recursive: true })
   console.log('Downloading @rolldown/binding-wasm32-wasi on WebContainer...')

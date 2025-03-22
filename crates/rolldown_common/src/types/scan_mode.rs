57a7e33975bd99d41bbf00719280cf90c2493311
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-03-22 18:55:07 +0800

    feat: add data structure that used for incremental build

---
+++ crates/rolldown_common/src/types/scan_mode.rs
@@ -0,0 +1,16 @@
+use arcstr::ArcStr;
+pub enum ScanMode {
+  Full,
+  //// vector of module id
+  Partial(Vec<ArcStr>),
+}
+
+impl ScanMode {
+  /// Returns `true` if the scan mode is [`Full`].
+  ///
+  /// [`Full`]: ScanMode::Full
+  #[must_use]
+  pub fn is_full(&self) -> bool {
+    matches!(self, Self::Full)
+  }
+}

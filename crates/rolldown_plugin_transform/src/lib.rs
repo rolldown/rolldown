303d35853d2b2c048e7a9e053d98fff49c548d38
shulaoda <laipichan@qq.com>
2025-03-22 16:57:52 +0800

    refactor(rolldown_plugin_transform): improve code style and align filter logic with Vite

--- crates/rolldown_plugin_transform/src/lib.rs
+++ crates/rolldown_plugin_transform/src/lib.rs
@@ -1,19 +1,21 @@
+use std::borrow::Cow;
+use std::path::Path;
+use sugar_path::SugarPath;
+
 use oxc::{
   codegen::{CodeGenerator, CodegenOptions, CodegenReturn},
   semantic::SemanticBuilder,
   span::SourceType,
-  transformer::{ReactRefreshOptions, TransformOptions, Transformer},
+  transformer::{EnvOptions, ReactRefreshOptions, TransformOptions, Transformer},
 };
+
 use rolldown_common::ModuleType;
 use rolldown_ecmascript::EcmaCompiler;
-
-use oxc::transformer::EnvOptions;
-use rolldown_plugin::Plugin;
-use rolldown_utils::clean_url::clean_url;
-use rolldown_utils::pattern_filter::{self, StringOrRegex};
-use std::borrow::Cow;
-use std::path::Path;
-use sugar_path::SugarPath;
+use rolldown_plugin::{Plugin, SharedTransformPluginContext};
+use rolldown_utils::{
+  clean_url::clean_url,
+  pattern_filter::{StringOrRegex, filter as pattern_filter},
+};
 
 #[derive(Debug, Default)]
 pub struct TransformPlugin {
@@ -35,31 +37,28 @@ impl Plugin for TransformPlugin {
 
   async fn transform(
     &self,
-    ctx: rolldown_plugin::SharedTransformPluginContext,
+    ctx: SharedTransformPluginContext,
     args: &rolldown_plugin::HookTransformArgs<'_>,
   ) -> rolldown_plugin::HookTransformReturn {
     if !self.filter(&ctx, args.id, args.module_type) {
       return Ok(None);
     }
+
     let source_type = {
-      let mut default_source_type = SourceType::default();
-      default_source_type = match args.module_type {
+      let default_source_type = SourceType::default();
+      match args.module_type {
         ModuleType::Jsx => default_source_type.with_jsx(true),
         ModuleType::Ts => default_source_type.with_typescript(true),
         ModuleType::Tsx => default_source_type.with_typescript(true).with_jsx(true),
         _ => return Ok(None),
-      };
-      default_source_type
-    };
-    let code = args.code;
-    let parse_result = EcmaCompiler::parse(args.id, code, source_type);
-    let mut ast = match parse_result {
-      Ok(ecma_ast) => ecma_ast,
-      Err(errs) => {
-        // TODO: better diagnostics handling
-        return Err(anyhow::format_err!("Error occurred when parsing {}\n: {:?}", args.id, errs));
       }
     };
+
+    let parse_result = EcmaCompiler::parse(args.id, args.code, source_type);
+    let mut ecma_ast = parse_result.map_err(|errs| {
+      // TODO: Improve diagnostics handling
+      anyhow::anyhow!("Error occurred when parsing {}\n: {:?}", args.id, errs)
+    })?;
 
     let env = if self.target.is_some() && self.browserslist.is_some() {
       Err("Cannot specify both `target` and `browserslist` at the same time".to_string())
@@ -69,21 +68,16 @@ impl Plugin for TransformPlugin {
       EnvOptions::from_browserslist_query(browserslist)
     } else {
       Ok(EnvOptions::default())
-    }
-    .map_err(|e| anyhow::anyhow!(e))?;
+    };
 
-    let ret = ast.program.with_mut(move |fields| {
+    let env = env.map_err(|e| anyhow::anyhow!(e))?;
+    let ret = ecma_ast.program.with_mut(move |fields| {
       let mut transformer_options = TransformOptions { env, ..TransformOptions::default() };
-      match args.module_type {
-        ModuleType::Jsx | ModuleType::Tsx => {
-          transformer_options.jsx.jsx_plugin = true;
-          if self.react_refresh {
-            transformer_options.jsx.refresh = Some(ReactRefreshOptions::default());
-          }
-        }
-        ModuleType::Ts => {}
-        _ => {
-          unreachable!()
+
+      if !matches!(args.module_type, ModuleType::Ts) {
+        transformer_options.jsx.jsx_plugin = true;
+        if self.react_refresh {
+          transformer_options.jsx.refresh = Some(ReactRefreshOptions::default());
         }
       }
 
@@ -91,28 +85,30 @@ impl Plugin for TransformPlugin {
       Transformer::new(fields.allocator, Path::new(args.id), &transformer_options)
         .build_with_scoping(scoping, fields.program)
     });
+
     if !ret.errors.is_empty() {
       // TODO: better error handling
-      return Err(anyhow::anyhow!("Transform failed, got {:#?}", ret.errors));
+      Err(anyhow::anyhow!("Transform failed, got {:#?}", ret.errors))?;
     }
-    let CodegenReturn { code, map, .. } = CodeGenerator::new()
+
+    let CodegenReturn { mut code, map, .. } = CodeGenerator::new()
       .with_options(CodegenOptions {
         source_map_path: Some(args.id.into()),
         ..CodegenOptions::default()
       })
-      .build(ast.program());
-    let code = if let Some(ref inject) = self.jsx_inject {
-      let mut ret = String::with_capacity(code.len() + 1 + inject.len());
-      ret.push_str(inject);
-      ret.push(';');
-      ret.push_str(&code);
-      ret
-    } else {
-      code
-    };
+      .build(ecma_ast.program());
+
+    if let Some(inject) = &self.jsx_inject {
+      let mut new_code = String::with_capacity(inject.len() + 1 + code.len());
+      new_code.push_str(inject);
+      new_code.push(';');
+      new_code.push_str(&code);
+      code = new_code;
+    }
+
     Ok(Some(rolldown_plugin::HookTransformOutput {
-      code: Some(code),
       map,
+      code: Some(code),
       module_type: Some(ModuleType::Js),
       ..Default::default()
     }))
@@ -120,27 +116,21 @@ impl Plugin for TransformPlugin {
 }
 
 impl TransformPlugin {
-  fn filter(
-    &self,
-    ctx: &rolldown_plugin::SharedTransformPluginContext,
-    id: &str,
-    module_type: &ModuleType,
-  ) -> bool {
+  fn filter(&self, ctx: &SharedTransformPluginContext, id: &str, module_type: &ModuleType) -> bool {
     if self.include.is_empty() && self.exclude.is_empty() {
       return matches!(module_type, ModuleType::Jsx | ModuleType::Tsx | ModuleType::Ts);
     }
+
     let normalized_path = Path::new(id).relative(ctx.inner.cwd());
     let normalized_id = normalized_path.to_string_lossy();
     let cleaned_id = clean_url(&normalized_id);
-    if cleaned_id == normalized_id {
-      pattern_filter::filter(Some(&self.exclude), Some(&self.include), id, &normalized_id).inner()
-    } else {
-      pattern_filter::filter(Some(&self.exclude), Some(&self.include), id, &normalized_id).inner()
-        && pattern_filter::filter(Some(&self.exclude), Some(&self.include), id, cleaned_id).inner()
-    }
-  }
 
-  pub fn from_targets(targets: Option<String>) -> Self {
-    Self { target: targets, ..Default::default() }
+    let result =
+      pattern_filter(Some(&self.exclude), Some(&self.include), id, &normalized_id).inner();
+    if cleaned_id == normalized_id {
+      result
+    } else {
+      result || pattern_filter(Some(&self.exclude), Some(&self.include), id, cleaned_id).inner()
+    }
   }
 }

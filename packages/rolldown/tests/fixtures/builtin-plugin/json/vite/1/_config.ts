29afe0e62ef7b94468a42b1c4ae853402b2bfeee
dalaoshu <laipichan@qq.com>
2025-03-25 23:59:16 +0800

    test(json): port relevant cases from `Vite` (#3959)
    ### Description
    
    Related to #3892
    
    There are many related tests, and I will try to remove unnecessary ones
    later.

---
+++ packages/rolldown/tests/fixtures/builtin-plugin/json/vite/1/_config.ts
@@ -0,0 +1,23 @@
+import path from 'node:path'
+import { expect } from 'vitest'
+import { defineTest } from 'rolldown-tests'
+import { jsonPlugin } from 'rolldown/experimental'
+
+export default defineTest({
+  config: {
+    input: 'main.js',
+    plugins: [
+      jsonPlugin({ namedExports: true, stringify: 'auto' }),
+      {
+        name: 'test-plugin',
+        async transform(code, id) {
+          if (id.endsWith('.json')) {
+            await expect(code).toMatchFileSnapshot(
+              path.resolve(import.meta.dirname, `${path.basename(id)}.snap`),
+            )
+          }
+        },
+      },
+    ],
+  },
+})
---
+++ packages/rolldown/tests/fixtures/builtin-plugin/json/vite/4/_config.ts
@@ -0,0 +1,23 @@
+import path from 'node:path'
+import { expect } from 'vitest'
+import { defineTest } from 'rolldown-tests'
+import { jsonPlugin } from 'rolldown/experimental'
+
+export default defineTest({
+  config: {
+    input: 'main.js',
+    plugins: [
+      jsonPlugin({ namedExports: false, stringify: false }),
+      {
+        name: 'test-plugin',
+        async transform(code, id) {
+          if (id.endsWith('data.json')) {
+            await expect(code).toMatchFileSnapshot(
+              path.resolve(import.meta.dirname, 'data.json.snap'),
+            )
+          }
+        },
+      },
+    ],
+  },
+})

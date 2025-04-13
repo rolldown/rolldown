4f2731eae4ff98361a086a1d2d7a0e87f187211e
shulaoda <laipichan@qq.com>
2025-04-13 12:19:43 +0800

    refactor(rolldown_plugin_json): improve lib.rs logic

--- crates/rolldown_plugin_json/src/lib.rs
+++ crates/rolldown_plugin_json/src/lib.rs
@@ -11,17 +11,17 @@ use serde_json::Value;
 
 #[derive(Debug, Default)]
 pub struct JsonPlugin {
-  pub stringify: JsonPluginStringify,
   pub is_build: bool,
   pub named_exports: bool,
+  pub stringify: JsonPluginStringify,
 }
 
-#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum JsonPluginStringify {
-  False,
-  True,
   #[default]
   Auto,
+  True,
+  False,
 }
 
 impl Plugin for JsonPlugin {
@@ -34,17 +34,17 @@ impl Plugin for JsonPlugin {
     _ctx: rolldown_plugin::SharedTransformPluginContext,
     args: &rolldown_plugin::HookTransformArgs<'_>,
   ) -> rolldown_plugin::HookTransformReturn {
-    // Not sure we should use `module type to filter, but for now prefer to follow vite behavior`
+    // Not sure we should use `module_type` to filter, but for now prefer to follow vite behavior
     if !utils::is_json_ext(args.id) || utils::is_special_query(args.id) {
       return Ok(None);
     }
+
     let code = utils::strip_bom(args.code);
 
     if self.stringify != JsonPluginStringify::False {
       if self.named_exports && code.trim_start().starts_with('{') {
         let parsed = serde_json::from_str::<Value>(code)?;
-        let parsed =
-          parsed.as_object().expect("should be object because the value starts with `{`");
+        let parsed = parsed.as_object().expect("should be object because it starts with `{`");
 
         let mut code = String::new();
         let mut default_object_code = "{\n".to_owned();
@@ -60,7 +60,6 @@ impl Plugin for JsonPlugin {
         default_object_code += "}";
 
         writeln!(code, "export default {default_object_code};").unwrap();
-
         return Ok(Some(HookTransformOutput {
           code: Some(code),
           map: Some(SourceMap::default()),
@@ -70,18 +69,18 @@ impl Plugin for JsonPlugin {
       }
 
       if self.stringify == JsonPluginStringify::True || code.len() > utils::THRESHOLD_SIZE {
-        let normalized_code = if self.is_build {
-          // TODO: perf: find better way than https://github.com/rolldown/vite/blob/3bf86e3f715c952a032b476b60c8c869e9c50f3f/packages/vite/src/node/plugins/json.ts#L55-L57
+        let json = if self.is_build {
+          // TODO(perf): find better way than https://github.com/rolldown/vite/blob/3bf86e3f/packages/vite/src/node/plugins/json.ts#L55-L57
           let value = serde_json::from_str::<Value>(code)?;
           Cow::Owned(serde_json::to_string(&value)?)
         } else {
           Cow::Borrowed(code)
         };
-        let normalized_code_string = serde_json::to_string(&normalized_code)?;
+
         return Ok(Some(HookTransformOutput {
           code: Some(concat_string!(
             "export default /*#__PURE__*/ JSON.parse(",
-            normalized_code_string,
+            serde_json::to_string(&json)?,
             ")"
           )),
           map: Some(SourceMap::default()),
@@ -91,10 +90,10 @@ impl Plugin for JsonPlugin {
       }
     }
 
-    let value = serde_json::from_str::<Value>(code)?;
-    let output = utils::json_to_esm(&value, self.named_exports);
+    let value = serde_json::from_str(code)?;
+    let code = utils::json_to_esm(&value, self.named_exports);
     Ok(Some(HookTransformOutput {
-      code: Some(output),
+      code: Some(code),
       map: Some(SourceMap::default()),
       module_type: Some(ModuleType::Js),
       ..Default::default()

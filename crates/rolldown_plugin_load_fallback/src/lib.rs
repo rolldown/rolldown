26c0d0c982b7b03575f9f69836e0dfc96088c43f
dalaoshu <laipichan@qq.com>
2025-04-14 18:32:44 +0800

    refactor(rust): improve `rolldown_plugin_load_fallback` (#4085)
    ### Description
    
    Related to #3968

--- crates/rolldown_plugin_load_fallback/src/lib.rs
+++ crates/rolldown_plugin_load_fallback/src/lib.rs
@@ -1,23 +1,29 @@
 use std::borrow::Cow;
 
 use rolldown_plugin::{HookLoadArgs, HookLoadOutput, HookLoadReturn, Plugin, PluginContext};
-use rolldown_utils::{clean_url::clean_url, dataurl::parse_data_url};
 
 #[derive(Debug)]
-pub struct LoadFallbackPlugin {}
+pub struct LoadFallbackPlugin;
 
 impl Plugin for LoadFallbackPlugin {
   fn name(&self) -> Cow<'static, str> {
     Cow::Borrowed("builtin:load-fallback")
   }
 
-  async fn load(&self, _ctx: &PluginContext, args: &HookLoadArgs<'_>) -> HookLoadReturn {
-    if parse_data_url(args.id).is_some() {
+  async fn load(&self, ctx: &PluginContext, args: &HookLoadArgs<'_>) -> HookLoadReturn {
+    if args.id.trim_start().starts_with("data:") {
       return Ok(None);
     }
-    let normalized_id = clean_url(args.id);
-    let code =
-      std::fs::read_to_string(normalized_id).or_else(|_| std::fs::read_to_string(args.id))?;
+
+    let Some(index) = memchr::memchr2(b'?', b'#', args.id.as_bytes()) else {
+      return Ok(None);
+    };
+
+    let path = &args.id[..index];
+    let Ok(code) = std::fs::read_to_string(path) else { return Ok(None) };
+
+    ctx.add_watch_file(path);
+
     Ok(Some(HookLoadOutput { code, ..Default::default() }))
   }
 }

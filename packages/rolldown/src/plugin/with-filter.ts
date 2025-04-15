698bd4c3c355388b3ca1821ccc262ac121327d36
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-04-15 14:48:16 +0800

    fix: support nested plugin for withFilter (#4106)
    <!-- Thank you for contributing! -->
    
    ### Description
    1. follow up of https://github.com/rolldown/rolldown/pull/4087
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/rolldown/src/plugin/with-filter.ts
+++ packages/rolldown/src/plugin/with-filter.ts
@@ -1,17 +1,38 @@
-import { HookFilterExtension, Plugin } from '..';
+import { isPromise } from 'node:util/types';
+import { HookFilterExtension, Plugin, RolldownPluginOption } from '..';
+import { StringOrRegExp } from '../types/utils';
+import { arraify } from '../utils/misc';
 
 type OverrideFilterObject = {
   transform?: HookFilterExtension<'transform'>['filter'];
   resolveId?: HookFilterExtension<'resolveId'>['filter'];
   load?: HookFilterExtension<'load'>['filter'];
+  pluginNamePattern?: StringOrRegExp[];
 };
-// TODO: maybe we should also change the name as well ?
-// my proposal is `${import.meta.filename}@${name}`
-export function withFilter<A, T extends Plugin<A>>(
-  plugin: T,
-  filter_obj: OverrideFilterObject,
+
+function withFilterImpl<A, T extends RolldownPluginOption<A>>(
+  pluginOption: T,
+  filterObjectList: OverrideFilterObject[],
 ): T {
-  plugin['transform'];
+  if (isPromise(pluginOption)) {
+    return pluginOption.then((p) => withFilter(p, filterObjectList)) as T;
+  }
+  if (pluginOption == false || pluginOption == null) {
+    return pluginOption;
+  }
+  if (Array.isArray(pluginOption)) {
+    return pluginOption.map((p) => withFilter(p, filterObjectList)) as T;
+  }
+  // TODO: check builtin plugin and parallel plugin
+  let plugin = pluginOption as Plugin<A>;
+  let filterObjectIndex = findMatchedFilterObject(
+    plugin.name,
+    filterObjectList,
+  );
+  if (filterObjectIndex === -1) {
+    return plugin as T;
+  }
+  let filterObject = filterObjectList[filterObjectIndex];
   Object.keys(plugin).forEach((key) => {
     switch (key) {
       case 'transform':
@@ -21,7 +42,7 @@ export function withFilter<A, T extends Plugin<A>>(
           return;
         }
         if (typeof plugin[key] === 'object') {
-          plugin[key].filter = filter_obj[key] ?? plugin[key].filter;
+          plugin[key].filter = filterObject[key] ?? plugin[key].filter;
         } else {
           // We could either remove the `@ts-expect-error` and duplicate case `transform`, `resolveId` and `load`
           // or use `@ts-expect-error` to just ignore the type error
@@ -30,7 +51,7 @@ export function withFilter<A, T extends Plugin<A>>(
             // @ts-expect-error
             handler: plugin[key],
             // @ts-expect-error
-            filter: filter_obj[key],
+            filter: filterObject[key],
           };
         }
         break;
@@ -38,5 +59,40 @@ export function withFilter<A, T extends Plugin<A>>(
         break;
     }
   });
-  return plugin;
+  return plugin as T;
+}
+
+export function withFilter<A, T extends RolldownPluginOption<A>>(
+  pluginOption: T,
+  filterObject: OverrideFilterObject | OverrideFilterObject[],
+): T {
+  return withFilterImpl(pluginOption, arraify(filterObject));
+}
+
+function findMatchedFilterObject(
+  pluginName: string,
+  overrideFilterObjectList: OverrideFilterObject[],
+): number {
+  if (
+    overrideFilterObjectList.length === 1 &&
+    overrideFilterObjectList[0].pluginNamePattern === undefined
+  ) {
+    return 0;
+  }
+
+  for (let i = 0; i < overrideFilterObjectList.length; i++) {
+    for (
+      let j = 0;
+      j < (overrideFilterObjectList[i].pluginNamePattern ?? []).length;
+      j++
+    ) {
+      let pattern = overrideFilterObjectList[i].pluginNamePattern![j];
+      if (typeof pattern === 'string' && pattern === pluginName) {
+        return i;
+      } else if (pattern instanceof RegExp && pattern.test(pluginName)) {
+        return i;
+      }
+    }
+  }
+  return -1;
 }
---
+++ packages/rolldown/tests/fixtures/plugin/transform/with-filter-nested/_config.ts
@@ -0,0 +1,54 @@
+import { RolldownPluginOption, withFilter } from "rolldown";
+import { defineTest } from "rolldown-tests";
+import { expect, vi } from "vitest";
+
+const transformFn = vi.fn();
+const transformFn1 = vi.fn();
+const transformFn2 = vi.fn();
+
+const nestedPlugin: RolldownPluginOption = [
+	{
+		name: "test-plugin-1",
+		transform: {
+			handler(_, _id) {
+				transformFn();
+			},
+		},
+	},
+	[
+		{
+			name: "test-plugin-1-1",
+			transform: {
+				handler(_, _id) {
+					transformFn1();
+				},
+			},
+		},
+		{
+			name: "test-plugin-1-2",
+			transform: {
+				handler(_, _id) {
+					transformFn2();
+				},
+			},
+		},
+	],
+];
+
+export default defineTest({
+	skipComposingJsPlugin: true,
+	config: {
+		// Without this override, the transform function will be called 9 times
+		plugins: [
+			withFilter(nestedPlugin, {
+				transform: { id: /\.vue$/ },
+				pluginNamePattern: [/test-plugin-1-.*/],
+			}),
+		],
+	},
+	afterTest: () => {
+		expect(transformFn).toHaveBeenCalledTimes(3);
+		expect(transformFn1).toHaveBeenCalledTimes(0);
+		expect(transformFn2).toHaveBeenCalledTimes(0);
+	},
+});

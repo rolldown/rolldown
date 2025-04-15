252adebe4cfa72abc629d85f61cff7cf05e8718e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-15 15:48:20 +0800

    refactor

--- packages/rolldown/build.ts
+++ packages/rolldown/build.ts
@@ -2,213 +2,155 @@ import colors from 'ansis';
 import { globSync } from 'glob';
 import fs from 'node:fs';
 import nodePath from 'node:path';
-import pkgJson from './package.json' with { type: 'json' };
-import { build, BuildOptions, defineConfig, type Plugin } from './src/index';
+// import pkgJson from './package.json' with { type: 'json' };
+import { build, BuildOptions, type Plugin } from './src/index';
 
 const isCI = !!process.env.CI;
 const isReleasingCI = !!process.env.RELEASING;
-const isBrowserBuild = !!process.env.BROWSER_PKG;
 
-const outputDir = isBrowserBuild
-  ? nodePath.resolve(__dirname, '../browser/dist')
-  : nodePath.resolve(__dirname, 'dist');
+// In `@rolldown/browser`, there will be three builds:
+// - CJS and ESM for Node (used in StackBlitz / WebContainers)
+// - ESM for bundlers (used in Vite and running in the browser)
+// - ESM with inlined dependencies for CDN imports
+const isBrowserPkg = !!process.env.BROWSER_PKG;
+
+const pkgRoot = isBrowserPkg
+  ? nodePath.resolve(__dirname, '../browser')
+  : __dirname;
+const outputDir = nodePath.resolve(pkgRoot, 'dist');
+const pkgJson = JSON.parse(
+  fs.readFileSync(nodePath.resolve(pkgRoot, 'package.json'), 'utf-8'),
+);
 
 const bindingFile = nodePath.resolve('src/binding.js');
+const bindingFileWasi = nodePath.resolve('src/rolldown-binding.wasi.cjs');
 const bindingFileWasiBrowser = nodePath.resolve(
   'src/rolldown-binding.wasi-browser.js',
 );
 
-const shared = defineConfig({
-  input: {
-    index: './src/index',
-    ...isBrowserBuild ? {} : {
-      cli: './src/cli/index',
-      'parallel-plugin': './src/parallel-plugin',
-      'parallel-plugin-worker': './src/parallel-plugin-worker',
-      'experimental-index': './src/experimental-index',
-      'parse-ast-index': './src/parse-ast-index',
-    },
-  },
-  platform: isBrowserBuild ? 'browser' : 'node',
-  resolve: {
-    extensions: ['.js', '.cjs', '.mjs', '.ts'],
-    alias: {
-      'node:path': 'pathe',
-      ...(isBrowserBuild ? { [bindingFile]: bindingFileWasiBrowser } : {}),
-    },
-  },
-  external: [
-    /rolldown-binding\..*\.node/,
-    /rolldown-binding\..*\.wasm/,
-    /@rolldown\/binding-.*/,
-    /\.\/rolldown-binding\.wasi\.cjs/,
-    // some dependencies, e.g. zod, cannot be inlined because their types
-    // are used in public APIs
-    ...(isBrowserBuild
-      ? []
-      : Object.keys(pkgJson.dependencies)),
-    bindingFileWasiBrowser,
-  ],
-  define: {
-    'import.meta.browserBuild': String(isBrowserBuild),
-  },
-  plugins: [
-    isBrowserBuild && {
-      name: 'remove-built-modules',
-      resolveId(id) {
-        if (id === 'node:os' || id === 'node:worker_threads') {
-          return { id, external: true, moduleSideEffects: false };
+const withShared = (
+  { browserBuild: isBrowserBuild, inlineDependency, ...options }: {
+    browserBuild?: boolean;
+    inlineDependency?: boolean;
+  } & BuildOptions,
+): BuildOptions => {
+  const alias = {};
+  if (isBrowserPkg) {
+    alias[bindingFile] = isBrowserBuild
+      ? bindingFileWasiBrowser
+      : bindingFileWasi;
+  }
+  if (isBrowserBuild) {
+    alias['node:path'] = 'pathe';
+  }
+
+  return {
+    input: {
+      index: './src/index',
+      ...!isBrowserBuild
+        ? {
+          cli: './src/cli/index',
+          'parallel-plugin': './src/parallel-plugin',
+          'parallel-plugin-worker': './src/parallel-plugin-worker',
+          'experimental-index': './src/experimental-index',
+          'parse-ast-index': './src/parse-ast-index',
         }
-      },
+        : {},
     },
-  ],
-});
-
-const esmSuffix = isBrowserBuild ? 'js' : 'mjs';
-
-const configs = defineConfig([
-  {
-    ...shared,
-    output: {
-      dir: outputDir,
-      format: 'esm',
-      entryFileNames: `[name].${esmSuffix}`,
-      chunkFileNames: `shared/[name]-[hash].${esmSuffix}`,
+    platform: isBrowserBuild ? 'browser' : 'node',
+    resolve: {
+      extensions: ['.js', '.cjs', '.mjs', '.ts'],
+      alias,
     },
+    external: [
+      /rolldown-binding\..*\.node/,
+      /rolldown-binding\..*\.wasm/,
+      /@rolldown\/binding-.*/,
+      /\.\/rolldown-binding\.wasi\.cjs/,
+      // some dependencies, e.g. zod, cannot be inlined because their types
+      // are used in public APIs
+      ...(inlineDependency ? [] : Object.keys(pkgJson.dependencies)),
+      bindingFileWasi,
+      bindingFileWasiBrowser,
+    ],
+    define: {
+      'import.meta.browserBuild': String(isBrowserBuild),
+    },
+    ...options,
     plugins: [
-      shared.plugins,
-      {
-        name: 'shim',
-        buildEnd() {
-          // wasm build rely on `.node` binaries. But we don't want to copy `.node` files
-          // to the dist folder, so we need to distinguish between `.wasm` and `.node` files.
-          const wasmFiles = globSync(['./src/rolldown-binding.*.wasm'], {
-            absolute: true,
-          });
-          const isWasmBuild = wasmFiles.length;
-
-          const nodeFiles = globSync(['./src/rolldown-binding.*.node'], {
-            absolute: true,
-          });
-
-          // Binary build is on the separate step on CI
-          if (!isCI && !wasmFiles.length && !nodeFiles.length) {
-            throw new Error('No binary files found');
+      isBrowserPkg && {
+        name: 'remove-built-modules',
+        resolveId(id) {
+          if (id === 'node:os' || id === 'node:worker_threads') {
+            return { id, external: true, moduleSideEffects: false };
           }
-
-          const copyTo = nodePath.resolve(outputDir);
-          fs.mkdirSync(copyTo, { recursive: true });
-
-          if (!isReleasingCI) {
-            // Released `rolldown` package import binary via `@rolldown/binding-<platform>` packages.
-            // There's no need to copy binary files to dist folder.
-
-            if (isWasmBuild) {
-              // Move the binary file to dist
-              wasmFiles.forEach((file) => {
-                const fileName = nodePath.basename(file);
-                if (isBrowserBuild && fileName.includes('debug')) {
-                  // NAPI-RS now generates a debug wasm binary no matter how and we don't want to ship it to npm.
-                  console.log(colors.yellow('[build:done]'), 'Skipping', file);
-                } else {
-                  console.log(
-                    colors.green('[build:done]'),
-                    'Copying',
-                    file,
-                    `to ${copyTo}`,
-                  );
-                  fs.cpSync(file, nodePath.join(copyTo, fileName));
-                }
-              });
-            } else {
-              // Move the binary file to dist
-              nodeFiles.forEach((file) => {
-                const fileName = nodePath.basename(file);
-                console.log(
-                  colors.green('[build:done]'),
-                  'Copying',
-                  file,
-                  `to ${copyTo}`,
-                );
-                fs.cpSync(file, nodePath.join(copyTo, fileName));
-              });
-            }
-
-            if (isBrowserBuild) {
-              const browserShims = globSync(
-                './src/*-browser.*js',
-                { absolute: true },
-              );
-              browserShims.forEach((file) => {
-                const fileName = nodePath.basename(file);
-                console.log(
-                  colors.green('[build:done]'),
-                  'Copying',
-                  file,
-                  `to ${copyTo}`,
-                );
-                fs.cpSync(file, nodePath.join(copyTo, fileName));
-              });
-            }
-          }
-
-          // Copy binding types and rollup types to dist
-          const distTypesDir = nodePath.resolve(outputDir, 'types');
-          fs.mkdirSync(distTypesDir, { recursive: true });
-          const types = globSync(['./src/*.d.ts'], {
-            absolute: true,
-          });
-          types.forEach((file) => {
-            const fileName = nodePath.basename(file);
-            console.log(
-              colors.green('[build:done]'),
-              'Copying',
-              file,
-              `to ${distTypesDir}`,
-            );
-            fs.cpSync(file, nodePath.join(distTypesDir, fileName), {
-              recursive: true,
-              force: true,
-            });
-          });
         },
       },
       patchBindingJs(),
+      options.plugins,
     ],
-  },
-]);
+  };
+};
 
-if (!isBrowserBuild) {
+const configs: BuildOptions[] = [
+  withShared({
+    plugins: [patchBindingJs()],
+    output: {
+      dir: outputDir,
+      format: 'esm',
+      entryFileNames: `[name].mjs`,
+      chunkFileNames: `shared/[name]-[hash].mjs`,
+    },
+  }),
+  withShared({
+    plugins: [shimImportMeta(), patchBindingJs()],
+    output: {
+      dir: outputDir,
+      format: 'cjs',
+      entryFileNames: '[name].cjs',
+      chunkFileNames: 'shared/[name]-[hash].cjs',
+    },
+  }),
+];
+
+if (isBrowserPkg) {
   configs.push(
-    {
-      ...shared,
-      plugins: [
-        shared.plugins,
-        {
-          name: 'shim-import-meta',
-          transform: {
-            filter: {
-              code: {
-                include: ['import.meta.resolve'],
-              },
-            },
-            handler(code, id) {
-              if (id.endsWith('.ts') && code.includes('import.meta.resolve')) {
-                return code.replace('import.meta.resolve', 'undefined');
-              }
-            },
-          },
-        },
-        patchBindingJs(),
-      ],
+    withShared({
+      browserBuild: true,
+      plugins: [],
       output: {
-        dir: outputDir,
-        format: 'cjs',
-        entryFileNames: '[name].cjs',
-        chunkFileNames: 'shared/[name]-[hash].cjs',
+        format: 'esm',
+        file: nodePath.resolve(outputDir, 'browser-bundler.js'),
+      },
+    }),
+    withShared({
+      browserBuild: true,
+      inlineDependency: true,
+      plugins: [],
+      output: {
+        format: 'esm',
+        file: nodePath.resolve(outputDir, 'browser.js'),
+      },
+    }),
+  );
+}
+
+function shimImportMeta(): Plugin {
+  return {
+    name: 'shim-import-meta',
+    transform: {
+      filter: {
+        code: {
+          include: ['import.meta.resolve'],
+        },
+      },
+      handler(code, id) {
+        if (id.endsWith('.ts') && code.includes('import.meta.resolve')) {
+          return code.replace('import.meta.resolve', 'undefined');
+        }
       },
     },
-  );
+  };
 }
 
 function patchBindingJs(): Plugin {
@@ -245,6 +187,102 @@ if (!nativeBinding && globalThis.process?.versions?.["webcontainer"]) {
 
 (async () => {
   for (const config of configs) {
-    await build(config as BuildOptions);
+    await build(config);
   }
+  copy();
 })();
+
+function copy() {
+  // wasm build rely on `.node` binaries. But we don't want to copy `.node` files
+  // to the dist folder, so we need to distinguish between `.wasm` and `.node` files.
+  const wasmFiles = globSync('./src/rolldown-binding.*.wasm', {
+    absolute: true,
+  });
+  const nodeFiles = globSync('./src/rolldown-binding.*.node', {
+    absolute: true,
+  });
+
+  // Binary build is on the separate step on CI
+  if (!isCI) {
+    if (isBrowserPkg && !wasmFiles.length) {
+      throw new Error('No WASM files found.');
+    }
+    if (!isBrowserPkg && !nodeFiles.length) {
+      throw new Error('No Node files found.');
+    }
+  }
+
+  const copyTo = nodePath.resolve(outputDir);
+  fs.mkdirSync(copyTo, { recursive: true });
+
+  if (!isReleasingCI) {
+    // Released `rolldown` package import binary via `@rolldown/binding-<platform>` packages.
+    // There's no need to copy binary files to dist folder.
+
+    if (isBrowserPkg) {
+      // Move the wasm file to dist
+      wasmFiles.forEach((file) => {
+        const fileName = nodePath.basename(file);
+        if (isBrowserPkg && fileName.includes('debug')) {
+          // NAPI-RS now generates a debug wasm binary no matter how and we don't want to ship it to npm.
+          console.log(colors.yellow('[build:done]'), 'Skipping', file);
+        } else {
+          console.log(
+            colors.green('[build:done]'),
+            'Copying',
+            file,
+            `to ${copyTo}`,
+          );
+          fs.cpSync(file, nodePath.join(copyTo, fileName));
+        }
+      });
+
+      const browserShims = globSync(
+        './src/*wasi*js',
+        { absolute: true },
+      );
+      browserShims.forEach((file) => {
+        const fileName = nodePath.basename(file);
+        console.log(
+          colors.green('[build:done]'),
+          'Copying',
+          file,
+          `to ${copyTo}`,
+        );
+        fs.cpSync(file, nodePath.join(copyTo, fileName));
+      });
+    } else {
+      // Move the binary file to dist
+      nodeFiles.forEach((file) => {
+        const fileName = nodePath.basename(file);
+        console.log(
+          colors.green('[build:done]'),
+          'Copying',
+          file,
+          `to ${copyTo}`,
+        );
+        fs.cpSync(file, nodePath.join(copyTo, fileName));
+      });
+    }
+  }
+
+  // Copy binding types and rollup types to dist
+  const distTypesDir = nodePath.resolve(outputDir, 'types');
+  fs.mkdirSync(distTypesDir, { recursive: true });
+  const types = globSync(['./src/*.d.ts'], {
+    absolute: true,
+  });
+  types.forEach((file) => {
+    const fileName = nodePath.basename(file);
+    console.log(
+      colors.green('[build:done]'),
+      'Copying',
+      file,
+      `to ${distTypesDir}`,
+    );
+    fs.cpSync(file, nodePath.join(distTypesDir, fileName), {
+      recursive: true,
+      force: true,
+    });
+  });
+}

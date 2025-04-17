5a5d56b5d09b51e5ef38cbcecf36c482f42b7f69
dalaoshu <laipichan@qq.com>
2025-04-17 18:41:24 +0800

    refactor(rolldown_plugin_transform): clear the original misaligned logic (#4152)
    ### Description
    
    Related to #3968

--- crates/rolldown_plugin_transform/src/lib.rs
+++ crates/rolldown_plugin_transform/src/lib.rs
@@ -3,15 +3,9 @@ mod utils;
 use std::borrow::Cow;
 use std::path::Path;
 
-use oxc::{
-  codegen::{CodeGenerator, CodegenOptions, CodegenReturn},
-  semantic::SemanticBuilder,
-  span::SourceType,
-  transformer::{EnvOptions, TransformOptions, Transformer},
-};
+use oxc::{span::SourceType, transformer::TransformOptions};
 
 use rolldown_common::ModuleType;
-use rolldown_ecmascript::EcmaCompiler;
 use rolldown_plugin::{Plugin, SharedTransformPluginContext};
 use rolldown_utils::{clean_url::clean_url, pattern_filter::StringOrRegex};
 
@@ -47,60 +41,6 @@ impl Plugin for TransformPlugin {
       return Ok(None);
     }
 
-    let source_type = {
-      let default_source_type = SourceType::default();
-      match args.module_type {
-        ModuleType::Jsx => default_source_type.with_jsx(true),
-        ModuleType::Ts => default_source_type.with_typescript(true),
-        ModuleType::Tsx => default_source_type.with_typescript(true).with_jsx(true),
-        _ => return Ok(None),
-      }
-    };
-
-    let parse_result = EcmaCompiler::parse(args.id, args.code, source_type);
-    let mut ecma_ast = parse_result.map_err(|errs| {
-      // TODO: Improve diagnostics handling
-      anyhow::anyhow!("Error occurred when parsing {}\n: {:?}", args.id, errs)
-    })?;
-
-    let env = EnvOptions::default();
-    let ret = ecma_ast.program.with_mut(move |fields| {
-      let mut transformer_options = TransformOptions { env, ..TransformOptions::default() };
-
-      if !matches!(args.module_type, ModuleType::Ts) {
-        transformer_options.jsx.jsx_plugin = true;
-      }
-
-      let scoping = SemanticBuilder::new().build(fields.program).semantic.into_scoping();
-      Transformer::new(fields.allocator, Path::new(args.id), &transformer_options)
-        .build_with_scoping(scoping, fields.program)
-    });
-
-    if !ret.errors.is_empty() {
-      // TODO: better error handling
-      Err(anyhow::anyhow!("Transform failed, got {:#?}", ret.errors))?;
-    }
-
-    let CodegenReturn { mut code, map, .. } = CodeGenerator::new()
-      .with_options(CodegenOptions {
-        source_map_path: Some(args.id.into()),
-        ..CodegenOptions::default()
-      })
-      .build(ecma_ast.program());
-
-    if let Some(inject) = &self.jsx_inject {
-      let mut new_code = String::with_capacity(inject.len() + 1 + code.len());
-      new_code.push_str(inject);
-      new_code.push(';');
-      new_code.push_str(&code);
-      code = new_code;
-    }
-
-    Ok(Some(rolldown_plugin::HookTransformOutput {
-      map,
-      code: Some(code),
-      module_type: Some(ModuleType::Js),
-      ..Default::default()
-    }))
+    Ok(None)
   }
 }

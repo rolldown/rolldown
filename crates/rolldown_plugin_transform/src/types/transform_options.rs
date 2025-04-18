2e85a0a6aeef5d96c07dd3ccd5c37247fa7dab5d
dalaoshu <laipichan@qq.com>
2025-04-18 14:02:52 +0800

    refactor(rolldown_plugin_transform): unify custom and tsconfig decorator options (#4172)
    ### Description
    
    Related to #3968

--- crates/rolldown_plugin_transform/src/types/transform_options.rs
+++ crates/rolldown_plugin_transform/src/types/transform_options.rs
@@ -1,10 +1,12 @@
 use itertools::Either;
 
-use super::jsx_options::JsxOptions;
+use super::{decorator_options::DecoratorOptions, jsx_options::JsxOptions};
 
 #[derive(Debug, Default, Clone)]
 pub struct TransformOptions {
   pub lang: Option<String>,
 
   pub jsx: Option<Either<String, JsxOptions>>,
+
+  pub decorator: Option<DecoratorOptions>,
 }

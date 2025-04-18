c92b7b6b91caadd966c7b1be01f214e5f77f6f14
shulaoda <laipichan@qq.com>
2025-04-18 16:46:58 +0800

    refactor(rolldown_transform_plugin): implement basic logic

--- crates/rolldown_plugin_transform/src/lib.rs
+++ crates/rolldown_plugin_transform/src/lib.rs
@@ -4,6 +4,10 @@ mod utils;
 use std::path::Path;
 use std::{borrow::Cow, sync::Arc};
 
+use oxc::codegen::{CodeGenerator, CodegenOptions, CodegenReturn};
+use oxc::parser::Parser;
+use oxc::semantic::SemanticBuilder;
+use oxc::transformer::Transformer;
 use rolldown_common::ModuleType;
 use rolldown_plugin::{Plugin, PluginContextResolveOptions, SharedTransformPluginContext};
 use rolldown_utils::{clean_url::clean_url, pattern_filter::StringOrRegex};
@@ -22,6 +26,7 @@ pub struct TransformPlugin {
   pub filename: String,
   pub environment_consumer: String,
 
+  pub sourcemap: bool,
   pub transform_options: TransformOptions,
 }
 
@@ -72,9 +77,50 @@ impl Plugin for TransformPlugin {
       return Ok(None);
     }
 
-    let (_source_type, _transform_options) =
+    let (source_type, transform_options) =
       self.get_modified_transform_options(&ctx, args.id, &cwd, ext.as_deref());
 
-    Ok(None)
+    let allocator = oxc::allocator::Allocator::default();
+    let ret = Parser::new(&allocator, args.code, source_type).parse();
+
+    if ret.panicked || !ret.errors.is_empty() {
+      // TODO: Improve diagnostics handling
+      Err(anyhow::anyhow!("Error occurred when parsing {}\n: {:?}", args.id, ret.errors))?;
+    }
+
+    let mut program = ret.program;
+    let scoping = SemanticBuilder::new().build(&program).semantic.into_scoping();
+    let transformer = Transformer::new(&allocator, Path::new(args.id), &transform_options);
+
+    let ret = transformer.build_with_scoping(scoping, &mut program);
+
+    if !ret.errors.is_empty() {
+      // TODO: better error handling
+      Err(anyhow::anyhow!("Transform failed, got {:#?}", ret.errors))?;
+    }
+
+    let mut codegen_options = CodegenOptions::default();
+
+    if self.sourcemap {
+      codegen_options.source_map_path = Some(args.id.into());
+    }
+
+    let ret = CodeGenerator::new().with_options(codegen_options).build(&program);
+    let CodegenReturn { mut code, map, .. } = ret;
+
+    if let Some(inject) = &self.jsx_inject {
+      let mut new_code = String::with_capacity(inject.len() + 1 + code.len());
+      new_code.push_str(inject);
+      new_code.push(';');
+      new_code.push_str(&code);
+      code = new_code;
+    }
+
+    Ok(Some(rolldown_plugin::HookTransformOutput {
+      map,
+      code: Some(code),
+      module_type: Some(ModuleType::Js),
+      ..Default::default()
+    }))
   }
 }

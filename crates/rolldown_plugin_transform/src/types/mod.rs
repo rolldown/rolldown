932b46f731cb91af734a6b32d0c72f7bdd7672be
dalaoshu <laipichan@qq.com>
2025-04-20 12:28:41 +0800

    feat(rolldown_plugin_transform): support `assumptions` option in `transform_options` (#4209)
    ### Description
    
    Related to #3968

--- crates/rolldown_plugin_transform/src/types/mod.rs
+++ crates/rolldown_plugin_transform/src/types/mod.rs
@@ -1,9 +1,11 @@
+mod compiler_assumptions;
 mod decorator_options;
 mod jsx_options;
 mod transform_options;
 mod typescript_options;
 
 pub use {
+  compiler_assumptions::CompilerAssumptions,
   decorator_options::DecoratorOptions,
   jsx_options::{JsxOptions, ReactRefreshOptions},
   transform_options::TransformOptions,

277d18657da6e734757eef24f43ee2095fabcd42
Alexander Lichter <github@lichter.io>
2025-04-21 23:25:58 +0200

    chore: remove part of test including warning

--- packages/rolldown/tests/fixtures/tree-shake/minify/_config.ts
+++ packages/rolldown/tests/fixtures/tree-shake/minify/_config.ts
@@ -2,25 +2,13 @@ import type { OutputChunk as RolldownOutputChunk } from 'rolldown'
 import { defineTest } from 'rolldown-tests'
 import { expect, vi } from 'vitest'
 
-const onLogFn = vi.fn()
-
 export default defineTest({
   config: {
     output: {
       minify: true,
-    },
-    onLog(level, log) {
-      expect(level).toBe('warn')
-      expect(log.code).toBe('MINIFY_WARNING')
-      expect(log.message).toContain(
-        'Setting "minify: true" is not recommended for production use.',
-      )
-      onLogFn()
-    },
+    }
   },
   afterTest: (output) => {
-    expect(onLogFn).toHaveBeenCalledTimes(1)
-
     output.output
       .filter(({ type }) => type === 'chunk')
       .forEach((chunk) => {

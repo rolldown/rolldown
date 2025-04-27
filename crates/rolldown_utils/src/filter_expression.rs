e2762f22491163bc802f58913da042d06f32a4e8
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-04-27 14:49:06 +0800

    feat: parse filter expr

--- crates/rolldown_utils/src/filter_expression.rs
+++ crates/rolldown_utils/src/filter_expression.rs
@@ -71,12 +71,74 @@ pub fn filter_exprs_interpreter(
   false
 }
 
+pub enum Token {
+  Id(StringOrRegex),
+  Code(StringOrRegex),
+  ModuleType(String),
+  And,
+  Or,
+  Not,
+  Include,
+  Exclude,
+}
+
+pub fn parse(mut tokens: Vec<Token>) -> FilterKind {
+  fn rec(tokens: &mut Vec<Token>) -> Option<FilterExpr> {
+    let token = tokens.pop()?;
+    match token {
+      Token::Id(string_or_regex) => {
+        return Some(FilterExpr::Id(string_or_regex));
+      }
+      Token::Code(string_or_regex) => {
+        return Some(FilterExpr::Code(string_or_regex));
+      }
+      Token::ModuleType(string) => {
+        return Some(FilterExpr::ModuleType(string));
+      }
+      Token::And => {
+        let left = rec(tokens)?;
+        let right = rec(tokens)?;
+        return Some(FilterExpr::And(Box::new(left), Box::new(right)));
+      }
+      Token::Or => {
+        let left = rec(tokens)?;
+        let right = rec(tokens)?;
+        return Some(FilterExpr::Or(Box::new(left), Box::new(right)));
+      }
+      Token::Not => {
+        let inner = rec(tokens)?;
+        return Some(FilterExpr::Not(Box::new(inner)));
+      }
+      Token::Include => {
+        unreachable!("Include token should not be in the expression");
+      }
+      Token::Exclude => {
+        unreachable!("Exclude token should not be in the expression");
+      }
+    }
+  }
+  match tokens.pop() {
+    Some(Token::Include) => {
+      let inner = rec(&mut tokens).expect("Failed to parse expression");
+      return FilterKind::Include(inner);
+    }
+    Some(Token::Exclude) => {
+      let inner = rec(&mut tokens).expect("Failed to parse expression");
+      return FilterKind::Exclude(inner);
+    }
+
+    _ => unreachable!("Expression should start with Include or Exclude"),
+  }
+}
+
 #[cfg(test)]
 mod test {
   use crate::{
-    filter_expression::{FilterExpr, filter_expr_interpreter},
+    filter_expression::{FilterExpr, Token, filter_expr_interpreter},
     pattern_filter::StringOrRegex,
   };
+
+  use super::{filter_exprs_interpreter, parse};
 
   #[test]
   fn test_filter_expr_interpreter() {
@@ -109,6 +171,29 @@ mod test {
       Some("import('foo')"),
       None,
       "."
+    ));
+  }
+
+  #[test]
+  fn parse_test() {
+    // exclude(and(id(/node_modules/), not(code(/import\\s*/))))
+    let mut tokens = vec![
+      Token::Exclude,
+      Token::And,
+      Token::Id(StringOrRegex::Regex("node_modules".into())),
+      Token::Not,
+      Token::Code(StringOrRegex::Regex("import\\s*".into())),
+    ];
+    tokens.reverse();
+
+    let expr = parse(tokens);
+    // the expr return `true`, but since it is a `Exclude`, finally it should be `false`
+    assert!(!filter_exprs_interpreter(
+      &[expr],
+      Some("/node_modules/bar.js"),
+      Some("console.log('test')"),
+      None,
+      ".",
     ));
   }
 }

6a9e83b52d381f64c864bbda56167e2216ae068a
shulaoda <laipichan@qq.com>
2025-04-30 17:34:24 +0800

    refactor(rolldown_plugin): reorganize file structure

---
+++ crates/rolldown_plugin/src/plugin_context/mod.rs
@@ -0,0 +1,5 @@
+mod plugin_context;
+mod transform_plugin_context;
+
+pub use plugin_context::{PluginContext, PluginContextImpl};
+pub use transform_plugin_context::{SharedTransformPluginContext, TransformPluginContext};

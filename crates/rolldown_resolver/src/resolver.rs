d38139ca96a548a8631c8a6e12a44ea2026b5b57
Boshen <boshenc@gmail.com>
2025-05-09 17:21:32 +0800

    feat(rolldown): oxc v9.0.0
    I removed the `modules` option in a previous version, it's now restored.

--- crates/rolldown_resolver/src/resolver.rs
+++ crates/rolldown_resolver/src/resolver.rs
@@ -112,6 +112,7 @@ impl<F: FileSystem + Default> Resolver<F> {
       fully_specified: false,
       main_fields,
       main_files: raw_resolve.main_files.unwrap_or_else(|| vec!["index".to_string()]),
+      modules: vec!["node_modules".into()],
       resolve_to_context: false,
       prefer_relative: false,
       prefer_absolute: false,

75757fa25b795308ffb105287327be1ce97dfbd9
翠 <green@sapphi.red>
2025-05-09 21:37:02 +0900

    feat(pluginutils): add `makeIdFiltersToMatchWithQuery` function (#4469)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    This PR adds `makeIdFiltersToMatchWithQuery` function to
    `@rolldown/pluginutils`.
    
    This function is useful for codes like:
    ```js
    const cleanId = cleanUrl(id)
    if (filter(id) || filter(filepath)) {
      // process
    }
    ```
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- packages/pluginutils/src/simple-filters.ts
+++ packages/pluginutils/src/simple-filters.ts
@@ -26,3 +26,49 @@ const escapeRegexRE = /[-/\\^$*+?.()|[\]{}]/g;
 function escapeRegex(str: string): string {
   return str.replace(escapeRegexRE, '\\$&');
 }
+
+type WidenString<T> = T extends string ? string : T;
+
+/**
+ * Converts a id filter to match with an id with a query.
+ *
+ * @param input the id filters to convert.
+ */
+export function makeIdFiltersToMatchWithQuery<T extends string | RegExp>(
+  input: T,
+): WidenString<T>;
+export function makeIdFiltersToMatchWithQuery<T extends string | RegExp>(
+  input: readonly T[],
+): WidenString<T>[];
+export function makeIdFiltersToMatchWithQuery(
+  input: string | RegExp | readonly (string | RegExp)[],
+): string | RegExp | (string | RegExp)[];
+export function makeIdFiltersToMatchWithQuery(
+  input: string | RegExp | readonly (string | RegExp)[],
+): string | RegExp | (string | RegExp)[] {
+  if (!Array.isArray(input)) {
+    return makeIdFilterToMatchWithQuery(
+      // Array.isArray cannot narrow the type
+      // https://github.com/microsoft/TypeScript/issues/17002
+      input as Exclude<typeof input, readonly unknown[]>,
+    );
+  }
+  return input.map((i) => makeIdFilterToMatchWithQuery(i));
+}
+
+function makeIdFilterToMatchWithQuery(
+  input: string | RegExp,
+): string | RegExp {
+  if (typeof input === 'string') {
+    return `${input}{?*,}`;
+  }
+  return makeRegexIdFilterToMatchWithQuery(input);
+}
+
+function makeRegexIdFilterToMatchWithQuery(input: RegExp) {
+  return new RegExp(
+    // replace `$` with `(?:\?.*)?$` (ignore `\$`)
+    input.source.replace(/(?<!\\)\$/g, '(?:\\?.*)?$'),
+    input.flags,
+  );
+}

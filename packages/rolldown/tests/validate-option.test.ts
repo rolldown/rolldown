080ce4eedaf5803a4da09a84e1f9c18498c38df4
underfin <likui.underfin@gmail.com>
2025-05-22 18:11:52 +0800

    feat: enable loose mode validate option and give warning (#4638)
    <!-- Thank you for contributing! -->
    
    ### Description
    close https://github.com/rolldown/rolldown/issues/4606
    close https://github.com/rolldown/rolldown/issues/4608
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->

--- packages/rolldown/tests/validate-option.test.ts
+++ packages/rolldown/tests/validate-option.test.ts
@@ -1,60 +1,50 @@
-import { test, expect } from 'vitest'
+import { test, expect, vi } from 'vitest'
 import { rolldown } from 'rolldown'
 
 test('validate input option', async () => {
-  try {
-    await rolldown({
-      // @ts-ignore invalid value
-      input: 1,
-      cwd: import.meta.dirname,
-      // @ts-ignore invalid key
+  const consoleSpy = vi.spyOn(console, 'warn')
+  await rolldown({
+    // @ts-ignore invalid value
+    input: 1,
+    cwd: import.meta.dirname,
+    // @ts-ignore invalid key
+    foo: 'bar',
+    resolve: {
+      // @ts-ignore nested invalid key
       foo: 'bar',
-      resolve: {
-        // @ts-ignore nested invalid key
-        foo: 'bar',
-      },
-      watch: {
-        // @ts-ignore
-        chokidar: {},
-      },
-      experimental: {
-        hmr: {}
-      }
-    })
-    expect.unreachable()
-  } catch (error: any) {
-    expect(error.message).toMatchInlineSnapshot(`
-      "Failed validate input options.
-      - For the "input". Invalid type: Expected (string | Array | Object) but received 1. 
-      - For the "resolve.foo". Invalid key: Expected never but received "foo". 
-      - For the "watch.chokidar". The "watch.chokidar" option is deprecated, please use "watch.notify" instead of it. 
-      - For the "foo". Invalid key: Expected never but received "foo". "
-    `)
-  }
+    },
+    watch: {
+      // @ts-ignore
+      chokidar: {},
+    },
+    experimental: {
+      hmr: {}
+    }
+  })
+  expect(consoleSpy).toHaveBeenCalledWith(`Warning validate input options.
+- For the "input". Invalid type: Expected (string | Array | Object) but received 1. 
+- For the "resolve.foo". Invalid key: Expected never but received "foo". 
+- For the "watch.chokidar". The "watch.chokidar" option is deprecated, please use "watch.notify" instead of it. 
+- For the "foo". Invalid key: Expected never but received "foo". `)
 })
 
 test('validate output option', async () => {
-  try {
-    const bundle = await rolldown({
-      input: './build-api/main.js',
-      cwd: import.meta.dirname,
-    })
-    await bundle.write({
-      // @ts-ignore  invalid key
-      foo: 'bar',
-      hoistTransitiveImports: false
-    })
-    expect.unreachable()
-  } catch (error: any) {
-    expect(error.message).toMatchInlineSnapshot(`
-        "Failed validate output options.
-        - For the "foo". Invalid key: Expected never but received "foo". "
-      `)
-  }
+  const consoleSpy = vi.spyOn(console, 'warn')
+  const bundle = await rolldown({
+    input: './build-api/main.js',
+    cwd: import.meta.dirname,
+  })
+  await bundle.write({
+    // @ts-ignore  invalid key
+    foo: 'bar',
+    hoistTransitiveImports: false
+  })
+  expect(consoleSpy).toHaveBeenCalledWith(`Warning validate output options.
+- For the "foo". Invalid key: Expected never but received "foo". `)
 })
 
-test('give a error for hoistTransitiveImports: true', async () => {
-  try {
+test('give a warning for hoistTransitiveImports: true', async () => {
+  const consoleSpy = vi.spyOn(console, 'warn')
     const bundle = await rolldown({
       input: './build-api/main.js',
       cwd: import.meta.dirname,
@@ -63,11 +53,6 @@ test('give a error for hoistTransitiveImports: true', async () => {
       // @ts-ignore  invalid value
       hoistTransitiveImports: true
     })
-    expect.unreachable()
-  } catch (error: any) {
-    expect(error.message).toMatchInlineSnapshot(`
-        "Failed validate output options.
-        - For the "hoistTransitiveImports". The 'true' value is not supported. "
-      `)
-  }
+    expect(consoleSpy).toHaveBeenCalledWith(`Warning validate output options.
+- For the "hoistTransitiveImports". The 'true' value is not supported. `)
 })

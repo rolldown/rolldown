67791aee67e4fcde8cd232c184264b2d65e29b09
shulaoda <laipichan@qq.com>
2025-05-24 23:15:26 +0800

    feat(rolldown_plugin_utils): support to_output_file_path_in_js

--- crates/rolldown_plugin_utils/src/lib.rs
+++ crates/rolldown_plugin_utils/src/lib.rs
@@ -1,3 +1,7 @@
+mod join_url_segments;
+mod to_output_file_path_in_js;
 mod to_relative_runtime_path;
 
+pub use join_url_segments::join_url_segments;
+pub use to_output_file_path_in_js::to_output_file_path_in_js;
 pub use to_relative_runtime_path::create_to_import_meta_url_based_relative_runtime;

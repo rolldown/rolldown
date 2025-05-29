11d5a97a11e620043ab69f9a2f6ae003968bb121
Boshen <boshenc@gmail.com>
2025-05-29 22:23:03 +0800

    refactor(bench): improve benchmark
    * Removes remapping bench, seems like not a useful benchmark to be
      recorded on codspeed.
    * Removes threejs10x, as codspeed measures CPU instructions, adding
      things that are the same doesn't help much
    * Removes benchmarking minification, oxc should take care of it
    * Simplifies the benchmark setup code

--- crates/bench/benches/bundle.rs
+++ crates/bench/benches/bundle.rs
@@ -2,43 +2,31 @@ use bench::{DeriveOptions, derive_benchmark_items};
 use criterion::{Criterion, criterion_group, criterion_main};
 use rolldown_testing::utils::assert_bundled;
 
+use rolldown_common::BundlerOptions;
+use rolldown_testing::bundler_options_presets::{rome_ts, threejs};
+
+fn items() -> Vec<(&'static str, BundlerOptions)> {
+  let mut result = vec![("threejs", threejs()), ("rome_ts", rome_ts())];
+  #[cfg(not(feature = "codspeed"))]
+  {
+    result.push(("threejs10x", rolldown_testing::bundler_options_presets::threejs10x()));
+  }
+  result
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
   let mut group = c.benchmark_group("bundle");
 
-  let derive_options = DeriveOptions { sourcemap: true, minify: true };
+  let derive_options = DeriveOptions { sourcemap: true, minify: false };
 
-  let items = [
-    derive_benchmark_items(
-      &derive_options,
-      "threejs".to_string(),
-      rolldown_testing::bundler_options_presets::threejs,
-    ),
-    derive_benchmark_items(
-      &derive_options,
-      "threejs10x".to_string(),
-      rolldown_testing::bundler_options_presets::threejs10x,
-    ),
-    derive_benchmark_items(
-      &derive_options,
-      "rome-ts".to_string(),
-      rolldown_testing::bundler_options_presets::rome_ts,
-    ),
-    derive_benchmark_items(
-      &derive_options,
-      "multi-duplicated-top-level-symbol".to_string(),
-      rolldown_testing::bundler_options_presets::multi_duplicated_symbol,
-    ),
-  ]
-  .into_iter()
-  .flatten()
-  .collect::<Vec<_>>();
-
-  group.sample_size(20);
-  items.into_iter().for_each(|item| {
-    group.bench_function(format!("bundle@{}", item.name), move |b| {
-      b.iter(|| assert_bundled((item.options)()));
+  items()
+    .into_iter()
+    .flat_map(|(name, options)| derive_benchmark_items(&derive_options, name, options.clone()))
+    .for_each(|item| {
+      group.bench_function(format!("bundle@{}", item.name), move |b| {
+        b.iter(|| assert_bundled(item.options.clone()));
+      });
     });
-  });
 }
 
 criterion_group!(benches, criterion_benchmark);

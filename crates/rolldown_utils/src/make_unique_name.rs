eeaf37cd4f684dc3c2892cf8e0941178fe8a3aef
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-06-04 22:05:17 +0800

    fix: 4702

--- crates/rolldown_utils/src/make_unique_name.rs
+++ crates/rolldown_utils/src/make_unique_name.rs
@@ -6,11 +6,35 @@ use sugar_path::SugarPath;
 
 use crate::{concat_string, dashmap::FxDashMap};
 
+/// Copy from rust std
+fn split_file_at_dot(file: &OsStr) -> (&OsStr, Option<&OsStr>) {
+  let slice = file.as_encoded_bytes();
+  if slice == b".." {
+    return (file, None);
+  }
+
+  // The unsafety here stems from converting between &OsStr and &[u8]
+  // and back. This is safe to do because (1) we only look at ASCII
+  // contents of the encoding and (2) new &OsStr values are produced
+  // only from ASCII-bounded slices of existing &OsStr values.
+  let i = match slice[1..].iter().position(|b| *b == b'.') {
+    Some(i) => i + 1,
+    None => return (file, None),
+  };
+  let before = &slice[..i];
+  let after = &slice[i + 1..];
+  unsafe {
+    (OsStr::from_encoded_bytes_unchecked(before), Some(OsStr::from_encoded_bytes_unchecked(after)))
+  }
+}
+
 pub fn make_unique_name(name: &ArcStr, used_name_counts: &FxDashMap<ArcStr, u32>) -> ArcStr {
   let mut candidate = name.clone();
   let extension = name
     .as_path()
-    .extension()
+    .file_name()
+    .map(split_file_at_dot)
+    .and_then(|(_before, after)| after)
     .and_then(OsStr::to_str)
     .map(|e| concat_string!(".", e))
     .unwrap_or_default();
@@ -67,5 +91,16 @@ mod tests {
 
     let unique_name = make_unique_name(&ArcStr::from("foo.js"), &used_name_counts);
     assert_eq!(unique_name.as_str(), "foo3.js");
+  }
+
+  #[test]
+  fn double_dot_extension() {
+    let used_name_counts = FxDashMap::default();
+
+    let unique_name = make_unique_name(&ArcStr::from("foo.d.js"), &used_name_counts);
+    assert_eq!(unique_name.as_str(), "foo.d.js");
+
+    let unique_name = make_unique_name(&ArcStr::from("foo.d.js"), &used_name_counts);
+    assert_eq!(unique_name.as_str(), "foo2.d.js");
   }
 }

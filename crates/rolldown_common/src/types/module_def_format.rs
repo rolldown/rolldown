b85da01530092e5d0acd1e1766d6e4a76524b7fe
Yunfei He <i.heyunfei@gmail.com>
2025-06-04 21:47:15 +0800

    chore(rust): add comments for `ModuleDefFormat` methods (#4821)
    <!-- Thank you for contributing! -->
    
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- crates/rolldown_common/src/types/module_def_format.rs
+++ crates/rolldown_common/src/types/module_def_format.rs
@@ -32,10 +32,14 @@ impl ModuleDefFormat {
     }
   }
 
+  /// Compared to `EcmaView::exports_kind == ExportKind::Esm`, this method ensures that not only the
+  /// module is esm, but also satisfies the conditions how node.js determines if a module is esm.
+  /// We use this method to determine if we want to simulate node.js's ESM execution behavior.
   pub fn is_esm(&self) -> bool {
     matches!(self, Self::EsmMjs | Self::EsmPackageJson | Self::EsmMts)
   }
 
+  /// Same as `is_esm`, but for CommonJS modules.
   pub fn is_commonjs(&self) -> bool {
     matches!(self, Self::CJS | Self::CjsPackageJson | Self::Cts)
   }

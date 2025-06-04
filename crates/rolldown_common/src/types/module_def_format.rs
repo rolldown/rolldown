932cf2ef1010a45efa3715112d4226547296dfb5
Yunfei He <i.heyunfei@gmail.com>
2025-06-04 21:07:36 +0800

    Update crates/rolldown_common/src/types/module_def_format.rs
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- crates/rolldown_common/src/types/module_def_format.rs
+++ crates/rolldown_common/src/types/module_def_format.rs
@@ -33,7 +33,7 @@ impl ModuleDefFormat {
   }
 
   /// Compared to `EcmaView::exports_kind == ExportKind::Esm`, this method ensures that not only the
-  ///  module is esm, but also satisfies the conditions how node.js determines if a module is esm.
+  /// module is esm, but also satisfies the conditions how node.js determines if a module is esm.
   /// We use this method to determine if we want to stimulate node.js's ESM execution behavior.
   pub fn is_esm(&self) -> bool {
     matches!(self, Self::EsmMjs | Self::EsmPackageJson | Self::EsmMts)

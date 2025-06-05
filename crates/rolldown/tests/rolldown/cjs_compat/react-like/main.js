9383fe784b329d1803d814d0c25833b7787c41d2
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-06-05 17:02:28 +0800

    fix: cjs namespace property access should only apply to ns_alias prop `default`

--- crates/rolldown/tests/rolldown/cjs_compat/react-like/main.js
+++ crates/rolldown/tests/rolldown/cjs_compat/react-like/main.js
@@ -1,5 +1,7 @@
-import React from './commonjs.js'
-import assert from 'node:assert'
+import React from "./commonjs.js";
+import { version } from "./commonjs2.js";
 
+import assert from "node:assert";
 
-assert.equal(React.createReactElement(), 'div')
+assert.equal(React.createReactElement(), "div");
+assert.equal(version.toString(), '1');

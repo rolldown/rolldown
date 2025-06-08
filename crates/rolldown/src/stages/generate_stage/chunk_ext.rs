d8e9e75b566cbbcd0854b1889f0f651063629e65
Yunfei He <i.heyunfei@gmail.com>
2025-06-09 02:23:18 +0800

    feat(attachDebugInfo): improve format and distinguish dynamic entries (#4861)

--- crates/rolldown/src/stages/generate_stage/chunk_ext.rs
+++ crates/rolldown/src/stages/generate_stage/chunk_ext.rs
@@ -14,7 +14,7 @@ pub trait ChunkDebugExt {
 pub enum ChunkCreationReason<'a> {
   AdvancedChunkGroup(&'a str),
   PreserveModules { is_user_defined_entry: bool, module_stable_id: &'a str },
-  UserDefinedEntry { debug_id: &'a str, entry_point_name: Option<&'a ArcStr> },
+  Entry { is_user_defined_entry: bool, entry_module_id: &'a str, name: Option<&'a ArcStr> },
   CommonChunk { bits: &'a BitSet, link_output: &'a LinkStageOutput },
 }
 
@@ -30,15 +30,23 @@ impl ChunkDebugExt for Chunk {
 
     let reason = match reason {
       ChunkCreationReason::AdvancedChunkGroup(name) => {
-        format!("AdvancedChunks: GroupName({name})")
+        format!("AdvancedChunks: [Group-Name: {name}]")
       }
       ChunkCreationReason::PreserveModules { is_user_defined_entry, module_stable_id } => {
         format!(
-          "Enabling Preserve Module: User-defined({is_user_defined_entry}), module({module_stable_id})",
+          "Enabling Preserve Module: [User-defined: {is_user_defined_entry}] [Module-Id: {module_stable_id}]",
         )
       }
-      ChunkCreationReason::UserDefinedEntry { debug_id, entry_point_name } => {
-        format!("User-defined Entry: input({debug_id}), name({entry_point_name:?})",)
+      ChunkCreationReason::Entry {
+        is_user_defined_entry,
+        entry_module_id: debug_id,
+        name: entry_point_name,
+      } => {
+        if is_user_defined_entry {
+          format!("User-defined Entry: [Entry-Module-Id: {debug_id}] [Name: {entry_point_name:?}]",)
+        } else {
+          format!("Dynamic Entry: [Entry-Module-Id: {debug_id}] [Name: {entry_point_name:?}]",)
+        }
       }
       ChunkCreationReason::CommonChunk { bits, link_output } => {
         let entries = link_output
@@ -54,7 +62,7 @@ impl ChunkDebugExt for Chunk {
             }
           })
           .join(", ");
-        format!("Common Chunk: SharedBy({entries})")
+        format!("Common Chunk: [Shared-By: {entries}]")
       }
     };
     self.create_reasons.push(reason);
--- crates/rolldown/tests/rolldown/function/experimental/attach_debug_info/entry-a.js
+++ crates/rolldown/tests/rolldown/function/experimental/attach_debug_info/entry-a.js
@@ -2,3 +2,4 @@ import "./shared.js";
 import "./share-splitted.js";
 
 console.log("entry-a");
+import("./dyn-entry.js").then(console.log);

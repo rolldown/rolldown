5d64d0e48d3716f7f96f5181e2011079a58ca355
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-11 20:43:10 +0900

    docs(pluginutils): add example to simple filter functions

--- packages/pluginutils/src/simple-filters.ts
+++ packages/pluginutils/src/simple-filters.ts
@@ -5,6 +5,18 @@
  *
  * @param str the string to match.
  * @param flags flags for the RegExp.
+ *
+ * @example
+ * ```ts
+ * import { exactRegex } from '@rolldown/pluginutils';
+ * const plugin = {
+ *   name: 'plugin',
+ *   resolveId: {
+ *     filter: { id: exactRegex('foo') },
+ *     handler(id) {} // will only be called for `foo`
+ *   }
+ * }
+ * ```
  */
 export function exactRegex(str: string, flags?: string): RegExp {
   return new RegExp(`^${escapeRegex(str)}$`, flags);
@@ -17,6 +29,18 @@ export function exactRegex(str: string, flags?: string): RegExp {
  *
  * @param str the string to match.
  * @param flags flags for the RegExp.
+ *
+ * @example
+ * ```ts
+ * import { prefixRegex } from '@rolldown/pluginutils';
+ * const plugin = {
+ *   name: 'plugin',
+ *   resolveId: {
+ *     filter: { id: prefixRegex('foo') },
+ *     handler(id) {} // will only be called for IDs starting with `foo`
+ *   }
+ * }
+ * ```
  */
 export function prefixRegex(str: string, flags?: string): RegExp {
   return new RegExp(`^${escapeRegex(str)}`, flags);
@@ -33,6 +57,25 @@ type WidenString<T> = T extends string ? string : T;
  * Converts a id filter to match with an id with a query.
  *
  * @param input the id filters to convert.
+ *
+ * @example
+ * ```ts
+ * import { makeIdFiltersToMatchWithQuery } from '@rolldown/pluginutils';
+ * const plugin = {
+ *   name: 'plugin',
+ *   transform: {
+ *     filter: { id: makeIdFiltersToMatchWithQuery(['**' + '/*.js', /\.ts$/]) },
+ *     // The handler will be called for IDs like:
+ *     // - foo.js
+ *     // - foo.js?foo
+ *     // - foo.txt?foo.js
+ *     // - foo.ts
+ *     // - foo.ts?foo
+ *     // - foo.txt?foo.ts
+ *     handler(code, id) {}
+ *   }
+ * }
+ * ```
  */
 export function makeIdFiltersToMatchWithQuery<T extends string | RegExp>(
   input: T,

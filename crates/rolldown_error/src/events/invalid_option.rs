031c0c01cd91f0af9e9b8e4e850f1b8313f39e16
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-06-11 12:23:39 +0800

    fix: better `NoEntryPoint` error message (#4903)
    Since the error is caused by incorrect configuration, which should not
    be unhandled.
    
    
    ![image](https://github.com/user-attachments/assets/764d63f9-9ce2-46de-9fe9-050b254dac05)

--- crates/rolldown_error/src/events/invalid_option.rs
+++ crates/rolldown_error/src/events/invalid_option.rs
@@ -6,6 +6,7 @@ pub enum InvalidOptionType {
   UnsupportedCodeSplittingFormat(String),
   InvalidOutputFile,
   InvalidOutputDirOption,
+  NoEntryPoint,
 }
 
 #[derive(Debug)]
@@ -20,11 +21,12 @@ impl BuildEvent for InvalidOption {
 
   fn message(&self, _opts: &DiagnosticOptions) -> String {
     match &self.invalid_option_type {
-      InvalidOptionType::UnsupportedCodeSplittingFormat(format) => {
-        format!("Invalid value \"{format}\" for option \"output.format\" - UMD and IIFE are not supported for code splitting. You may set `output.inlineDynamicImports` to `true` when using dynamic imports.")
-      }
-      InvalidOptionType::InvalidOutputFile => "Invalid value for option \"output.file\" - When building multiple chunks, the \"output.dir\" option must be used, not \"output.file\". You may set `output.inlineDynamicImports` to `true` when using dynamic imports.".to_string(),
-      InvalidOptionType::InvalidOutputDirOption => "Invalid value for option \"output.dir\" - you must set either \"output.file\" for a single-file build or \"output.dir\" when generating multiple chunks.".to_string()
+        InvalidOptionType::UnsupportedCodeSplittingFormat(format) => {
+            format!("Invalid value \"{format}\" for option \"output.format\" - UMD and IIFE are not supported for code splitting. You may set `output.inlineDynamicImports` to `true` when using dynamic imports.")
+          }
+        InvalidOptionType::InvalidOutputFile => "Invalid value for option \"output.file\" - When building multiple chunks, the \"output.dir\" option must be used, not \"output.file\". You may set `output.inlineDynamicImports` to `true` when using dynamic imports.".to_string(),
+        InvalidOptionType::InvalidOutputDirOption => "Invalid value for option \"output.dir\" - you must set either \"output.file\" for a single-file build or \"output.dir\" when generating multiple chunks.".to_string(),
+        InvalidOptionType::NoEntryPoint =>"You must supply `options.input` to rolldown, you should at least provide one entrypoint via `options.input` or `this.emitFile({type: 'chunk', ...})` (https://rollupjs.org/plugin-development/#this-emitfile)".to_string(),
     }
   }
 }

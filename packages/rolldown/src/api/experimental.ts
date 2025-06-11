82a453ac8073ef102cc22d007a8a6fbc727d78b5
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-11 19:50:09 +0900

    fix(node): call options hook for experimental_scan

--- packages/rolldown/src/api/experimental.ts
+++ packages/rolldown/src/api/experimental.ts
@@ -1,4 +1,5 @@
 import type { InputOptions } from '../options/input-options';
+import { PluginDriver } from '../plugin/plugin-driver';
 import { createBundler } from '../utils/create-bundler';
 import { handleOutputErrors } from '../utils/transform-to-rollup-output';
 
@@ -8,7 +9,8 @@ import { handleOutputErrors } from '../utils/transform-to-rollup-output';
  * Calling this API will only execute the scan stage of rolldown.
  */
 export const experimental_scan = async (input: InputOptions): Promise<void> => {
-  const { bundler, stopWorkers } = await createBundler(input, {});
+  const inputOptions = await PluginDriver.callOptionsHook(input);
+  const { bundler, stopWorkers } = await createBundler(inputOptions, {});
   const output = await bundler.scan();
   handleOutputErrors(output);
   await stopWorkers?.();

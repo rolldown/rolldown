579823a965eb1855588f5873eb721d2d078f1121
翠 <green@sapphi.red>
2025-06-13 11:31:20 +0900

    test: evaluate hmr tests in node (#4930)
    Added HMR runtime for tests and made the test to evaluate HMR patch
    chunks.

---
+++ crates/rolldown_testing/src/hmr-runtime.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/// <reference path="../../../crates/rolldown/src/runtime/runtime-extra-dev-common.js" />
+
+class TestDevRuntime extends DevRuntime {
+  /**
+   * @override
+   * @param {string} _moduleId
+   */
+  createModuleHotContext(_moduleId) {
+    return { accept() {} };
+  }
+  /**
+   * @override
+   * @param {string[]} _boundaries
+   */
+  applyUpdates(_boundaries) {
+    // do nothing
+  }
+}
+
+(/** @type {any} */ (globalThis)).__rolldown_runtime__ ??= new TestDevRuntime();
+
+/** @type {string[]} */
+const testPatches = /** @type {any} */ (globalThis).__testPatches;
+if (testPatches) {
+  setTimeout(async () => {
+    for (const patchChunk of testPatches) {
+      await import(patchChunk);
+    }
+  }, 0);
+}

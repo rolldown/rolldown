5c72f6e8520c3aa8316705d0e4b3c4dbe5913f93
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-06-17 01:21:57 +0800

    staging tsdown

--- tsdown.mjs
+++ tsdown.mjs
@@ -1,1 +1,11 @@
-import 'tsdown/run';
+import { spawnSync } from 'child_process';
+import path from 'path';
+
+spawnSync(process.execPath, [
+  '--import',
+  'tsx/esm',
+  '-C',
+  'dev',
+  path.resolve(import.meta.dirname, 'node_modules/tsdown/dist/run.mjs'),
+  ...process.argv.slice(2),
+], { stdio: 'inherit' });

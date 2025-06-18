c41cb56437ef54306190176c9bab0f6c46522c1c
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-06-19 02:24:10 +0800

    fix(rolldown_plugin_alias): correctly handle resolved_id

--- crates/rolldown_plugin/src/types/hook_resolve_id_output.rs
+++ crates/rolldown_plugin/src/types/hook_resolve_id_output.rs
@@ -1,5 +1,5 @@
 use arcstr::ArcStr;
-use rolldown_common::{ResolvedExternal, side_effects::HookSideEffects};
+use rolldown_common::{ResolvedExternal, ResolvedId, side_effects::HookSideEffects};
 
 #[derive(Debug, Default)]
 pub struct HookResolveIdOutput {
@@ -7,4 +7,19 @@ pub struct HookResolveIdOutput {
   pub external: Option<ResolvedExternal>,
   pub normalize_external_id: Option<bool>,
   pub side_effects: Option<HookSideEffects>,
+}
+
+impl HookResolveIdOutput {
+  pub fn from_id(id: impl Into<ArcStr>) -> Self {
+    Self { id: id.into(), ..Default::default() }
+  }
+
+  pub fn from_resolved_id(resolved_id: ResolvedId) -> Self {
+    Self {
+      id: resolved_id.id,
+      external: Some(resolved_id.external),
+      side_effects: resolved_id.side_effects,
+      normalize_external_id: None,
+    }
+  }
 }

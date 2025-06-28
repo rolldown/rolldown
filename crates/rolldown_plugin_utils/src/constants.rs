6156f4bcc9e6dcb3a2c8c64467a43e0295e2bd6b
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-06-28 08:54:34 +0800

    feat(rolldown_plugin_import_glob): align edge cases with rolldown-vite

--- crates/rolldown_plugin_utils/src/constants.rs
+++ crates/rolldown_plugin_utils/src/constants.rs
@@ -2,7 +2,7 @@ use rolldown_plugin::typedmap::TypedMapKey;
 
 #[derive(Hash, PartialEq, Eq)]
 pub struct ViteImportGlob;
-pub struct ViteImportGlobValue(bool);
+pub struct ViteImportGlobValue(pub bool);
 
 impl ViteImportGlobValue {
   pub fn is_sub_imports_pattern(&self) -> bool {

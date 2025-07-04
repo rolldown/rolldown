29605e6cf5e16edb429fe8d6cba771caff5ef4ab
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-07-04 18:43:00 +0800

    fix(rolldown_plugin_build_import_analysis): avoid default keyword error

--- packages/rolldown/tests/fixtures/builtin-plugin/build-import-analysis/basic/main.js
+++ packages/rolldown/tests/fixtures/builtin-plugin/build-import-analysis/basic/main.js
@@ -1,7 +1,7 @@
 import assert from 'node:assert'
 
 const { foo } = await import('./lib.js')
-const a = (await import('./lib.js')).foo
+const a = (await import('./lib.js')).default
 const b = (await (() => import('./lib.js'))()).foo
 const c = await import('./lib.js').then((c) => c.foo)
 import('./lib.js').then(({ foo }) => {

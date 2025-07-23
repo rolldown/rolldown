d784fa9251217b4555c269fdbae1fea1bc2d90ae
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-07-23 16:43:30 +0800

    Update crates/rolldown/src/module_finalizers/mod.rs
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- crates/rolldown/src/module_finalizers/mod.rs
+++ crates/rolldown/src/module_finalizers/mod.rs
@@ -1038,7 +1038,7 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
                     }
                   }
                   ExportsKind::CommonJs => {
-                    // If **commonjs** treeshake is eanbled,the module_namespace is included on
+                    // If **commonjs** treeshake is enabled, the module_namespace is included on
                     // demand, we should skip generate related `__reExport` statements
                     // See: https://github.com/rolldown/rolldown/blob/60fc81ada3955ce84b38a5edbb33a169d1f89f15/crates/rolldown/src/stages/link_stage/reference_needed_symbols.rs?plain=1#L148-L150
                     if !self.ctx.module_namespace_included {

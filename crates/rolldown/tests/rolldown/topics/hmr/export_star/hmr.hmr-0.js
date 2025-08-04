8dfd8ba07e529fdb6b843e9d9992b83d242cf9a2
Yunfei He <i.heyunfei@gmail.com>
2025-08-04 21:35:17 +0800

    test(rust/hmr): support writting assertion tests

--- crates/rolldown/tests/rolldown/topics/hmr/export_star/hmr.hmr-0.js
+++ crates/rolldown/tests/rolldown/topics/hmr/export_star/hmr.hmr-0.js
@@ -1,6 +1,11 @@
 
-import { foo, bar } from './sub/index.js'
+import { foo, bar, named } from './sub/index.js'
 
-console.log(foo,  bar)
+const { assert } = globalThis.__node;
+
+assert.strictEqual(foo, 'foo')
+assert.strictEqual(bar, 'bar')
+assert.strictEqual(named, 'named')
+
 
 import.meta.hot.accept()

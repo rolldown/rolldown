fc195550b76dbb36de8aab2f592f95d2a13ec156
Yunfei He <i.heyunfei@gmail.com>
2025-08-05 15:42:24 +0800

    fix(hmr): support re-assignment on `module.exports`

--- crates/rolldown/tests/rolldown/topics/hmr/runtime_correctness/cases/require/index.js
+++ crates/rolldown/tests/rolldown/topics/hmr/runtime_correctness/cases/require/index.js
@@ -1,6 +1,5 @@
 import assert from 'node:assert'
 
 const requiredCjsLib = require('./cjs-lib')
-// hyf0 FIXME: Should support re-assign `module.exports`
-// assert.strictEqual(requiredCjsLib(), 'exports')
+assert.strictEqual(requiredCjsLib(), 'exports')
 assert.strictEqual(requiredCjsLib.foo, 'foo')

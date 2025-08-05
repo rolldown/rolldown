055b015600df308b81da7dbfd71c5288ba0f3133
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-05 20:49:21 +0900

    test(hmr): `this` in CJS module should point to `module.exports`

--- crates/rolldown/tests/rolldown/topics/hmr/runtime_correctness/cases/require/cjs-lib.js
+++ crates/rolldown/tests/rolldown/topics/hmr/runtime_correctness/cases/require/cjs-lib.js
@@ -2,3 +2,4 @@ module.exports = function () {
   return 'exports'
 }
 module.exports.foo = 'foo'
+this.bar = 'bar'
--- crates/rolldown/tests/rolldown/topics/hmr/runtime_correctness/cases/require/index.js
+++ crates/rolldown/tests/rolldown/topics/hmr/runtime_correctness/cases/require/index.js
@@ -3,3 +3,4 @@ import assert from 'node:assert'
 const requiredCjsLib = require('./cjs-lib')
 assert.strictEqual(requiredCjsLib(), 'exports')
 assert.strictEqual(requiredCjsLib.foo, 'foo')
+assert.strictEqual(requiredCjsLib.bar, 'bar')

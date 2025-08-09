96175f62654390c58b9a4dcf00c62d24d3939714
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-08-09 17:44:41 +0800

    chore: clean up basic-vue

--- examples/basic-vue/rolldown.config.js
+++ examples/basic-vue/rolldown.config.js
@@ -2,24 +2,4 @@ import { defineConfig } from 'rolldown';
 
 export default defineConfig({
   input: './index.js',
-  resolve: {
-    // This needs to be explicitly set for now because oxc resolver doesn't
-    // assume default exports conditions. Rolldown will ship with a default that
-    // aligns with Vite in the future.
-    conditionNames: ['import'],
-  },
-  output: {
-    plugins: [
-      {
-        name: 'test-plugin',
-        outputOptions: function(options) {
-          options.banner = '/* banner */';
-          return options;
-        },
-      },
-    ],
-  },
-  experimental: {
-    enableComposingJsPlugins: true,
-  },
 });

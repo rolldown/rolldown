5b6be0612213fe2bbdc9b50c95618d3bdf35d8f9
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-08-14 17:06:15 +0800

    refactor: improve bundler close logic

--- packages/rolldown/tests/build-api/build-api.test.ts
+++ packages/rolldown/tests/build-api/build-api.test.ts
@@ -1,36 +1,38 @@
-import { test, expect } from 'vitest'
-import { rolldown } from 'rolldown'
-import path from 'node:path'
+import path from 'node:path';
+import { rolldown } from 'rolldown';
+import { expect, test } from 'vitest';
 
 test('rolldown write twice', async () => {
   const bundle = await rolldown({
     input: './main.js',
     cwd: import.meta.dirname,
-  })
+  });
   const esmOutput = await bundle.write({
     format: 'esm',
     entryFileNames: 'main.mjs',
-  })
-  expect(await bundle.watchFiles).toStrictEqual([path.join(import.meta.dirname, 'main.js')])
-  expect(esmOutput.output[0].fileName).toBe('main.mjs')
-  expect(esmOutput.output[0].code).toBeDefined()
+  });
+  expect(await bundle.watchFiles).toStrictEqual([
+    path.join(import.meta.dirname, 'main.js'),
+  ]);
+  expect(esmOutput.output[0].fileName).toBe('main.mjs');
+  expect(esmOutput.output[0].code).toBeDefined();
 
   const output = await bundle.write({
     format: 'iife',
     entryFileNames: 'main.js',
-  })
-  expect(output.output[0].fileName).toBe('main.js')
-  expect(output.output[0].code.includes('(function() {')).toBe(true)
-})
+  });
+  expect(output.output[0].fileName).toBe('main.js');
+  expect(output.output[0].code.includes('(function() {')).toBe(true);
+});
 
 test('rolldown concurrent write', async () => {
   const bundle = await rolldown({
     input: ['./main.js'],
     cwd: import.meta.dirname,
-  })
-  await write()
+  });
+  await write();
   // Execute twice
-  await write()
+  await write();
 
   async function write() {
     await Promise.all([
@@ -40,16 +42,81 @@ test('rolldown concurrent write', async () => {
         dir: './dist',
         entryFileNames: 'main.cjs',
       }),
-    ])
+    ]);
   }
-})
+});
 
 test('should support `Symbol.asyncDispose` of the rolldown bundle and set closed state to true', async () => {
   const bundle = await rolldown({
     input: ['./main.js'],
     cwd: import.meta.dirname,
-  })
+  });
+  await bundle.generate();
+  await bundle[Symbol.asyncDispose]();
+  expect(bundle.closed).toBe(true);
+});
 
-  await bundle[Symbol.asyncDispose]()
-  expect(bundle.closed).toBe(true)
-})
+test('passes errors from closeBundle hook', async () => {
+  let handledError = false;
+  try {
+    const bundle = await rolldown({
+      input: './main.js',
+      cwd: import.meta.dirname,
+      plugins: [
+        {
+          name: 'test',
+          closeBundle() {
+            this.error('close bundle error');
+          },
+        },
+      ],
+    });
+    await bundle.generate();
+    await bundle.close();
+  } catch (error: any) {
+    expect(error.message).toBe('close bundle error');
+    handledError = true;
+  } finally {
+    expect(handledError).toBeTruthy();
+  }
+});
+
+test('supports closeBundle hook', async () => {
+  let closeBundleCalls = 0;
+  try {
+    const bundle = await rolldown({
+      input: './main.js',
+      cwd: import.meta.dirname,
+      plugins: [
+        {
+          name: 'test',
+          closeBundle() {
+            closeBundleCalls++;
+          },
+        },
+      ],
+    });
+    await bundle.generate();
+    await bundle.close();
+  } finally {
+    expect(closeBundleCalls).toBe(1);
+  }
+});
+
+test('closeBundle hook is not called if closed directly', async () => {
+  await expect(async () => {
+    const bundle = await rolldown({
+      input: './main.js',
+      cwd: import.meta.dirname,
+      plugins: [
+        {
+          name: 'test',
+          closeBundle() {
+            this.error('close bundle error');
+          },
+        },
+      ],
+    });
+    await bundle.close();
+  }).not.toThrow()
+});

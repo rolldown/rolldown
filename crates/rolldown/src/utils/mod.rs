9ba89954d3faa14124adf7f8b11d25adc0e1f472
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-08-16 04:39:59 +0800

    refactor: move finalize_normal_module into ScopeHoistingFinalizerContext

--- crates/rolldown/src/utils/mod.rs
+++ crates/rolldown/src/utils/mod.rs
@@ -16,37 +16,3 @@ pub mod text_to_esm;
 pub mod transform_source;
 pub mod tweak_ast_for_scanning;
 pub mod uuid;
-
-use oxc::{allocator::TakeIn, ast_visit::VisitMut};
-use rolldown_common::AstScopes;
-use rolldown_ecmascript::EcmaAst;
-use rolldown_ecmascript_utils::AstSnippet;
-use rolldown_utils::indexmap::FxIndexSet;
-use rustc_hash::FxHashSet;
-
-use super::module_finalizers::{ScopeHoistingFinalizer, ScopeHoistingFinalizerContext};
-
-#[tracing::instrument(level = "trace", skip_all)]
-pub fn finalize_normal_module<'me>(
-  ctx: ScopeHoistingFinalizerContext<'me>,
-  ast: &'me mut EcmaAst,
-  ast_scope: &'me AstScopes,
-) -> ScopeHoistingFinalizerContext<'me> {
-  ast.program.with_mut(|fields| {
-    let (oxc_program, alloc) = (fields.program, fields.allocator);
-    let mut finalizer = ScopeHoistingFinalizer {
-      alloc,
-      ctx,
-      scope: ast_scope,
-      snippet: AstSnippet::new(alloc),
-      comments: oxc_program.comments.take_in(alloc),
-      generated_init_esm_importee_ids: FxHashSet::default(),
-      scope_stack: vec![],
-      top_level_var_bindings: FxIndexSet::default(),
-      is_top_level: false,
-    };
-    finalizer.visit_program(oxc_program);
-    oxc_program.comments = finalizer.comments.take_in(alloc);
-    finalizer.ctx
-  })
-}

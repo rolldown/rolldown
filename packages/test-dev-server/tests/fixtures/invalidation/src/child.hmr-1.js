f64214efbbc10a2430064a6da52e6562bcb5fc82
Yunfei He <i.heyunfei@gmail.com>
2025-08-17 14:49:52 +0800

    test(hmr): improve test of `import.meta.hot.invalidate`

--- packages/test-dev-server/tests/fixtures/invalidation/src/child.hmr-1.js
+++ packages/test-dev-server/tests/fixtures/invalidation/src/child.hmr-1.js
@@ -1,9 +1,15 @@
+import nodeFs from 'node:fs';
+
 if (import.meta.hot) {
   import.meta.hot.accept((newExports) => {
-    if (newExports.value === 'child-edited') {
+    if (newExports.value === 'child-handleable') {
+      globalThis.records.push('child-handleable');
+      nodeFs.writeFileSync('./ok-0', '');
+    } else if (newExports.value === 'child-unhandleable') {
+      globalThis.records.push('child-unhandleable');
       import.meta.hot.invalidate();
     }
   });
 }
 
-export const value = 'child-edited';
+export const value = 'child-handleable';
--- packages/test-dev-server/tests/fixtures/invalidation/src/parent.js
+++ packages/test-dev-server/tests/fixtures/invalidation/src/parent.js
@@ -1,9 +1,19 @@
+import assert from 'node:assert';
+import nodeFs from 'node:fs';
 import { value } from './child';
 
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
 
-if (value === 'child-edited') {
-  globalThis.nodeFs.writeFileSync('./ok-1', '');
+if (value === 'child-handleable') {
+  throw new Error(
+    "This change should be handled by child itself. It shouldn't propagate to parent.",
+  );
+} else if (value === 'child-unhandleable') {
+  assert.deepStrictEqual(globalThis.records, [
+    'child-handleable',
+    'child-unhandleable',
+  ]);
+  nodeFs.writeFileSync('./ok-1', '');
 }

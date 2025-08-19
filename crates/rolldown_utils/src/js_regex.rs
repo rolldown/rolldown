0090e2082814117da016b68def79b67ecf9494dc
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-08-19 16:39:16 +0800

    fix: don't match CRLF for `/./` (#5790)
    Closed https://github.com/rolldown/rolldown/issues/5784

--- crates/rolldown_utils/src/js_regex.rs
+++ crates/rolldown_utils/src/js_regex.rs
@@ -21,22 +21,28 @@ impl From<&str> for HybridRegex {
 }
 impl HybridRegex {
   pub fn new(pattern: &str) -> anyhow::Result<Self> {
-    match regex::Regex::new(pattern).map(HybridRegex::Optimize) {
+    let regex_pattern = Self::get_regex_pattern(pattern, "");
+    match regex::Regex::new(&regex_pattern).map(HybridRegex::Optimize) {
       Ok(reg) => Ok(reg),
       Err(_) => regress::Regex::new(pattern).map(HybridRegex::Ecma).map_err(anyhow::Error::from),
     }
   }
 
   pub fn with_flags(pattern: &str, flags: &str) -> anyhow::Result<Self> {
-    let regex_pattern =
-      if flags.is_empty() { pattern } else { &concat_string!("(?", flags, ")", pattern) };
-
-    match regex::Regex::new(regex_pattern).map(HybridRegex::Optimize) {
+    let regex_pattern = Self::get_regex_pattern(pattern, flags);
+    match regex::Regex::new(&regex_pattern).map(HybridRegex::Optimize) {
       Ok(reg) => Ok(reg),
       Err(_) => regress::Regex::with_flags(pattern, flags)
         .map(HybridRegex::Ecma)
         .map_err(anyhow::Error::from),
     }
+  }
+
+  fn get_regex_pattern(pattern: &str, flags: &str) -> String {
+    // ECMAScript regex treats CRLF as a line break like LF
+    // (e.g. `/a$/m.test("a\r\n")` and `/a$/m.test("a\n")` both returns `true`)
+    // Also when `s` flag is not used, `.` does not match CRLF like LF does not.
+    concat_string!("(?R", flags, ")", pattern)
   }
 
   pub fn matches(&self, text: &str) -> bool {
@@ -89,14 +95,14 @@ fn regress_regexp_replace_all<'h>(
 
 #[cfg(test)]
 mod test {
-  use crate::js_regex::regress_regexp_replace_all;
+  use crate::js_regex::{HybridRegex, regress_regexp_replace_all};
 
   #[test]
   fn with_flags() {
-    let reg = super::HybridRegex::with_flags("a", "i").unwrap();
+    let reg = HybridRegex::with_flags("a", "i").unwrap();
     assert!(reg.matches("A"));
 
-    let reg = super::HybridRegex::new("a").unwrap();
+    let reg = HybridRegex::new("a").unwrap();
     assert!(!reg.matches("A"));
   }
 
@@ -104,5 +110,26 @@ mod test {
   fn regress_replace_all() {
     let reg = regress::Regex::new("\\d+").unwrap();
     assert_eq!(regress_regexp_replace_all(&reg, "111aa111", "1"), "1aa1");
+  }
+
+  #[test]
+  fn js_regex_compat_dot() {
+    let dot_reg = HybridRegex::new(".").unwrap();
+    assert!(!dot_reg.matches("\n"));
+    assert!(!dot_reg.matches("\r\n"));
+    // assert!(!dot_reg.matches("\u{2028}")); // FIXME: LINE SEPARATOR should not match
+    // assert!(!dot_reg.matches("\u{2029}")); // FIXME: PARAGRAPH SEPARATOR should not match
+    let dots_reg = HybridRegex::with_flags(".", "s").unwrap();
+    assert!(dots_reg.matches("\n"));
+    assert!(dots_reg.matches("\r\n"));
+    assert!(dots_reg.matches("\u{2028}"));
+    assert!(dots_reg.matches("\u{2029}"));
+  }
+
+  #[test]
+  fn js_regex_compat_multiline() {
+    let reg = HybridRegex::with_flags("a$", "m").unwrap();
+    assert!(reg.matches("a\n"));
+    assert!(reg.matches("a\r\n"));
   }
 }

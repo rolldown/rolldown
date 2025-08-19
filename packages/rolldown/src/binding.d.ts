ab7758fa19c84f121fa3d59adfec74a0ab8d6d64
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-19 18:22:52 +0900

    fix(plugin/vite-resolve): enable `yarnPnp` option when pnp is detected

--- packages/rolldown/src/binding.d.ts
+++ packages/rolldown/src/binding.d.ts
@@ -1961,6 +1961,7 @@ export interface BindingViteResolvePluginConfig {
   resolveSubpathImports: (id: string, importer: string, isRequire: boolean, scan: boolean) => VoidNullable<string>
   onWarn?: (message: string) => void
   onDebug?: (message: string) => void
+  yarnPnp: boolean
 }
 
 export interface BindingViteResolvePluginResolveOptions {

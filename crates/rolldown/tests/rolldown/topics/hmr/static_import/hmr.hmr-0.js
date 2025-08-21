6dd3d3eb198c0063c90888f2e4a11f65e043a69a
翠 <green@sapphi.red>
2025-08-22 00:18:06 +0900

    test(hmr): static import cases (#5820)
    refs #4299

--- crates/rolldown/tests/rolldown/topics/hmr/static_import/hmr.hmr-0.js
+++ crates/rolldown/tests/rolldown/topics/hmr/static_import/hmr.hmr-0.js
@@ -1,7 +1,24 @@
-import './exist-dep-cjs'
-import './exist-dep-esm'
-import './new-dep-cjs'
-import './new-dep-esm'
+import assert from 'node:assert'
+import './modules/dep-cjs'
+import './modules/dep-esm'
+import cjsDefault from './modules/dep-cjs-default'
+import esmDefault from './modules/dep-esm-default'
+import { named as cjsNamed } from './modules/dep-cjs-named'
+import { named as esmNamed } from './modules/dep-esm-named'
+import * as cjsNamespace from './modules/dep-cjs-namespace'
+import * as esmNamespace from './modules/dep-esm-namespace'
+import './modules/new-dep-cjs'
+import './modules/new-dep-esm'
+
+assert.strictEqual(cjsDefault, 'cjs-default')
+assert.strictEqual(esmDefault, 'esm-default')
+assert.strictEqual(cjsNamed, 'cjs-named')
+assert.strictEqual(esmNamed, 'esm-named')
+assert.deepStrictEqual(cjsNamespace, {
+  value: 'cjs-namespace',
+  default: { value: 'cjs-namespace' }
+})
+assert.deepStrictEqual(esmNamespace, { value: 'esm-namespace' })
 
 import.meta.hot.accept((mod) => {
   if (mod) {

9c0e1a3568c240f40614142aa7e5692b11a3c5fa
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-08-24 01:01:23 +0800

    chore: bump rolldown-ariadne (#5863)
    Support rendering help and note without any labels.
    
    see test case in https://github.com/rolldown/ariadne/pull/11/files

--- packages/rolldown/tests/fixtures/plugin/prefer-builtin-feature/_config.ts
+++ packages/rolldown/tests/fixtures/plugin/prefer-builtin-feature/_config.ts
@@ -18,9 +18,17 @@ export default defineTest({
 		},
 	},
 	afterTest() {
-		expect(warnings).toEqual([
-			`[PREFER_BUILTIN_FEATURE] Warning: The functionality provided by \`@rollup/plugin-json\` is already covered natively, maybe you could remove the plugin from your configuration\n`,
-      `[PREFER_BUILTIN_FEATURE] Warning: Rolldown supports \`inject\` natively, please refer https://rolldown.rs/reference/config-options for more details, this is performant than passing \`@rollup/plugin-inject\` to plugins option.\n`
-		]);
+		expect(warnings).toMatchInlineSnapshot(`
+			[
+			  "[PREFER_BUILTIN_FEATURE] Warning: The functionality provided by \`@rollup/plugin-json\` is already covered natively, maybe you could remove the plugin from your configuration
+			  │ 
+			  │ Help: This diagnostic may be false positive, you could turn it off via \`checks.preferBuiltinFeature\`
+			",
+			  "[PREFER_BUILTIN_FEATURE] Warning: Rolldown supports \`inject\` natively, please refer https://rolldown.rs/reference/config-options for more details, this is performant than passing \`@rollup/plugin-inject\` to plugins option.
+			  │ 
+			  │ Help: This diagnostic may be false positive, you could turn it off via \`checks.preferBuiltinFeature\`
+			",
+			]
+		`);
 	},
 });

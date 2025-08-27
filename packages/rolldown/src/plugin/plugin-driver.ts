3938a1dd4bbf037f80a0ffd711325be9e7b7dd3c
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-08-27 22:52:54 +0800

    fix: use Symbol existance to detect if a plugin is BuiltinPlugin

--- packages/rolldown/src/plugin/plugin-driver.ts
+++ packages/rolldown/src/plugin/plugin-driver.ts
@@ -1,5 +1,5 @@
 import type { InputOptions, OutputOptions, RolldownPlugin } from '..';
-import { BuiltinPlugin } from '../builtin-plugin/utils';
+import { isBuiltinPlugin } from '../builtin-plugin/utils';
 import type { LogHandler } from '../log/log-handler';
 import { getLogger, getOnLog } from '../log/logger';
 import { LOG_LEVEL_INFO, type LogLevelOption } from '../log/logging';
@@ -90,7 +90,7 @@ export function getObjectPlugins(plugins: RolldownPlugin[]): Plugin[] {
     if ('_parallel' in plugin) {
       return undefined;
     }
-    if (plugin instanceof BuiltinPlugin) {
+    if (isBuiltinPlugin(plugin)) {
       return undefined;
     }
     return plugin;

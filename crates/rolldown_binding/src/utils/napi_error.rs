d4be0f0cf0cf5f4ca9e9f048e93fcc0e507866dd
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-28 15:07:04 +0900

    fix: update

--- crates/rolldown_binding/src/utils/napi_error.rs
+++ crates/rolldown_binding/src/utils/napi_error.rs
@@ -1,17 +1,25 @@
 pub fn resolve_error(specifier: &str, err: anyhow::Error) -> napi::Error {
-  let mut new_err =
-    napi::Error::from_reason(format!("Errored while resolving {specifier:?} in `this.resolve`."));
-  if let Ok(cause) = err.downcast::<napi::Error>() {
-    new_err.set_cause(cause);
+  if err.downcast_ref::<napi::Error>().is_some() {
+    let mut new_err =
+      napi::Error::from_reason(format!("Errored while resolving {specifier:?} in `this.resolve`."));
+    new_err.set_cause(err.downcast::<napi::Error>().unwrap());
+    new_err
+  } else {
+    napi::Error::from_reason(format!(
+      "Errored while resolving {specifier:?} in `this.resolve`. Got {err:?}."
+    ))
   }
-  new_err
 }
 
 pub fn load_error(specifier: &str, err: anyhow::Error) -> napi::Error {
-  let mut new_err =
-    napi::Error::from_reason(format!("Errored while load {specifier:?} in `this.load`."));
-  if let Ok(cause) = err.downcast::<napi::Error>() {
-    new_err.set_cause(cause);
+  if err.downcast_ref::<napi::Error>().is_some() {
+    let mut new_err =
+      napi::Error::from_reason(format!("Errored while load {specifier:?} in `this.load`."));
+    new_err.set_cause(err.downcast::<napi::Error>().unwrap());
+    new_err
+  } else {
+    napi::Error::from_reason(format!(
+      "Errored while load {specifier:?} in `this.load`. Got {err:?}."
+    ))
   }
-  new_err
 }

f5510f0e5b5580cbd293648e86003cf27e11de52
Boshen <boshenc@gmail.com>
2025-08-30 14:15:49 +0800

    feat(rolldown): oxc v0.84.0

--- packages/rolldown/tests/fixtures/output/sourcemap/methods/_config.ts
+++ packages/rolldown/tests/fixtures/output/sourcemap/methods/_config.ts
@@ -11,18 +11,18 @@ export default defineTest({
   afterTest: function (output) {
     expect(output.output[0].map).toBeDefined()
     expect(output.output[0].map!.toString()).toMatchInlineSnapshot(
-      `"{"version":3,"file":"main.js","names":[],"sources":["../main.js"],"sourcesContent":["console.log(foo)\\n"],"mappings":";AAAA,QAAQ,IAAI"}"`,
+      `"{"version":3,"file":"main.js","names":[],"sources":["../main.js"],"sourcesContent":["console.log(foo)\\n"],"mappings":";AAAA,QAAQ,IAAI,IAAI"}"`,
     )
     expect(output.output[0].map!.toUrl()).toMatchInlineSnapshot(
-      `"data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm5hbWVzIjpbXSwic291cmNlcyI6WyIuLi9tYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKGZvbylcbiJdLCJtYXBwaW5ncyI6IjtBQUFBLFFBQVEsSUFBSSJ9"`,
+      `"data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm5hbWVzIjpbXSwic291cmNlcyI6WyIuLi9tYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKGZvbylcbiJdLCJtYXBwaW5ncyI6IjtBQUFBLFFBQVEsSUFBSSxJQUFJIn0="`,
     )
     const map = output.output[0].map!
     map.file = 'main2.js'
     expect(map.toString()).toMatchInlineSnapshot(
-      `"{"version":3,"file":"main2.js","names":[],"sources":["../main.js"],"sourcesContent":["console.log(foo)\\n"],"mappings":";AAAA,QAAQ,IAAI"}"`,
+      `"{"version":3,"file":"main2.js","names":[],"sources":["../main.js"],"sourcesContent":["console.log(foo)\\n"],"mappings":";AAAA,QAAQ,IAAI,IAAI"}"`,
     )
     expect(map.toUrl()).toMatchInlineSnapshot(
-      `"data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbjIuanMiLCJuYW1lcyI6W10sInNvdXJjZXMiOlsiLi4vbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhmb28pXG4iXSwibWFwcGluZ3MiOiI7QUFBQSxRQUFRLElBQUkifQ=="`,
+      `"data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbjIuanMiLCJuYW1lcyI6W10sInNvdXJjZXMiOlsiLi4vbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhmb28pXG4iXSwibWFwcGluZ3MiOiI7QUFBQSxRQUFRLElBQUksSUFBSSJ9"`,
     )
   },
 })

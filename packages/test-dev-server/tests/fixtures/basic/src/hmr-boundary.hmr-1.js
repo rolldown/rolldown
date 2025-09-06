1e6e529a8285299e6c5141ec934a018d5a6f71a8
Yunfei He <i.heyunfei@gmail.com>
2025-09-06 20:27:13 +0800

    test(hmr): tweak improper test

--- packages/test-dev-server/tests/fixtures/basic/src/hmr-boundary.hmr-1.js
+++
@@ -1,7 +0,0 @@
-import { value as depValue } from './new-dep';
-export const value = depValue;
-
-import.meta.hot.accept((newExports) => {
-  globalThis.hmrChange(newExports);
-});
-console.log('HMR boundary file changed');

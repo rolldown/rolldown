ec758d423ce79b97b0332815d7eb35ddf0212a92
Yunfei He <i.heyunfei@gmail.com>
2025-09-07 20:56:06 +0800

    test(hmr): tweak improper test (#6034)

--- packages/test-dev-server/tests/fixtures/basic/src/hmr-boundary.js
+++
@@ -1,5 +0,0 @@
-export const value = 1;
-
-import.meta.hot.accept((newExports) => {
-  globalThis.hmrChange(newExports);
-});
---
+++ packages/test-dev-server/tests/fixtures/edit/src/main.js
@@ -0,0 +1,10 @@
+import assert from 'node:assert';
+import nodeFs from 'node:fs';
+export { value as fooValue } from './foo';
+
+if (import.meta.hot) {
+  import.meta.hot.accept((newExports) => {
+    assert.equal(newExports.fooValue, 'edited-foo');
+    nodeFs.writeFileSync('./ok-0', '');
+  });
+}

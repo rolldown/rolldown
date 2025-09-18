20d9cdd75b43f047d881ed5072fd7a3aecbdf7cc
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-09-18 08:47:27 +0000

    refactor: remove unused `SpanExt` (#6266)

--- crates/rolldown_ecmascript_utils/src/extensions/mod.rs
+++ crates/rolldown_ecmascript_utils/src/extensions/mod.rs
@@ -1,2 +1,1 @@
 pub mod ast_ext;
-pub mod span_ext;
--- crates/rolldown_ecmascript_utils/src/lib.rs
+++ crates/rolldown_ecmascript_utils/src/lib.rs
@@ -4,13 +4,10 @@ mod scope;
 
 pub use crate::{
   ast_snippet::AstSnippet,
-  extensions::{
-    ast_ext::{
-      binding_identifier_ext::BindingIdentifierExt, binding_pattern_ext::BindingPatternExt,
-      call_expression_ext::CallExpressionExt, expression_ext::ExpressionExt, function::FunctionExt,
-      jsx::JsxExt, statement_ext::StatementExt,
-    },
-    span_ext::SpanExt,
+  extensions::ast_ext::{
+    binding_identifier_ext::BindingIdentifierExt, binding_pattern_ext::BindingPatternExt,
+    call_expression_ext::CallExpressionExt, expression_ext::ExpressionExt, function::FunctionExt,
+    jsx::JsxExt, statement_ext::StatementExt,
   },
   scope::is_top_level,
 };

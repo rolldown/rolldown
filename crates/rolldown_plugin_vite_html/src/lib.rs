07af74cf8696600080f8efb177e3db2c9773182a
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-09-25 14:05:00 +0000

    feat(rolldown_plugin_vite_html): use `html5ever` and `markup5ever_rcdom` (#6327)

--- crates/rolldown_plugin_vite_html/src/lib.rs
+++ crates/rolldown_plugin_vite_html/src/lib.rs
@@ -1,6 +1,10 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, rc::Rc};
 
+use html5ever::{ParseOpts, parse_document, tendril::TendrilSink as _};
+use markup5ever_rcdom as rcdom;
 use rolldown_plugin::{HookUsage, Plugin};
+use rolldown_utils::pattern_filter::normalize_path;
+use sugar_path::SugarPath as _;
 
 #[derive(Debug, Default)]
 pub struct ViteHtmlPlugin;
@@ -14,11 +18,35 @@ impl Plugin for ViteHtmlPlugin {
     HookUsage::Transform | HookUsage::GenerateBundle
   }
 
+  #[expect(unused_variables)]
   async fn transform(
     &self,
-    _ctx: rolldown_plugin::SharedTransformPluginContext,
-    _args: &rolldown_plugin::HookTransformArgs<'_>,
+    ctx: rolldown_plugin::SharedTransformPluginContext,
+    args: &rolldown_plugin::HookTransformArgs<'_>,
   ) -> rolldown_plugin::HookTransformReturn {
+    if !args.id.ends_with(".html") {
+      return Ok(None);
+    }
+
+    let path = args.id.relative(ctx.cwd());
+    let relative_url_path = normalize_path(&path.to_string_lossy());
+
+    let dom = parse_document(rcdom::RcDom::default(), ParseOpts::default()).one(args.code.as_str());
+
+    // TODO: Extract into a function
+    let mut stack = vec![dom.document];
+    while let Some(node) = stack.pop() {
+      match &node.data {
+        rcdom::NodeData::Element { name, .. } if &*name.local == "script" => {
+          todo!()
+        }
+        _ => {}
+      }
+      for child in node.children.borrow().iter() {
+        stack.push(Rc::clone(child));
+      }
+    }
+
     todo!()
   }
 

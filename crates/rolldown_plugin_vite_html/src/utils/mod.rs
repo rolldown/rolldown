40a5060f2436c3802ba122bf4c85fc36692e3b3e
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-10-14 14:56:03 +0000

    feat(rolldown_plugin_vite_html): support inject_to_head function (#6522)

--- crates/rolldown_plugin_vite_html/src/utils/mod.rs
+++ crates/rolldown_plugin_vite_html/src/utils/mod.rs
@@ -1,4 +1,5 @@
 mod helpers;
+mod html_inject;
 mod plugin_ext;
 mod script_inline_import_visistor;
 
@@ -9,4 +10,6 @@ pub use helpers::{
   ImportedChunk, get_imported_chunks, is_entirely_import, is_excluded_url,
   overwrite_check_public_file,
 };
+#[expect(unused_imports)]
+pub use html_inject::inject_to_head;
 pub use script_inline_import_visistor::ScriptInlineImportVisitor;

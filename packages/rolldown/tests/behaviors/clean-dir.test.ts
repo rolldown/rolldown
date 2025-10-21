f6ef257b4238b0436172a50ae8bd49734e3969de
James Aprosail <aprosail@outlook.com>
2025-10-21 11:10:18 +0800

    test(clean-dir): mv test dir and fixes

--- packages/rolldown/tests/behaviors/clean-dir.test.ts
+++ packages/rolldown/tests/behaviors/clean-dir.test.ts
@@ -1,4 +1,4 @@
-import { existsSync } from 'node:fs';
+import { existsSync, rmSync } from 'node:fs';
 import { join } from 'node:path';
 import { rolldown } from 'rolldown';
 import { expect, test } from 'vitest';
@@ -6,17 +6,21 @@ import { expect, test } from 'vitest';
 const root = import.meta.dirname
 
 test('clean out dir', async () => {
+  const outdir = join(root, 'dist/clean-dir')
+  if (existsSync(outdir)) rmSync(outdir, { recursive: true })
+  
   const bundler = await rolldown({ input: 'index.ts', cwd: root })
-  const outdir = 'dist'
   await bundler.write({ dir: outdir, entryFileNames: 'index1.js', cleanDir: true })
-  expect(existsSync(join(root, 'dist/index1.js'))).toBe(true)
+  expect(existsSync(join(outdir, 'index1.js'))).toBe(true)
 
   await bundler.write({ dir: outdir, entryFileNames: 'index2.js', cleanDir: false })
-  expect(existsSync(join(root, 'dist/index1.js'))).toBe(true)
-  expect(existsSync(join(root, 'dist/index2.js'))).toBe(true)
+  expect(existsSync(join(outdir, 'index1.js'))).toBe(true)
+  expect(existsSync(join(outdir, 'index2.js'))).toBe(true)
 
   await bundler.write({ dir: outdir, entryFileNames: 'index3.js', cleanDir: true })
-  expect(existsSync(join(root, 'dist/index1.js'))).toBe(false)
-  expect(existsSync(join(root, 'dist/index2.js'))).toBe(false)
-  expect(existsSync(join(root, 'dist/index3.js'))).toBe(true)
+  expect(existsSync(join(outdir, 'index1.js'))).toBe(false)
+  expect(existsSync(join(outdir, 'index2.js'))).toBe(false)
+  expect(existsSync(join(outdir, 'index3.js'))).toBe(true)
+
+  rmSync(outdir, { recursive: true })
 })

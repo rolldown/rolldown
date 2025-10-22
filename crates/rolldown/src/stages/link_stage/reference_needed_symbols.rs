01858fd1bc07b435aaf8c92776b13a7c038483c6
IWANABETHATGUY <iwanabethatguy@qq.com>
2025-10-22 14:18:43 +0000

    fix: rename variable to better reflect its purpose and fix CJS imports (#6663)
    Closed #6587

--- crates/rolldown/src/stages/link_stage/reference_needed_symbols.rs
+++ crates/rolldown/src/stages/link_stage/reference_needed_symbols.rs
@@ -98,8 +98,7 @@ impl LinkStage<'_> {
                       if matches!(
                         self.options.format,
                         OutputFormat::Cjs | OutputFormat::Iife | OutputFormat::Umd
-                      ) && !rec.meta.contains(ImportRecordMeta::IsPlainImport)
-                      {
+                      ) {
                         stmt_info.side_effect = true.into();
                         depended_runtime_helper_map[RuntimeHelper::ToEsm.bit_index()]
                           .push(stmt_info_idx);

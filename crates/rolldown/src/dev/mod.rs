142ed998ee9f2694f62c2307c6cf7c038ebdc3f7
hyf0 <i.heyunfei@gmail.com>
2025-10-22 14:02:31 +0000

    chore(test): move dev related options to rolldown_dev_common (#6667)
    I added `rolldown_dev_common`​ because types in `rolldown_dev_common`​ only relies on `rolldown_common`​. To emphasize the relationship, I used a new crate. Also splitting to more crates to friendly to compiling in paralell.

--- crates/rolldown/src/dev/mod.rs
+++ crates/rolldown/src/dev/mod.rs
@@ -4,15 +4,15 @@ pub mod build_state_machine;
 pub mod building_task;
 pub mod dev_context;
 pub mod dev_engine;
-pub mod dev_options;
-pub mod rebuild_strategy;
 pub mod types;
 pub mod watcher_event_handler;
 
 use std::sync::Arc;
 
-pub use dev_options::{DevOptions, NormalizedDevOptions, OnHmrUpdatesCallback};
-pub use rebuild_strategy::RebuildStrategy;
+pub use rolldown_dev_common::types::{
+  BundleOutput, DevOptions, DevWatchOptions, NormalizedDevOptions, OnHmrUpdatesCallback,
+  OnOutputCallback, RebuildStrategy, SharedNormalizedDevOptions, normalize_dev_options,
+};
 use rolldown_utils::dashmap::FxDashMap;
 
 use crate::dev::types::client_session::ClientSession;
--- crates/rolldown/src/types/bundle_output.rs
+++ crates/rolldown/src/types/bundle_output.rs
@@ -1,8 +1,1 @@
-use rolldown_common::Output;
-use rolldown_error::BuildDiagnostic;
-
-#[derive(Default)]
-pub struct BundleOutput {
-  pub warnings: Vec<BuildDiagnostic>,
-  pub assets: Vec<Output>,
-}
+pub use rolldown_dev_common::types::BundleOutput;

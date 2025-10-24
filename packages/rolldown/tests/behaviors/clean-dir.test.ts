20bf5f77017ca1bcadaad8968d8e157f2a009d01
James Aprosail <aprosail@outlook.com>
2025-10-24 13:59:06 +0800

    fix(clean-dir): advance clean outdir to allow `generateBundle` outputs (#6647)
    See:
    https://github.com/rolldown/rolldown/pull/6486#issuecomment-3421660516
    
    Advance the "clean dir" code before the `bundle_up` is called inside the
    `bundle_write` function: `fn generate_bundle` is called in `fn
    bundle_up`, and the "clean dir" code is advanced before the call of `fn
    bundle_up` in `fn bundle_write`.
    
    - [x] Fix the source code and pass all current tests.
    - [x] Create new behavior tests about the `generateBundle` and
    `writeBundle` hook.
    
    I also fix another bug about testing timeout configuration, which
    prevents the previous random failure when testing spent more time than
    the default 20s (20_000ms).

--- packages/rolldown/tests/behaviors/clean-dir.test.ts
+++ packages/rolldown/tests/behaviors/clean-dir.test.ts
@@ -1,22 +1,104 @@
-import { existsSync } from 'node:fs';
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  rmSync,
+  writeFileSync,
+} from 'node:fs';
 import { join } from 'node:path';
-import { rolldown } from 'rolldown';
+import { rolldown, RolldownPlugin } from 'rolldown';
 import { expect, test } from 'vitest';
 
-const root = import.meta.dirname
+const root = import.meta.dirname;
+const input = join(root, 'index.ts');
 
-test('clean out dir', async () => {
-  const bundler = await rolldown({ input: 'index.ts', cwd: root })
-  const outdir = 'dist'
-  await bundler.write({ dir: outdir, entryFileNames: 'index1.js', cleanDir: true })
-  expect(existsSync(join(root, 'dist/index1.js'))).toBe(true)
+test('clean outdir', async () => {
+  const outdir = join(root, 'dist/clean-dir');
+  if (existsSync(outdir)) rmSync(outdir, { recursive: true });
 
-  await bundler.write({ dir: outdir, entryFileNames: 'index2.js', cleanDir: false })
-  expect(existsSync(join(root, 'dist/index1.js'))).toBe(true)
-  expect(existsSync(join(root, 'dist/index2.js'))).toBe(true)
+  const bundler = await rolldown({ input, cwd: root });
+  await bundler.write({
+    dir: outdir,
+    entryFileNames: 'index1.js',
+    cleanDir: true,
+  });
+  expect(existsSync(join(outdir, 'index1.js'))).toBe(true);
 
-  await bundler.write({ dir: outdir, entryFileNames: 'index3.js', cleanDir: true })
-  expect(existsSync(join(root, 'dist/index1.js'))).toBe(false)
-  expect(existsSync(join(root, 'dist/index2.js'))).toBe(false)
-  expect(existsSync(join(root, 'dist/index3.js'))).toBe(true)
-})
+  await bundler.write({
+    dir: outdir,
+    entryFileNames: 'index2.js',
+    cleanDir: false,
+  });
+  expect(existsSync(join(outdir, 'index1.js'))).toBe(true);
+  expect(existsSync(join(outdir, 'index2.js'))).toBe(true);
+
+  await bundler.write({
+    dir: outdir,
+    entryFileNames: 'index3.js',
+    cleanDir: true,
+  });
+  expect(existsSync(join(outdir, 'index1.js'))).toBe(false);
+  expect(existsSync(join(outdir, 'index2.js'))).toBe(false);
+  expect(existsSync(join(outdir, 'index3.js'))).toBe(true);
+
+  rmSync(outdir, { recursive: true });
+});
+
+// When cleanDir is true, and there are file output in
+// the `generateBundle` hook, the file should not be cleaned.
+test('clean outdir hooks', async () => {
+  const generateBundleFile = 'generate-bundle.md';
+  const generateBundleContent = 'Generate bundle output.';
+
+  const writeBundleFile = 'write-bundle.md';
+  const writeBundleContent = 'Write bundle output.';
+
+  function examplePlugin(): RolldownPlugin {
+    return {
+      name: 'example-plugin',
+      generateBundle(outputOptions) {
+        const outputDir = outputOptions.dir;
+        if (!outputDir) throw new Error('cannot get outdir in plugin');
+        mkdirSync(outputDir, { recursive: true });
+        writeFileSync(
+          join(outputDir, generateBundleFile),
+          generateBundleContent,
+        );
+      },
+      writeBundle(outputOptions) {
+        const outputDir = outputOptions.dir;
+        if (!outputDir) throw new Error('cannot get outdir in plugin');
+        mkdirSync(outputDir, { recursive: true });
+        writeFileSync(join(outputDir, writeBundleFile), writeBundleContent);
+      },
+    };
+  }
+
+  const outdir = join(root, 'dist/clean-dir-hooks');
+  if (existsSync(outdir)) rmSync(outdir, { recursive: true });
+  expect(existsSync(outdir)).toBe(false);
+
+  const bundler = await rolldown({
+    plugins: [examplePlugin()],
+    input,
+    cwd: root,
+  });
+  
+  await bundler.write({
+    dir: outdir,
+    entryFileNames: 'index.js',
+    cleanDir: true,
+  });
+
+  expect(existsSync(join(outdir, generateBundleFile))).toBe(true);
+  expect(readFileSync(join(outdir, generateBundleFile), 'utf-8')).toBe(
+    generateBundleContent,
+  );
+
+  expect(existsSync(join(outdir, writeBundleFile))).toBe(true);
+  expect(readFileSync(join(outdir, writeBundleFile), 'utf-8')).toBe(
+    writeBundleContent,
+  );
+
+  rmSync(outdir, { recursive: true });
+});

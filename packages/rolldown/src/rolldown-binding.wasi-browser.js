4b563b1e53095f52cdfb6af59ab62d1f2a2aa1ea
hyf0 <i.heyunfei@gmail.com>
2025-10-25 13:45:22 +0000

    refactor: remove unnecessaey lazy data passing for `BindingOutputs` (#6692)
    https://github.com/rolldown/rolldown/blob/a53d4418cab2293168f6545e1060dac58fdf55fb/packages/rolldown/src/utils/transform-to-rollup-output.ts#L135
    
    get called all the time

--- packages/rolldown/src/rolldown-binding.wasi-browser.js
+++ packages/rolldown/src/rolldown-binding.wasi-browser.js
@@ -94,7 +94,6 @@ export const BindingModuleInfo = __napiModule.exports.BindingModuleInfo
 export const BindingNormalizedOptions = __napiModule.exports.BindingNormalizedOptions
 export const BindingOutputAsset = __napiModule.exports.BindingOutputAsset
 export const BindingOutputChunk = __napiModule.exports.BindingOutputChunk
-export const BindingOutputs = __napiModule.exports.BindingOutputs
 export const BindingPluginContext = __napiModule.exports.BindingPluginContext
 export const BindingRenderedChunk = __napiModule.exports.BindingRenderedChunk
 export const BindingRenderedChunkMeta = __napiModule.exports.BindingRenderedChunkMeta

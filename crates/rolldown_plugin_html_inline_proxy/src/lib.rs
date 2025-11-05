d3fe7d8842b929dbb338c87026875e56b14db47f
dalaoshu <165626830+shulaoda@users.noreply.github.com>
2025-11-05 16:59:58 +0800

    feat(rolldown_plugin_html_inline_proxy): align `resolveId` hook logic (#6854)

--- crates/rolldown_plugin_html_inline_proxy/src/lib.rs
+++ crates/rolldown_plugin_html_inline_proxy/src/lib.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 
-use rolldown_plugin::{HookUsage, Plugin};
+use rolldown_plugin::{HookResolveIdOutput, HookUsage, Plugin};
 
 #[derive(Debug)]
 pub struct HtmlInlineProxyPlugin;
@@ -11,6 +11,17 @@ impl Plugin for HtmlInlineProxyPlugin {
   }
 
   fn register_hook_usage(&self) -> HookUsage {
-    HookUsage::empty()
+    HookUsage::ResolveId
+  }
+
+  async fn resolve_id(
+    &self,
+    _ctx: &rolldown_plugin::PluginContext,
+    args: &rolldown_plugin::HookResolveIdArgs<'_>,
+  ) -> rolldown_plugin::HookResolveIdReturn {
+    if rolldown_plugin_utils::find_special_query(args.specifier, b"html-proxy").is_some() {
+      return Ok(Some(HookResolveIdOutput::from_id(args.specifier)));
+    }
+    Ok(None)
   }
 }

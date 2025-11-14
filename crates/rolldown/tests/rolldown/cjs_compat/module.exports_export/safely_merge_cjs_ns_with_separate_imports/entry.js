5d656f3f497f1bd2f1daa6770cc61776ead46575
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-11-15 01:14:52 +0900

    test: fix order

--- crates/rolldown/tests/rolldown/cjs_compat/module.exports_export/safely_merge_cjs_ns_with_separate_imports/entry.js
+++ crates/rolldown/tests/rolldown/cjs_compat/module.exports_export/safely_merge_cjs_ns_with_separate_imports/entry.js
@@ -1,7 +1,7 @@
 // Test case with SEPARATE import statements for named and default imports
 import assert from 'node:assert';
-import React from 'this-is-only-used-for-testing';
 import { useState } from 'this-is-only-used-for-testing';
+import React from 'this-is-only-used-for-testing';
 
 assert.deepStrictEqual(typeof React, 'function');
 assert.deepStrictEqual(React, React.default);

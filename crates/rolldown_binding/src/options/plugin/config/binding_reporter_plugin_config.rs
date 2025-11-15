26996a053954223bc1f4866a9349913199eb2e5c
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-11-15 19:28:49 +0800

    refactor(rolldown_plugin_reporter): use root instead of cwd

--- crates/rolldown_binding/src/options/plugin/config/binding_reporter_plugin_config.rs
+++ crates/rolldown_binding/src/options/plugin/config/binding_reporter_plugin_config.rs
@@ -1,9 +1,20 @@
+use std::{
+  path::PathBuf,
+  sync::{
+    Arc, RwLock,
+    atomic::{AtomicBool, AtomicU32},
+  },
+  time::Instant,
+};
+
 use rolldown_plugin_reporter::ReporterPlugin;
+use sugar_path::SugarPath as _;
 
 #[napi_derive::napi(object, object_to_js = false)]
 #[derive(Debug, Default)]
 #[expect(clippy::struct_excessive_bools)]
 pub struct BindingReporterPluginConfig {
+  pub root: String,
   pub is_tty: bool,
   pub is_lib: bool,
   pub assets_dir: String,
@@ -15,14 +26,21 @@ pub struct BindingReporterPluginConfig {
 
 impl From<BindingReporterPluginConfig> for ReporterPlugin {
   fn from(config: BindingReporterPluginConfig) -> Self {
-    Self::new(
-      config.is_tty,
-      config.should_log_info,
-      config.chunk_limit as usize,
-      config.report_compressed_size,
-      config.assets_dir,
-      config.is_lib,
-      config.warn_large_chunks,
-    )
+    Self {
+      root: PathBuf::from(config.root).normalize(),
+      is_lib: config.is_lib,
+      is_tty: config.is_tty,
+      assets_dir: config.assets_dir,
+      chunk_limit: config.chunk_limit as usize,
+      should_log_info: config.should_log_info,
+      warn_large_chunks: config.warn_large_chunks,
+      report_compressed_size: config.report_compressed_size,
+      chunk_count: AtomicU32::new(0),
+      compressed_count: AtomicU32::new(0),
+      has_rendered_chunk: AtomicBool::new(false),
+      has_transformed: AtomicBool::new(false),
+      transformed_count: AtomicU32::new(0),
+      latest_checkpoint: Arc::new(RwLock::new(Instant::now())),
+    }
   }
 }

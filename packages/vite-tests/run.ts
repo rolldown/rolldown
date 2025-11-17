6920c907efaef95435a55ee64fa19c4758e05c9c
shulaoda <165626830+shulaoda@users.noreply.github.com>
2025-11-17 10:09:49 +0000

    test(vite-tests): use integration branch for vite compatibility tests (#7091)
    closes #7090
    
    Let's fix the CI issue first, and later we can explore whether there's a better way to determine if the branch is up to date, or even automate the rebasing process.

--- packages/vite-tests/run.ts
+++ packages/vite-tests/run.ts
@@ -46,7 +46,12 @@ fs.rmSync(REPO_PATH, { recursive: true, force: true });
 
 await runCmdAndPipeOrExit(
   '# Cloning rolldown-vite repo...',
-  ['git', ['clone', 'https://github.com/vitejs/rolldown-vite.git', REPO_PATH]],
+  ['git', ['clone', '--branch', 'integration', 'https://github.com/vitejs/rolldown-vite.git', REPO_PATH]],
+);
+
+await runCmdAndPipeOrExit(
+  '# Rebasing integration onto rolldown-vite...',
+  ['git', ['rebase', 'origin/rolldown-vite'], { nodeOptions: { cwd: REPO_PATH } }],
 );
 
 printTitle('# Updating pnpm-workspace.yaml to link to local rolldown...');

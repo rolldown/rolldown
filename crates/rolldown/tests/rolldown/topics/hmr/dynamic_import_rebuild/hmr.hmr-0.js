455b8b9d07e9d0f5f447c51ed7f4717acc359a36
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-11-19 11:17:14 +0900

    test(dev): add dynamic import rebuild test

---
+++ crates/rolldown/tests/rolldown/topics/hmr/dynamic_import_rebuild/hmr.hmr-0.js
@@ -0,0 +1,9 @@
+export async function foo() {
+  await import('./exist-dep-cjs').then(console.log)
+  await import('./exist-dep-esm').then(console.log)
+}
+
+export async function bar() {
+  await import('./new-dep-cjs.js').then(console.log)
+  await import('./new-dep-esm.js').then(console.log)
+}

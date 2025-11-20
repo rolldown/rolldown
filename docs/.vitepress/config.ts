61cfa066f74dfc619c7a81213e6d224ba07dd6af
Barbapapazes <e.soubiran25@gmail.com>
2025-11-20 13:47:07 +0100

    chore: update vitepress-plugin-og

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -332,10 +332,10 @@ export default defineConfig({
       text: 'Edit this page on GitHub',
     },
   },
-  async transformPageData(pageData) {
+  async transformPageData(pageData, ctx) {
     // Automatically handle OG images for all markdown files.
     if (!pageData.frontmatter.image && pageData.filePath !== 'index.md') {
-      await addOgImage(pageData, {
+      await addOgImage(pageData, ctx, {
         domain: 'https://rolldown.rs',
         maxTitleSizePerLine: 16,
       });

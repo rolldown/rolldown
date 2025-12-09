f4adbf889392497692859f37ed3880e222fe2f14
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-12-09 08:39:08 +0000

    chore: hide oxc runtime in snapshots (#7388)
    Hide oxc runtime in snapshots as normally we're not interested in them when looking at the snapshot.

--- crates/rolldown_testing/src/utils/mod.rs
+++ crates/rolldown_testing/src/utils/mod.rs
@@ -30,13 +30,18 @@ pub(crate) static HMR_RUNTIME_MODULE_OUTPUT_RE: LazyLock<Regex> = LazyLock::new(
     .expect("invalid hmr runtime module output regex")
 });
 
+pub(crate) static OXC_RUNTIME_MODULE_OUTPUT_RE: LazyLock<Regex> = LazyLock::new(|| {
+  Regex::new(r"(//#region \\0@oxc-project\+runtime@[\s\S]+?//#endregion)")
+    .expect("invalid oxc runtime module output regex")
+});
+
 // Some content of snapshot are meaningless, we'd like to remove them to reduce the noise when reviewing snapshots.
 pub fn tweak_snapshot(
   content: &str,
   hide_runtime_module: bool,
   hide_hmr_runtime: bool,
 ) -> Cow<'_, str> {
-  if !hide_runtime_module && !hide_hmr_runtime {
+  if !hide_runtime_module && !hide_hmr_runtime && !content.contains("\\0@oxc-project+runtime@") {
     return Cow::Borrowed(content);
   }
 
@@ -51,6 +56,10 @@ pub fn tweak_snapshot(
     result =
       HMR_RUNTIME_MODULE_OUTPUT_RE.replace_all(&result, "// HIDDEN [rolldown:hmr]").into_owned();
   }
+
+  result = OXC_RUNTIME_MODULE_OUTPUT_RE
+    .replace_all(&result, "// HIDDEN [\\0@oxc-project+runtime@0.0.0/file.js]")
+    .into_owned();
 
   Cow::Owned(result)
 }

c02e885de55c93f6683492abab5ce8c8c31d9b5f
Evan You <evan@vuejs.org>
2025-12-26 15:53:59 +0800

    bump theme + adjust css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import RolldownTheme from '@voidzero-dev/vitepress-theme/src/rolldown';
 import 'virtual:group-icons.css';
 import type { Theme } from 'vitepress';
 import DefinedIn from './components/DefinedIn.vue';
+import './styles.css';
 
 export default {
   extends: RolldownTheme,

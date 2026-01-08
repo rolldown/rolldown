cee9d1cb75274a8331ba279840a48de9b176c89d
mdong1909 <mdong1909@users.noreply.github.com>
2026-01-08 17:02:12 +0800

    fmt

--- docs/.vitepress/theme/FeatureGrid.vue
+++ docs/.vitepress/theme/FeatureGrid.vue
@@ -1,7 +1,7 @@
 <script setup>
-import RiveAnimation from "@voidzero-dev/vitepress-theme/src/components/shared/RiveAnimation.vue";
-import vitePoweredAnimation from '@voidzero-dev/vitepress-theme/src/assets/rolldown/animations/640_x_300_design_for_vite.riv'
-import PerformanceBar from "@voidzero-dev/vitepress-theme/src/components/shared/PerformanceBar.vue";
+import RiveAnimation from '@voidzero-dev/vitepress-theme/src/components/shared/RiveAnimation.vue';
+import vitePoweredAnimation from '@voidzero-dev/vitepress-theme/src/assets/rolldown/animations/640_x_300_design_for_vite.riv';
+import PerformanceBar from '@voidzero-dev/vitepress-theme/src/components/shared/PerformanceBar.vue';
 import performanceBarBg from '@voidzero-dev/vitepress-theme/src/assets/rolldown/rolldown-rollup-background.jpg';
 
 const performanceData = [
@@ -9,12 +9,12 @@ const performanceData = [
     name: 'Rolldown',
     percentage: 4.01,
     time: '1.61s',
-    isPrimary: true
+    isPrimary: true,
   },
   {
     name: 'esbuild',
     percentage: 4.24,
-    time: '1.70s'
+    time: '1.70s',
   },
   {
     name: 'rspack',
@@ -24,31 +24,44 @@ const performanceData = [
   {
     name: 'Rollup + esbuild',
     percentage: 100,
-    time: '40.10s'
-  }
-]
+    time: '40.10s',
+  },
+];
 </script>
 
 <template>
-  <section class="wrapper wrapper--ticks border-t grid lg:grid-cols-2 divide-x divide-y divide-nickel">
+  <section
+    class="wrapper wrapper--ticks border-t grid lg:grid-cols-2 divide-x divide-y divide-nickel"
+  >
     <div class="flex flex-col gap-3 justify-between">
       <div class="p-5 sm:p-15 pb-0 sm:pb-0 flex flex-col gap-3">
         <h5 class="text-balance sm:text-pretty text-white">Speed of Rust</h5>
-        <p class="sm:max-w-[30rem] text-pretty">Rolldown handles tens of thousands of modules without breaking a sweat
+        <p class="sm:max-w-[30rem] text-pretty">
+          Rolldown handles tens of thousands of modules without breaking a sweat
         </p>
       </div>
       <div class="p-5 sm:p-15 flex flex-col gap-10">
         <div class="flex flex-col gap-4">
-          <PerformanceBar v-for="item in performanceData" :key="item.name" :data="item"
-            :background-image="performanceBarBg" />
+          <PerformanceBar
+            v-for="item in performanceData"
+            :key="item.name"
+            :data="item"
+            :background-image="performanceBarBg"
+          />
         </div>
         <div>
-          <p class="text-sm"><a
+          <p class="text-sm">
+            <a
               href="https://github.com/rolldown/benchmarks/tree/f3997c801b7d261e8aaa15d4affbf6730cc19884?tab=readme-ov-file#ubuntu-latest-updated-2025-12-21"
-              target="_blank" class="text-white font-medium">Benchmark</a> bundling <span
-              class="text-white font-medium">19k</span>
-            modules.</p>
-          <p class="text-xs">10k React JSX components + 9k iconify JS files, with minification and source maps.</p>
+              target="_blank"
+              class="text-white font-medium"
+              >Benchmark</a
+            >
+            bundling <span class="text-white font-medium">19k</span> modules.
+          </p>
+          <p class="text-xs">
+            10k React JSX components + 9k iconify JS files, with minification and source maps.
+          </p>
         </div>
       </div>
     </div>
@@ -59,18 +72,31 @@ const performanceData = [
         <p class="max-w-[30rem] text-pretty">Familiar API & options with a rich plugin ecosystem</p>
       </div>
       <div class="card-bg px-5 sm:px-20 pt-5 sm:pt-10 flex justify-center">
-        <img src="@assets/rolldown/rolldown-rollup.png" class="-mb-[60px]" inert loading="lazy" alt="rollup compatible">
+        <img
+          src="@assets/rolldown/rolldown-rollup.png"
+          class="-mb-[60px]"
+          inert
+          loading="lazy"
+          alt="rollup compatible"
+        />
       </div>
     </div>
 
     <div class="flex flex-col gap-3 justify-between">
       <div class="p-5 sm:p-15 pb-0 sm:pb-0 flex flex-col gap-3">
         <h5 class="text-white">Esbuild Feature Parity</h5>
-        <p class="max-w-[26rem] text-pretty">Built-in transforms, define, inject, minify, CSS bundling & more...</p>
+        <p class="max-w-[26rem] text-pretty">
+          Built-in transforms, define, inject, minify, CSS bundling & more...
+        </p>
       </div>
       <div class="flex-1 flex items-center justify-center p-8 sm:p-15 sm:pt-10">
-        <img src="@assets/rolldown/rolldown-esbuild-parity.png" inert loading="lazy" alt="jest compatible"
-          class="w-full max-h-60 object-contain">
+        <img
+          src="@assets/rolldown/rolldown-esbuild-parity.png"
+          inert
+          loading="lazy"
+          alt="jest compatible"
+          class="w-full max-h-60 object-contain"
+        />
       </div>
     </div>
 
@@ -79,8 +105,12 @@ const performanceData = [
         <h5 class="text-white">Designed for Vite</h5>
         <p class="max-w-[25rem] text-pretty">The unified bundler powering Vite 8+</p>
       </div>
-      <RiveAnimation :desktop-src="vitePoweredAnimation" :desktop-width="640" :desktop-height="300"
-        canvas-class="w-full mb-10" />
+      <RiveAnimation
+        :desktop-src="vitePoweredAnimation"
+        :desktop-width="640"
+        :desktop-height="300"
+        canvas-class="w-full mb-10"
+      />
     </div>
   </section>
 </template>
--- docs/.vitepress/theme/Home.vue
+++ docs/.vitepress/theme/Home.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Hero from './Hero.vue'
+import Hero from './Hero.vue';
 // import TrustedBy from '@components/oss/TrustedBy.vue'
-import HeadingSection from '@voidzero-dev/vitepress-theme/src/components/oss/HeadingSection.vue'
-import RolldownFeatureGrid from './FeatureGrid.vue'
-import Sponsors from '@voidzero-dev/vitepress-theme/src/components/oss/Sponsors.vue'
-import Spacer from '@voidzero-dev/vitepress-theme/src/components/shared/Spacer.vue'
-import Footer from '@voidzero-dev/vitepress-theme/src/components/oss/Footer.vue'
+import HeadingSection from '@voidzero-dev/vitepress-theme/src/components/oss/HeadingSection.vue';
+import RolldownFeatureGrid from './FeatureGrid.vue';
+import Sponsors from '@voidzero-dev/vitepress-theme/src/components/oss/Sponsors.vue';
+import Spacer from '@voidzero-dev/vitepress-theme/src/components/shared/Spacer.vue';
+import Footer from '@voidzero-dev/vitepress-theme/src/components/oss/Footer.vue';
 </script>
 
 <template>
@@ -16,8 +16,14 @@ import Footer from '@voidzero-dev/vitepress-theme/src/components/oss/Footer.vue'
   <RolldownFeatureGrid />
   <Sponsors
     description="Rolldown is free and open source, made possible by a full-time team and passionate open-source contributors."
-    sponsorLinkText="Contribute" sponsorLink="/contribution-guide" />
+    sponsorLinkText="Contribute"
+    sponsorLink="/contribution-guide"
+  />
   <Spacer />
-  <Footer heading="Optimize your JavaScript code with Rolldown" subheading="Why do we still need bundlers?"
-    button-text="Learn more" button-link="/in-depth/why-bundlers" />
+  <Footer
+    heading="Optimize your JavaScript code with Rolldown"
+    subheading="Why do we still need bundlers?"
+    button-text="Learn more"
+    button-link="/in-depth/why-bundlers"
+  />
 </template>

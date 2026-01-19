40287ffcd31aa77f0edcf6c3d1ef2d52af7c79bd
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-19 14:48:28 +0900

    wip(dev/lazy): inlined dynamic import in lazy chunk

--- examples/lazy/src/async-entry-a.js
+++ examples/lazy/src/async-entry-a.js
@@ -3,3 +3,9 @@ import './async-lib-shared.js';
 
 console.log('async-entry-a.js', asyncLibA);
 document.getElementById('root').innerHTML += '[async-entry-a.js] loaded\n';
+
+import './inlined.js';
+setTimeout(async () => {
+  const exports = await import('./inlined.js');
+  console.log('Loaded inlined.js:', exports);
+}, 1000 * 1);

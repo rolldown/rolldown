185c295802b494e8adfb8197f7f120724c0f559f
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-19 15:07:56 +0900

    wip(dev/lazy): `this` in static class field is replaced with `void 0`

--- examples/lazy/src/async-entry-a.js
+++ examples/lazy/src/async-entry-a.js
@@ -3,3 +3,9 @@ import './async-lib-shared.js';
 
 console.log('async-entry-a.js', asyncLibA);
 document.getElementById('root').innerHTML += '[async-entry-a.js] loaded\n';
+
+class Foo {
+  // eslint-disable-next-line no-unused-private-class-members
+  static #_ = (this.foo = 0);
+}
+console.log(new Foo().foo);

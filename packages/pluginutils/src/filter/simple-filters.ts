42f34d67921a1d7cd875a7c318f9483d3a3614a6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-05 14:39:09 -0500

    This is gnarly

--- packages/pluginutils/src/filter/simple-filters.ts
+++ packages/pluginutils/src/filter/simple-filters.ts
@@ -46,7 +46,7 @@ export function exactRegex(str: string, flags?: string): RegExp {
  * ```
  */
 export function matchExt(ext: string): RegExp {
-  return exactRegex(`\.${ext}(?=$|[?#])`);
+  return new RegExp(`^[^?#]*\\.${escapeRegex(ext)}(?=$|[?#])`);
 }
 
 /**

e8c96224d3e9be8f39627d796ff7795ac5f87aca
hyf0 <i.heyunfei@gmail.com>
2026-02-16 15:44:22 +0000

    fix(cjs): bailout treeshaking on cjs modules that have multiple re-exports (#8348)
    Details is explained in https://github.com/rolldown/rolldown/issues/8345#issuecomment-3908370251.
    
    Fixes https://github.com/rolldown/rolldown/issues/8345.

---
+++ crates/rolldown/tests/rolldown/issues/8345/lib/prod.js
@@ -0,0 +1,4 @@
+'use strict';
+var a = 1;
+var b = 2;
+exports.a = a, exports.b = b;
---
+++ crates/rolldown/tests/rolldown/issues/8345/main.js
@@ -0,0 +1,11 @@
+import assert from 'node:assert'
+
+// This tests that conditional CJS re-exports preserve exports from all branches.
+// When platform: "node", process.env.NODE_ENV is not defined, so both branches
+// of the conditional require are included. The exports from both branches must
+// be preserved (not tree-shaken).
+const lib = require('./lib/index.js')
+assert.strictEqual(typeof lib.a, 'number', 'lib.a should be a number')
+assert.strictEqual(typeof lib.b, 'number', 'lib.b should be a number')
+assert.strictEqual(lib.a, 1)
+assert.strictEqual(lib.b, 2)

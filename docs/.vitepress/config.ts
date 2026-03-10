41e79833120976c81491189b969b4425d083e6bc
dalaoshu <165626830+shulaoda@users.noreply.github.com>
2026-03-10 17:53:25 +0800

    revert: "docs: add viteplus alpha announcement banner (#8615)" (#8616)
    This reverts #8615

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -283,12 +283,6 @@ const config = defineConfig({
       },
     },
 
-    banner: {
-      id: 'viteplus-alpha',
-      text: 'Announcing Vite+ Alpha: Open source. Unified. Next-gen.',
-      url: 'https://voidzero.dev/posts/announcing-vite-plus-alpha?utm_source=rolldown&utm_content=top_banner',
-    },
-
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       {

52e84dd75aa2f6bcd6a7c0bf71cdd1f639054a26
Dunqing <dengqing0821@gmail.com>
2026-03-14 22:18:49 +0800

    refactor(rust): remove `BindingIdentifierExt` to use `BindingIdentifier::symbol_id()` instead (#8667)
    They are literally the same thing and much cleaner.

--- crates/rolldown_ecmascript_utils/src/lib.rs
+++ crates/rolldown_ecmascript_utils/src/lib.rs
@@ -6,9 +6,8 @@ mod source_utils;
 pub use crate::{
   ast_snippet::AstSnippet,
   extensions::ast_ext::{
-    binding_identifier_ext::BindingIdentifierExt, binding_pattern_ext::BindingPatternExt,
-    call_expression_ext::CallExpressionExt, expression_ext::ExpressionExt, function::FunctionExt,
-    jsx::JsxExt, statement_ext::StatementExt,
+    binding_pattern_ext::BindingPatternExt, call_expression_ext::CallExpressionExt,
+    expression_ext::ExpressionExt, function::FunctionExt, jsx::JsxExt, statement_ext::StatementExt,
   },
   scope::is_top_level,
   source_utils::contains_script_closing_tag,

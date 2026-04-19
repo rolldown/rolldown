e749cffd4a2a4a1b266d66e4fc28eef07346eefd
Alexander Lichter <github@lichter.io>
2026-04-19 22:57:30 +0200

    docs: add feedback form

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useData } from 'vitepress';
+import RolldownTheme from '@voidzero-dev/vitepress-theme/src/rolldown';
+import { computed } from 'vue';
+
+const { Layout } = RolldownTheme;
+const { frontmatter } = useData();
+
+const isDoc = computed(
+  () => frontmatter.value.layout === undefined || frontmatter.value.layout === 'doc',
+);
+</script>
+
+<template>
+  <Layout>
+    <template v-if="isDoc" #doc-after>
+      <FeedbackWidget />
+    </template>
+    <template v-if="isDoc" #aside-outline-after>
+      <FeedbackWidgetCompact />
+    </template>
+  </Layout>
+</template>

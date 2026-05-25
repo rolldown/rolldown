df616cb6fd8dfda3cadbf2f22504cc17789f1308
Dunqing <dengqing0821@gmail.com>
2026-05-25 13:39:48 +0800

    perf(finalizers): replace `_reExport` construction with a direct call to avoid calling `clone_in` (#9501)
    Previously, calling `clone_in` was to avoid borrow check errors, but I
    found we can refactor to call `self.finalized_expr_for_runtime_symbol`
    at the use site to avoid this error without using `clone_in`.
    
    ---------
    
    Co-authored-by: Jerry Zhao <165626830+shulaoda@users.noreply.github.com>

--- crates/rolldown/src/module_finalizers/mod.rs
+++ crates/rolldown/src/module_finalizers/mod.rs
@@ -888,10 +888,9 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
 
     let mut re_export_external_stmts: Option<_> = None;
     if !export_all_externals_rec_ids.is_empty() {
-      // construct `__reExport(importer_exports, importee_exports)`
-      let re_export_fn_ref = self.finalized_expr_for_runtime_symbol("__reExport");
       match self.ctx.options.format {
         OutputFormat::Esm => {
+          let re_export_name = self.canonical_name_for_runtime("__reExport");
           let stmts = export_all_externals_rec_ids.iter().copied().flat_map(|idx| {
             let rec = &self.ctx.module.import_records[idx];
             if rec.meta.contains(ImportRecordMeta::EntryLevelExternal)
@@ -911,8 +910,9 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
               return vec![];
             };
             let importee_name = &module.get_import_path(self.ctx.chunk, self.ctx.resolved_paths);
+            // construct `__reExport(importer_exports, importee_exports)`
             let call_expr = self.snippet.re_export_call_expr(
-              re_export_fn_ref.clone_in(self.alloc),
+              self.snippet.id_ref_expr(re_export_name, SPAN),
               self.snippet.id_ref_expr(binding_name_for_namespace_object_ref, SPAN),
               self.snippet.id_ref_expr(importee_namespace_name, SPAN),
             );
@@ -930,8 +930,6 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
         }
         OutputFormat::Cjs | OutputFormat::Iife | OutputFormat::Umd => {
           let stmts = export_all_externals_rec_ids.iter().copied().filter_map(|idx| {
-            // Insert `__reExport(importer_exports, require('ext'))`
-            let re_export_fn_ref = self.finalized_expr_for_runtime_symbol("__reExport");
             // importer_exports
             let (importer_namespace_ref_expr, _) = self.finalized_expr_for_symbol_ref(
               self.ctx.module.namespace_object_ref,
@@ -942,7 +940,8 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
             let importee = rec.resolved_module.map(|module_idx| &self.ctx.modules[module_idx])?;
 
             let re_export_call_expr = self.snippet.re_export_call_expr(
-              re_export_fn_ref.clone_in(self.alloc),
+              // Insert `__reExport(importer_exports, require('ext'))`
+              self.finalized_expr_for_runtime_symbol("__reExport"),
               importer_namespace_ref_expr,
               self.snippet.call_expr_with_arg_expr_expr(
                 "require",
@@ -1643,7 +1642,6 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
                 match importee.exports_kind {
                   ExportsKind::Esm => {
                     if importee_linking_info.has_dynamic_exports {
-                      let re_export_fn_ref = self.finalized_expr_for_runtime_symbol("__reExport");
                       // exports
                       let (importer_namespace_ref, _) = self.finalized_expr_for_symbol_ref(
                         self.ctx.module.namespace_object_ref,
@@ -1658,7 +1656,7 @@ impl<'me, 'ast> ScopeHoistingFinalizer<'me, 'ast> {
                       );
 
                       let call_expr = self.snippet.re_export_call_expr(
-                        re_export_fn_ref,
+                        self.finalized_expr_for_runtime_symbol("__reExport"),
                         importer_namespace_ref,
                         importee_namespace_ref,
                       );

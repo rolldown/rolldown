a82f2b13c5b9f220636033a47c527418ab7da040
shulaoda <165626830+shulaoda@users.noreply.github.com>
2026-07-04 08:01:37 +0800

    chore: remove dead CSS/HTML cache structs from plugin utils

--- crates/rolldown_plugin_utils/src/constants.rs
+++ crates/rolldown_plugin_utils/src/constants.rs
@@ -2,10 +2,8 @@ use std::sync::Arc;
 
 use arcstr::ArcStr;
 
-use rolldown_common::OutputChunk;
 use rolldown_plugin::typedmap::TypedMapKey;
 use rolldown_utils::dashmap::{FxDashMap, FxDashSet};
-use rustc_hash::FxHashMap;
 
 // Use `10kB` as a threshold for 'auto'
 // https://v8.dev/blog/cost-of-javascript-2019#json
@@ -27,7 +25,6 @@ impl TypedMapKey for ViteImportGlob {
 
 #[derive(Debug, Default)]
 pub struct ChunkMetadata {
-  pub imported_css: FxDashSet<ArcStr>,
   pub imported_assets: FxDashSet<ArcStr>,
 }
 
@@ -40,62 +37,4 @@ impl ViteMetadata {
   pub fn get(&self, key: ArcStr) -> Arc<ChunkMetadata> {
     self.inner.entry(key).or_default().clone()
   }
-}
-
-#[derive(Debug, Default)]
-pub struct CSSEntriesCache {
-  pub inner: FxDashMap<ArcStr, ArcStr>,
-}
-
-#[derive(Debug, Default)]
-pub struct CSSModuleCache {
-  pub inner: FxDashMap<String, FxHashMap<String, String>>,
-}
-
-#[derive(Debug, Default)]
-pub struct HTMLProxyResult {
-  pub inner: FxDashMap<String, String>,
-}
-
-#[derive(Debug, Default)]
-pub struct HTMLProxyMapItem {
-  pub code: ArcStr,
-  pub map: Option<rolldown_sourcemap::SourceMap>,
-}
-
-#[derive(Debug, Default)]
-pub struct HTMLProxyMap {
-  pub inner: FxDashMap<String, FxDashMap<usize, HTMLProxyMapItem>>,
-}
-
-#[derive(Debug, Default)]
-pub struct CSSStyles {
-  pub inner: FxDashMap<String, String>,
-}
-
-#[derive(Debug, Default)]
-pub struct PureCSSChunks {
-  pub inner: FxDashSet<ArcStr>,
-}
-
-#[derive(Debug, Default)]
-pub struct CSSChunkCache {
-  pub inner: FxDashMap<ArcStr, String>,
-}
-
-#[derive(Debug, Default)]
-pub struct CSSBundleName(pub String);
-
-#[derive(Debug, Default)]
-pub struct RemovedPureCSSFilesCache {
-  pub inner: FxDashMap<ArcStr, Arc<OutputChunk>>,
-}
-
-#[derive(Debug, Default)]
-pub struct CSSUrlCache {
-  pub inner: FxDashMap<String, String>,
-}
-
-pub struct CSSScopeToMap {
-  pub inner: FxHashMap<String, (String, Option<String>)>,
 }

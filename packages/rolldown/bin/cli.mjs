c5fc4733dad27d38a97930043ad602eeca6e6023
btea <2356281422@qq.com>
2026-08-21 20:14:30 +0800

    fix: update

--- packages/rolldown/bin/cli.mjs
+++ packages/rolldown/bin/cli.mjs
@@ -2,5 +2,10 @@
 import module from 'node:module';
 try {
   module.enableCompileCache?.();
+  setTimeout(() => {
+    try {
+      module.flushCompileCache?.()
+    } catch {}
+  }, 10 * 1000).unref()
 } catch {}
 await import('../dist/cli.mjs');

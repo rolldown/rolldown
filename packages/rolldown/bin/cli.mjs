4f810961e7af3877737bdb041b032e1dec288082
btea <2356281422@qq.com>
2026-08-21 21:36:30 +0800

    perf: enable compiler cache via `module.enableCompileCache()` (#10678)
    <!--
    - What is this PR solving? Write a clear and concise description.
    - Reference the issues it solves (e.g. `fixes #123`).
    - What other alternatives have you explored?
    - Are there any parts you think require more attention from reviewers?
    
    Also, please make sure you do the following:
    
    - Read the Contributing Guidelines at
    https://rolldown.rs/contribution-guide/.
    - Check that there isn't already a PR that solves the problem the same
    way. If you find a duplicate, please help us review it.
    - Update the corresponding documentation if needed.
    - Include relevant tests that fail without this PR but pass with it. If
    the tests are not included, explain why.
    
    Thank you for contributing to Rolldown!
    -->
    https://beta.docs.nodejs.org/module#moduleenablecompilecacheoptions
    
    ```
    hyperfine --warmup 5 --runs 50 'node bin/cli.mjs --version'  # without this PR
     Time (mean ± σ):      50.4 ms ±   2.1 ms    [User: 48.4 ms, System: 7.6 ms]
     Range (min … max):    47.4 ms …  58.3 ms    50 runs
    
    hyperfine --warmup 5 --runs 50 'node bin/cli.mjs --version'  # with this PR
     Time (mean ± σ):      47.6 ms ±   1.0 ms    [User: 45.8 ms, System: 7.6 ms]
     Range (min … max):    46.2 ms …  51.0 ms    50 runs
    ```
    
    ---------
    
    Co-authored-by: Jerry Zhao <165626830+shulaoda@users.noreply.github.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/rolldown/bin/cli.mjs
+++ packages/rolldown/bin/cli.mjs
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-import '../dist/cli.mjs';
+import module from 'node:module';
+try {
+  module.enableCompileCache?.();
+  setTimeout(() => {
+    try {
+      module.flushCompileCache?.();
+    } catch {}
+  }, 10 * 1000).unref();
+} catch {}
+await import('../dist/cli.mjs');

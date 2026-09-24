dc0b0e1135696127a6d43183cec9267d12c15772
IWANABETHATGUY <iwanabethatguy@qq.com>
2026-09-25 00:12:25 +0800

    refactor(binding): remove `__internalForcePanic`
    verify-debuginfo no longer calls it.

--- packages/rolldown/src/binding.cjs
+++ packages/rolldown/src/binding.cjs
@@ -827,7 +827,6 @@ module.exports.BindingWatcherEvent = nativeBinding.BindingWatcherEvent
 module.exports.ParallelJsPluginRegistry = nativeBinding.ParallelJsPluginRegistry
 module.exports.TraceSubscriberGuard = nativeBinding.TraceSubscriberGuard
 module.exports.TsconfigCache = nativeBinding.TsconfigCache
-module.exports.__internalForcePanic = nativeBinding.__internalForcePanic
 module.exports.BindingAttachDebugInfo = nativeBinding.BindingAttachDebugInfo
 module.exports.BindingBuiltinPluginName = nativeBinding.BindingBuiltinPluginName
 module.exports.BindingChunkModuleOrderBy = nativeBinding.BindingChunkModuleOrderBy
